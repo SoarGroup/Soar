0e2950a0cde29fcda8cbf096f8e6388a5f8f2f94
Shelly Nason <snason@umich.edu>
2003-07-31 15:14:32 +0000

    set default gamma to 1

--- OldSoar/branches/snason_RL_branch/soar/kernel/soar_core_utils.c
+++ OldSoar/branches/snason_RL_branch/soar/kernel/soar_core_utils.c
@@ -582,7 +582,7 @@ void soar_default_create_agent_procedure (char * agent_name) {
   current_agent(making_binary)                      = FALSE;  // SAN
   current_agent(next_Q)                             = 0;  // SAN
   current_agent(alpha)                              = 0.5;  // SAN
-  current_agent(gamma)                              = 0.9;  // SAN
+  current_agent(gamma)                              = 1.0;  // SAN
   current_agent(Temp)                               = 50.0;   // SAN
   current_agent(records)                            = NIL;
   current_agent(alias_list)                         = NIL;  /* AGR 568 */
