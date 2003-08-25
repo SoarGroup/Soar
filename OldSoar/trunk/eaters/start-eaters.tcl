dbb58b50437469ac7040a4af8e08d82bfda770e5
Bob Marinier <bob.marinier@soartech.com>
2003-08-25 14:43:29 +0000

    updated paths

--- OldSoar/trunk/eaters/start-eaters.tcl
+++ OldSoar/trunk/eaters/start-eaters.tcl
@@ -2,8 +2,8 @@
 
 set tcl_pkgPath {} 
 
-set soar_library [file join [file join [pwd] ..] [file join "soar-8.4.5" "library"]]
-set tsi_library  [file join [file join [pwd] ..] tsi-3.1.5]
+set soar_library [file join [file join [pwd] ..] [file join "soar-8.5.0" "library"]]
+set tsi_library  [file join [file join [pwd] ..] tsi-3.2.0]
 set soar_doc_dir [file join $soar_library doc]
 set doc_dir [file join $soar_library doc]
  
