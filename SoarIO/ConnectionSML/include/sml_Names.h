555cc23fe41d972af4f2ba25d25ef5300a120f77
Doug Pearson <doug@threepenny.net>
2005-02-09 18:31:23 +0000

    Converted to char const* const

--- SoarIO/ConnectionSML/include/sml_Names.h
+++ SoarIO/ConnectionSML/include/sml_Names.h
@@ -18,172 +18,132 @@ class sml_Names
 {
 public:
 	// <sml> tag identifiers
-	static char const* kTagSML ;
-	static char const* kID ;
-	static char const* kAck ;
-	static char const* kDocType ;
-	static char const* kDocType_Call ;
-	static char const* kDocType_Response ;
-	static char const* kDocType_Notify ;
-	static char const* kSoarVersion ;
-	static char const* kSMLVersion ;
-	static char const* kSMLVersionValue ;
-	static char const* kSoarVersionValue ;
-	static char const* kOutputLinkName ;
+	static char const* const kTagSML ;
+	static char const* const kID ;
+	static char const* const kAck ;
+	static char const* const kDocType ;
+	static char const* const kDocType_Call ;
+	static char const* const kDocType_Response ;
+	static char const* const kDocType_Notify ;
+	static char const* const kSoarVersion ;
+	static char const* const kSMLVersion ;
+	static char const* const kSMLVersionValue ;
+	static char const* const kSoarVersionValue ;
+	static char const* const kOutputLinkName ;
 
 	// <command> tag identifiers
-	static char const* kTagCommand ;
-	static char const* kCommandName ;
-	static char const* kCommandOutput ;
-	static char const* kRawOutput ;
-	static char const* kStructuredOutput ;
+	static char const* const kTagCommand ;
+	static char const* const kCommandName ;
+	static char const* const kCommandOutput ;
+	static char const* const kRawOutput ;
+	static char const* const kStructuredOutput ;
 
 	// <arg> tag identifiers
-	static char const* kTagArg ;
-	static char const* kArgParam ;
-	static char const* kArgType ;
+	static char const* const kTagArg ;
+	static char const* const kArgParam ;
+	static char const* const kArgType ;
 
 	// <error> tag identifiers
-	static char const* kTagError ;
-	static char const* kErrorCode ;
+	static char const* const kTagError ;
+	static char const* const kErrorCode ;
 
 	// <name> tag identifiers
-	static char const* kTagName ;
+	static char const* const kTagName ;
 
 	// <result> tag identifiers
-	static char const* kTagResult ;
+	static char const* const kTagResult ;
 
 	// input values (for update param)
-	static char const* kValueDelta ;
-	static char const* kValueFull ;
+	static char const* const kValueDelta ;
+	static char const* const kValueFull ;
 
 	// <wme> tag identifiers
-	static char const* kTagWME ;
-	static char const* kWME_TimeTag ;
-	static char const* kWME_Id ;
-	static char const* kWME_Attribute ;
-	static char const* kWME_Value ;
-	static char const* kWME_ValueType ;
-	static char const* kWME_Action ;
-	static char const* kValueAdd	;
-	static char const* kValueRemove ;
+	static char const* const kTagWME ;
+	static char const* const kWME_TimeTag ;
+	static char const* const kWME_Id ;
+	static char const* const kWME_Attribute ;
+	static char const* const kWME_Value ;
+	static char const* const kWME_ValueType ;
+	static char const* const kWME_Action ;
+	static char const* const kValueAdd	;
+	static char const* const kValueRemove ;
 
 	// Types
-	static char const* kTypeString ;
-	static char const* kTypeInt ;
-	static char const* kTypeDouble ;
-	static char const* kTypeChar ;
-	static char const* kTypeBoolean ;
-	static char const* kTypeID ;
-	static char const* kTypeVariable ;
+	static char const* const kTypeString ;
+	static char const* const kTypeInt ;
+	static char const* const kTypeDouble ;
+	static char const* const kTypeChar ;
+	static char const* const kTypeBoolean ;
+	static char const* const kTypeID ;
+	static char const* const kTypeVariable ;
 
 	// Params
-	static char const* kParamAgent ;
-	static char const* kParamKernel ;
-	static char const* kParamThis ;
-	static char const* kParamName ;
-	static char const* kParamFilename ;
-	static char const* kParamLearning ;
-	static char const* kParamOSupportMode ;
-	static char const* kParamValue ;
-	static char const* kParamWme ;
-	static char const* kParamWmeObject ;
-	static char const* kParamAttribute ;
-	static char const* kParamCount ;
-	static char const* kParamLength ;
-	static char const* kParamThread	;
-	static char const* kParamProcess ;
-	static char const* kParamLine ;
-	static char const* kParamLocation ;
-	static char const* kParamLogLocation ;
-	static char const* kParamLogLevel ;
-	static char const* kParamInputProducer ;
-	static char const* kParamOutputProcessor ;
-	static char const* kParamWorkingMemory ;
-	static char const* kParamAttributePath ;
-	static char const* kParamUpdate ;
-	static char const* kParamEventID ;
-	static char const* kParamLearnSetting;
-	static char const* kParamLogSetting;
-	static char const* kParamDirectory;
-	static char const* kParamSeconds;
-	static char const* kParamWarningsSetting;
-	static char const* kParamPhase ;
-	static char const* kParamInstance ;
-	static char const* kParamTimers;
-	static char const* kParamMessage;
-	static char const* kParamAlias;
-	static char const* kParamAliasedCommand;
-	static char const* kParamIndifferentSelectionMode;
-	static char const* kParamNumericIndifferentMode;
-	static char const* kParamRunResult;
-	static char const* kParamVersionMajor;
-	static char const* kParamVersionMinor;
-	static char const* kParamWaitSNC;
+	static char const* const kParamAgent ;
+	static char const* const kParamKernel ;
+	static char const* const kParamThis ;
+	static char const* const kParamName ;
+	static char const* const kParamFilename ;
+	static char const* const kParamLearning ;
+	static char const* const kParamOSupportMode ;
+	static char const* const kParamValue ;
+	static char const* const kParamWme ;
+	static char const* const kParamWmeObject ;
+	static char const* const kParamAttribute ;
+	static char const* const kParamCount ;
+	static char const* const kParamLength ;
+	static char const* const kParamThread	;
+	static char const* const kParamProcess ;
+	static char const* const kParamLine ;
+	static char const* const kParamLocation ;
+	static char const* const kParamLogLocation ;
+	static char const* const kParamLogLevel ;
+	static char const* const kParamInputProducer ;
+	static char const* const kParamOutputProcessor ;
+	static char const* const kParamWorkingMemory ;
+	static char const* const kParamAttributePath ;
+	static char const* const kParamUpdate ;
+	static char const* const kParamEventID ;
+	static char const* const kParamLearnSetting;
+	static char const* const kParamLogSetting;
+	static char const* const kParamDirectory;
+	static char const* const kParamSeconds;
+	static char const* const kParamWarningsSetting;
+	static char const* const kParamPhase ;
+	static char const* const kParamInstance ;
+	static char const* const kParamTimers;
+	static char const* const kParamMessage;
+	static char const* const kParamAlias;
+	static char const* const kParamAliasedCommand;
+	static char const* const kParamIndifferentSelectionMode;
+	static char const* const kParamNumericIndifferentMode;
+	static char const* const kParamRunResult;
+	static char const* const kParamVersionMajor;
+	static char const* const kParamVersionMinor;
+	static char const* const kParamWaitSNC;
+	static char const* const kParamFunction ;
 
 	// Values (these are not case sensitive unlike the rest)
-	static char const* kTrue ;
-	static char const* kFalse ;
+	static char const* const kTrue ;
+	static char const* const kFalse ;
 
 	// sgio style commands
-	static char const* kCommand_CreateAgent ;
-	static char const* kCommand_DestroyAgent ;
-	static char const* kCommand_GetAgentList ;
-	static char const* kCommand_LoadProductions ;
-	static char const* kCommand_GetInputLink ;
-	static char const* kCommand_GetOutputLink ;
-	static char const* kCommand_Run ;
-	static char const* kCommand_Input ;
-	static char const* kCommand_Output ;
-	static char const* kCommand_CheckForIncomingCommands ;
-	static char const* kCommand_StopOnOutput ;
-	static char const* kCommand_RegisterForEvent ;
-	static char const* kCommand_UnregisterForEvent ;
-	static char const* kCommand_Event ;			// Just passes event id
+	static char const* const kCommand_CreateAgent ;
+	static char const* const kCommand_DestroyAgent ;
+	static char const* const kCommand_GetAgentList ;
+	static char const* const kCommand_LoadProductions ;
+	static char const* const kCommand_GetInputLink ;
+	static char const* const kCommand_GetOutputLink ;
+	static char const* const kCommand_Run ;
+	static char const* const kCommand_Input ;
+	static char const* const kCommand_Output ;
+	static char const* const kCommand_CheckForIncomingCommands ;
+	static char const* const kCommand_StopOnOutput ;
+	static char const* const kCommand_RegisterForEvent ;
+	static char const* const kCommand_UnregisterForEvent ;
+	static char const* const kCommand_Event ;			// Just passes event id
 
 	// Command line interface
-	static char const* kCommand_CommandLine ;
-
-	// gSKI style commands
-	static char const* kgSKI_CreateKernelFactory ;
-	static char const* kgSKI_IRelease_Release ;
-	static char const* kgSKI_IKernelFactory_Create ;
-	static char const* kgSKI_IKernelFactory_DestroyKernel ;
-	static char const* kgSKI_IKernel_GetAgentManager ;
-	static char const* kgSKI_IInputLink_GetInputLinkMemory ;
-	static char const* kgSKI_IAgent_GetInputLink ;
-	static char const* kgSKI_IAgentManager_AddAgent	;
-	static char const* kgSKI_IAgentManager_RemoveAgent ;
-	static char const* kgSKI_IWme_GetValue ;
-	static char const* kgSKI_ISymbol_GetObject ;
-	static char const* kgSKI_ISymbol_GetString ;
-	static char const* kgSKI_ISymbol_GetInt	;
-	static char const* kgSKI_IWorkingMemory_ReplaceStringWme ;
-	static char const* kgSKI_IWorkingMemory_ReplaceIntWme ;
-	static char const* kgSKI_IWorkingMemory_ReplaceDoubleWme ;
-	static char const* kgSKI_IWorkingMemory_AddWmeNewObject ;
-	static char const* kgSKI_IWorkingMemory_AddWmeString ;
-	static char const* kgSKI_IWorkingMemory_AddWmeInt ;
-	static char const* kgSKI_IWorkingMemory_AddWmeDouble ;
-	static char const* kgSKI_IWorkingMemory_RemoveObject ;
-	static char const* kgSKI_IWorkingMemory_AddWmeObjectLink ;
-	static char const* kgSKI_IWorkingMemory_GetAgent ;
-	static char const* kgSKI_IAgent_RunInClientThread ;
-	static char const* kgSKI_IWMObject_GetWMEs ;
-	static char const* kgSKI_IInputLink_AddInputProducer ;
-	static char const* kgSKI_IInputLink_GetRootObject ;
-	static char const* kgSKI_IInputProducer_Update ;
-	static char const* kgSKI_IOutputProcessor_ProcessOutput ;
-	static char const* kgSKI_IAgent_GetOutputLink ;
-	static char const* kgSKI_IOutputLink_AddOutputProcessor ;
-	static char const* kgSKI_IOutputLink_SetAutomaticUpdate ;
-	static char const* kgSKI_IOutputLink_GetRootObject ;
-	static char const* kgSKI_IOutputLink_GetOutputLinkMemory ;
-
-	// These should work for all pointer iterators (which is almost all iterators)
-	static char const* kgSKI_IIterator_Pointer_IsValid ;
-	static char const* kgSKI_IIterator_Pointer_GetVal ;
-	static char const* kgSKI_IIterator_Pointer_Next ;
+	static char const* const kCommand_CommandLine ;
 } ;
 
 }
--- SoarIO/ConnectionSML/src/sml_Names.cpp
+++ SoarIO/ConnectionSML/src/sml_Names.cpp
@@ -17,169 +17,129 @@
 using namespace sml ;
 
 // <sml> tag identifiers
-char const* sml_Names::kTagSML			= "sml" ;
-char const* sml_Names::kID				= "id" ;
-char const* sml_Names::kAck				= "ack" ;
-char const* sml_Names::kDocType			= "doctype" ;
-char const* sml_Names::kDocType_Call	= "call" ;
-char const* sml_Names::kDocType_Response= "response" ;
-char const* sml_Names::kDocType_Notify	= "notify" ;
-char const* sml_Names::kSoarVersion		= "soarversion" ;
-char const* sml_Names::kSMLVersion		= "smlversion" ;
-char const* sml_Names::kSMLVersionValue	= "8.6.0" ;
-char const* sml_Names::kSoarVersionValue= "1.0" ;
-char const* sml_Names::kOutputLinkName  = "output-link" ;
+char const* const sml_Names::kTagSML			= "sml" ;
+char const* const sml_Names::kID				= "id" ;
+char const* const sml_Names::kAck				= "ack" ;
+char const* const sml_Names::kDocType			= "doctype" ;
+char const* const sml_Names::kDocType_Call		= "call" ;
+char const* const sml_Names::kDocType_Response	= "response" ;
+char const* const sml_Names::kDocType_Notify	= "notify" ;
+char const* const sml_Names::kSoarVersion		= "soarversion" ;
+char const* const sml_Names::kSMLVersion		= "smlversion" ;
+char const* const sml_Names::kSMLVersionValue	= "8.6.0" ;
+char const* const sml_Names::kSoarVersionValue	= "1.0" ;
+char const* const sml_Names::kOutputLinkName	= "output-link" ;
 
 // <command> tag identifiers
-char const* sml_Names::kTagCommand		= "command" ;
-char const* sml_Names::kCommandName		= "name" ;
-char const* sml_Names::kCommandOutput	= "output" ;
-char const* sml_Names::kRawOutput		= "raw" ;
-char const* sml_Names::kStructuredOutput = "structured" ;
+char const* const sml_Names::kTagCommand		= "command" ;
+char const* const sml_Names::kCommandName		= "name" ;
+char const* const sml_Names::kCommandOutput		= "output" ;
+char const* const sml_Names::kRawOutput			= "raw" ;
+char const* const sml_Names::kStructuredOutput  = "structured" ;
 
 // <arg> tag identifiers
-char const* sml_Names::kTagArg			= "arg" ;
-char const* sml_Names::kArgParam		= "param" ;
-char const* sml_Names::kArgType			= "type" ;
+char const* const sml_Names::kTagArg			= "arg" ;
+char const* const sml_Names::kArgParam			= "param" ;
+char const* const sml_Names::kArgType			= "type" ;
 
 // <error> tag identifiers
-char const* sml_Names::kTagError		= "error" ;
-char const* sml_Names::kErrorCode		= "code" ;
+char const* const sml_Names::kTagError		= "error" ;
+char const* const sml_Names::kErrorCode		= "code" ;
 
 // <name> tag identifiers
-char const* sml_Names::kTagName			= "name" ;
+char const* const sml_Names::kTagName		= "name" ;
 
 // <result> tag identifiers
-char const* sml_Names::kTagResult		= "result" ;
+char const* const sml_Names::kTagResult		= "result" ;
 
 // input values (for update param)
-char const* sml_Names::kValueDelta		= "delta" ;
-char const* sml_Names::kValueFull		= "full" ;
+char const* const sml_Names::kValueDelta	= "delta" ;
+char const* const sml_Names::kValueFull		= "full" ;
 
 // <wme> tag identifiers
-char const* sml_Names::kTagWME			= "wme" ;
-char const* sml_Names::kWME_TimeTag		= "tag" ;
-char const* sml_Names::kWME_Id			= "id" ;
-char const* sml_Names::kWME_Attribute	= "att" ;
-char const* sml_Names::kWME_Value		= "value" ;
-char const* sml_Names::kWME_ValueType	= "type" ;
-char const* sml_Names::kWME_Action		= "action" ;
-char const* sml_Names::kValueAdd		= "add" ;
-char const* sml_Names::kValueRemove		= "remove" ;
+char const* const sml_Names::kTagWME		= "wme" ;
+char const* const sml_Names::kWME_TimeTag	= "tag" ;
+char const* const sml_Names::kWME_Id		= "id" ;
+char const* const sml_Names::kWME_Attribute	= "att" ;
+char const* const sml_Names::kWME_Value		= "value" ;
+char const* const sml_Names::kWME_ValueType	= "type" ;
+char const* const sml_Names::kWME_Action	= "action" ;
+char const* const sml_Names::kValueAdd		= "add" ;
+char const* const sml_Names::kValueRemove	= "remove" ;
 
 // Types
-char const* sml_Names::kTypeString	= "string" ;
-char const* sml_Names::kTypeInt		= "int" ;
-char const* sml_Names::kTypeDouble	= "double" ;
-char const* sml_Names::kTypeChar	= "char" ;
-char const* sml_Names::kTypeBoolean = "boolean" ;
-char const* sml_Names::kTypeID		= "id" ;
-char const* sml_Names::kTypeVariable = "variable" ;
+char const* const sml_Names::kTypeString	= "string" ;
+char const* const sml_Names::kTypeInt		= "int" ;
+char const* const sml_Names::kTypeDouble	= "double" ;
+char const* const sml_Names::kTypeChar		= "char" ;
+char const* const sml_Names::kTypeBoolean	= "boolean" ;
+char const* const sml_Names::kTypeID		= "id" ;
+char const* const sml_Names::kTypeVariable	= "variable" ;
 
 // Parameter names
-char const* sml_Names::kParamAgent				= "agent" ;
-char const* sml_Names::kParamKernel				= "kernel" ;
-char const* sml_Names::kParamThis				= "this" ;
-char const* sml_Names::kParamName				= "name" ;
-char const* sml_Names::kParamFilename			= "filename" ;
-char const* sml_Names::kParamLearning			= "learning" ;
-char const* sml_Names::kParamOSupportMode		= "osupportmode" ;
-char const* sml_Names::kParamValue				= "value" ;
-char const* sml_Names::kParamWme				= "wme" ;
-char const* sml_Names::kParamWmeObject			= "wmeobject" ;
-char const* sml_Names::kParamAttribute			= "att" ;
-char const* sml_Names::kParamLength				= "length" ;
-char const* sml_Names::kParamCount				= "count" ;
-char const* sml_Names::kParamThread				= "thread" ;
-char const* sml_Names::kParamProcess			= "process" ;
-char const* sml_Names::kParamLine				= "line" ;
-char const* sml_Names::kParamLocation			= "location" ;
-char const* sml_Names::kParamLogLocation		= "loglocation" ;
-char const* sml_Names::kParamLogLevel			= "loglevel" ;
-char const* sml_Names::kParamInputProducer		= "inputproducer" ;
-char const* sml_Names::kParamWorkingMemory		= "workingmemory" ;
-char const* sml_Names::kParamOutputProcessor	= "outputprocessor" ;
-char const* sml_Names::kParamAttributePath		= "attpath" ;
-char const* sml_Names::kParamUpdate				= "update" ;
-char const* sml_Names::kParamLearnSetting		= "learn_setting";
-char const* sml_Names::kParamLogSetting			= "log_setting";
-char const* sml_Names::kParamDirectory			= "directory";
-char const* sml_Names::kParamSeconds			= "seconds";
-char const* sml_Names::kParamWarningsSetting	= "warnings_setting";
-char const* sml_Names::kParamEventID			= "eventid" ;
-char const* sml_Names::kParamPhase				= "phase" ;
-char const* sml_Names::kParamInstance			= "instance" ;
-char const* sml_Names::kParamTimers				= "timers";
-char const* sml_Names::kParamMessage			= "message";
-char const* sml_Names::kParamAlias				= "alias";
-char const* sml_Names::kParamAliasedCommand		= "aliasedcommand";
-char const* sml_Names::kParamIndifferentSelectionMode = "indifferentselectionmode";
-char const* sml_Names::kParamNumericIndifferentMode	= "numericindifferentmode";
-char const* sml_Names::kParamRunResult			= "runresult";
-char const* sml_Names::kParamVersionMajor		= "versionmajor";
-char const* sml_Names::kParamVersionMinor		= "versionminor";
-char const* sml_Names::kParamWaitSNC			= "waitsnc";
+char const* const sml_Names::kParamAgent			= "agent" ;
+char const* const sml_Names::kParamKernel			= "kernel" ;
+char const* const sml_Names::kParamThis				= "this" ;
+char const* const sml_Names::kParamName				= "name" ;
+char const* const sml_Names::kParamFilename			= "filename" ;
+char const* const sml_Names::kParamLearning			= "learning" ;
+char const* const sml_Names::kParamOSupportMode		= "osupportmode" ;
+char const* const sml_Names::kParamValue			= "value" ;
+char const* const sml_Names::kParamWme				= "wme" ;
+char const* const sml_Names::kParamWmeObject		= "wmeobject" ;
+char const* const sml_Names::kParamAttribute		= "att" ;
+char const* const sml_Names::kParamLength			= "length" ;
+char const* const sml_Names::kParamCount			= "count" ;
+char const* const sml_Names::kParamThread			= "thread" ;
+char const* const sml_Names::kParamProcess			= "process" ;
+char const* const sml_Names::kParamLine				= "line" ;
+char const* const sml_Names::kParamLocation			= "location" ;
+char const* const sml_Names::kParamLogLocation		= "loglocation" ;
+char const* const sml_Names::kParamLogLevel			= "loglevel" ;
+char const* const sml_Names::kParamInputProducer	= "inputproducer" ;
+char const* const sml_Names::kParamWorkingMemory	= "workingmemory" ;
+char const* const sml_Names::kParamOutputProcessor	= "outputprocessor" ;
+char const* const sml_Names::kParamAttributePath	= "attpath" ;
+char const* const sml_Names::kParamUpdate			= "update" ;
+char const* const sml_Names::kParamLearnSetting		= "learn_setting";
+char const* const sml_Names::kParamLogSetting		= "log_setting";
+char const* const sml_Names::kParamDirectory		= "directory";
+char const* const sml_Names::kParamSeconds			= "seconds";
+char const* const sml_Names::kParamWarningsSetting	= "warnings_setting";
+char const* const sml_Names::kParamEventID			= "eventid" ;
+char const* const sml_Names::kParamPhase			= "phase" ;
+char const* const sml_Names::kParamInstance			= "instance" ;
+char const* const sml_Names::kParamTimers			= "timers";
+char const* const sml_Names::kParamMessage			= "message";
+char const* const sml_Names::kParamAlias			= "alias";
+char const* const sml_Names::kParamAliasedCommand		= "aliasedcommand";
+char const* const sml_Names::kParamIndifferentSelectionMode = "indifferentselectionmode";
+char const* const sml_Names::kParamNumericIndifferentMode	= "numericindifferentmode";
+char const* const sml_Names::kParamRunResult		= "runresult";
+char const* const sml_Names::kParamVersionMajor		= "versionmajor";
+char const* const sml_Names::kParamVersionMinor		= "versionminor";
+char const* const sml_Names::kParamWaitSNC			= "waitsnc";
+char const* const sml_Names::kParamFunction			= "function" ;
 
 // Values (these are not case sensitive unlike the rest)
-char const* sml_Names::kTrue	= "true" ;
-char const* sml_Names::kFalse	= "false" ;
+char const* const sml_Names::kTrue	= "true" ;
+char const* const sml_Names::kFalse	= "false" ;
 
 // sgio style commands
-char const* sml_Names::kCommand_CreateAgent		= "create_agent" ;
-char const* sml_Names::kCommand_DestroyAgent	= "destroy_agent" ;
-char const* sml_Names::kCommand_GetAgentList	= "get_agent_list" ;
-char const* sml_Names::kCommand_LoadProductions = "load_productions" ;
-char const* sml_Names::kCommand_GetInputLink	= "get_input_link" ;
-char const* sml_Names::kCommand_GetOutputLink	= "get_output_link" ;
-char const* sml_Names::kCommand_Run				= "run" ;
-char const* sml_Names::kCommand_Input			= "input" ;
-char const* sml_Names::kCommand_Output			= "output" ;
-char const* sml_Names::kCommand_CheckForIncomingCommands = "check_for_incoming_commands" ;
-char const* sml_Names::kCommand_StopOnOutput	= "stop_on_output" ;
-char const* sml_Names::kCommand_RegisterForEvent = "register_for_event" ;
-char const* sml_Names::kCommand_UnregisterForEvent = "unregister_for_event" ;
-char const* sml_Names::kCommand_Event			= "event" ;	// Just passes event id
+char const* const sml_Names::kCommand_CreateAgent	= "create_agent" ;
+char const* const sml_Names::kCommand_DestroyAgent	= "destroy_agent" ;
+char const* const sml_Names::kCommand_GetAgentList	= "get_agent_list" ;
+char const* const sml_Names::kCommand_LoadProductions = "load_productions" ;
+char const* const sml_Names::kCommand_GetInputLink	= "get_input_link" ;
+char const* const sml_Names::kCommand_GetOutputLink	= "get_output_link" ;
+char const* const sml_Names::kCommand_Run			= "run" ;
+char const* const sml_Names::kCommand_Input			= "input" ;
+char const* const sml_Names::kCommand_Output		= "output" ;
+char const* const sml_Names::kCommand_CheckForIncomingCommands = "check_for_incoming_commands" ;
+char const* const sml_Names::kCommand_StopOnOutput		 = "stop_on_output" ;
+char const* const sml_Names::kCommand_RegisterForEvent	 = "register_for_event" ;
+char const* const sml_Names::kCommand_UnregisterForEvent = "unregister_for_event" ;
+char const* const sml_Names::kCommand_Event				 = "event" ;	// Just passes event id
 
 // command line interface
-char const* sml_Names::kCommand_CommandLine  = "cmdline" ;
-
-// gSKI commands
-char const* sml_Names::kgSKI_CreateKernelFactory			= "gski::createkernelfactory" ;
-char const* sml_Names::kgSKI_IRelease_Release				= "gski::irelease::release" ;
-char const* sml_Names::kgSKI_IKernelFactory_Create			= "gski::ikernelfactory::create" ;
-char const* sml_Names::kgSKI_IKernelFactory_DestroyKernel	= "gski::ikernelfactory::destroykernel" ;
-char const* sml_Names::kgSKI_IKernel_GetAgentManager		= "gski::ikernel::getagentmanager" ;
-char const* sml_Names::kgSKI_IInputLink_GetInputLinkMemory	= "gski::iinputlink::getinputlinkmemory" ;
-char const* sml_Names::kgSKI_IInputLink_GetRootObject		= "gski::iinputlink::getrootobject" ;
-char const* sml_Names::kgSKI_IAgent_GetInputLink			= "gski::iagent::getinputlink" ;
-char const* sml_Names::kgSKI_IAgentManager_AddAgent			= "gski::iagentmanager::addagent" ;
-char const* sml_Names::kgSKI_IAgentManager_RemoveAgent		= "gski::iagentmanager::removeagent" ;
-char const* sml_Names::kgSKI_IWme_GetValue					= "gski::iwme::getvalue" ;
-char const* sml_Names::kgSKI_ISymbol_GetObject				= "gski::isymbol::getobject" ;
-char const* sml_Names::kgSKI_ISymbol_GetString				= "gski::isymbol::getstring" ;
-char const* sml_Names::kgSKI_ISymbol_GetInt					= "gski::isymbol::getint" ;
-char const* sml_Names::kgSKI_IWorkingMemory_ReplaceStringWme= "gski::iworkingmemory::replacestringwme" ;
-char const* sml_Names::kgSKI_IWorkingMemory_ReplaceIntWme	= "gski::iworkingmemory::replaceintwme" ;
-char const* sml_Names::kgSKI_IWorkingMemory_ReplaceDoubleWme= "gski::iworkingmemory::replacedoublewme" ;
-char const* sml_Names::kgSKI_IWorkingMemory_AddWmeNewObject = "gski::iworkingmemory::addwmenewobject" ;
-char const* sml_Names::kgSKI_IWorkingMemory_AddWmeString	= "gski::iworkingmemory::addwmestring" ;
-char const* sml_Names::kgSKI_IWorkingMemory_AddWmeInt		= "gski::iworkingmemory::addwmeint" ;
-char const* sml_Names::kgSKI_IWorkingMemory_RemoveObject	= "gski::iworkingmemory::removeobject" ;
-char const* sml_Names::kgSKI_IWorkingMemory_GetAgent		= "gski::iworkingmemory::getagent" ;
-char const* sml_Names::kgSKI_IWorkingMemory_AddWmeObjectLink= "gski::iworkingmemory::addwmeobjectlink" ;
-char const* sml_Names::kgSKI_IAgent_RunInClientThread		= "gski::iagent::runinclientthread" ;
-char const* sml_Names::kgSKI_IWMObject_GetWMEs				= "gski::iwmobject::getwmes" ;
-char const* sml_Names::kgSKI_IInputLink_AddInputProducer	= "gski::iinputlink::addinputproducer" ;
-char const* sml_Names::kgSKI_IInputProducer_Update			= "gski::iinputproducer::update" ;
-char const* sml_Names::kgSKI_IOutputProcessor_ProcessOutput	= "gski::ioutputprocessor::processoutput" ;
-char const* sml_Names::kgSKI_IAgent_GetOutputLink			= "gski::iagent::getoutputlink" ;
-char const* sml_Names::kgSKI_IOutputLink_AddOutputProcessor = "gski::ioutputlink::addoutputprocessor" ;
-char const* sml_Names::kgSKI_IOutputLink_SetAutomaticUpdate = "gski::ioutput::setautomaticupdate" ;
-char const* sml_Names::kgSKI_IOutputLink_GetRootObject		= "gski::ioutputlink::getrootobject" ;
-char const* sml_Names::kgSKI_IOutputLink_GetOutputLinkMemory= "gski::ioutputlink::getoutputlinkmemory" ;
-
-// These should work for all pointer iterators (which is almost all iterators)
-char const* sml_Names::kgSKI_IIterator_Pointer_IsValid		= "gski::iiterator_pointer_isvalid" ;
-char const* sml_Names::kgSKI_IIterator_Pointer_GetVal		= "gski::iiterator_pointer_getval" ;
-char const* sml_Names::kgSKI_IIterator_Pointer_Next			= "gski::iiterator_pointer_next" ;
-
+char const* const sml_Names::kCommand_CommandLine  = "cmdline" ;
