08cf7ac96aca88ce02548de29c5252132e87b4e3
Bob Marinier <bob.marinier@soartech.com>
2005-03-11 14:54:49 +0000

    attempting to make portable

--- SoarIO/bin/makeTclSMLPackage.tcl
+++ SoarIO/bin/makeTclSMLPackage.tcl
@@ -1,2 +1,7 @@
-pkg_mkIndex -verbose . Tcl_sml_ClientInterface[info sharedlibextension]
-exit
+if {$tcl_platform(platform) == "windows"} {
+	set directory "."
+} else {
+	set directory "../ClientSMLSWIG/Tcl/.libs"
+}
+
+pkg_mkIndex -verbose $directory *Tcl_sml_ClientInterface[info sharedlibextension]
