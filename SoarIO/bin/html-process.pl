0711086ee798cb8b4c3736189a04e35ce8aa5755
Jon Voigt <voigtjr@gmail.com>
2005-03-31 20:51:07 +0000

    added pre-process script

---
+++ SoarIO/bin/html-process.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+use strict;
+
+use HTML::TreeBuilder;
+
+my $tb = HTML::TreeBuilder->new;
+$tb->ignore_text(0);
+$tb->ignore_unknown(1);
+$tb->implicit_tags(1);
+$tb->warn(1);
+
+my $html = $tb->parse_file("Add-wme");
+
+while (my $element = $html->find("style")) {
+  #print "SERVED a style\n";
+  $element->delete();
+}
+
+while (my $element = $html->find("img")) {
+  #print "SERVED an img\n";
+  $element->delete();
+}
+
+while (my $element = $html->find("meta")) {
+  #print "SERVED a meta\n";
+  $element->delete();
+}
+
+while (my $element = $html->find("link")) {
+  #print "SERVED a link\n";
+  $element->delete();
+}
+
+while (my $element = $html->find("script")) {
+  #print "SERVED a script\n";
+  $element->delete();
+}
+
+while (my $element = $html->look_down("id","column-one")) {
+  #print "SERVED column-one\n";
+  $element->delete();
+}
+
+while (my $element = $html->look_down("id","footer")) {
+  #print "SERVED footer\n";
+  $element->delete();
+}
+
+while (my $element = $html->look_down("id","toc")) {
+  #print "SERVED toc\n";
+  $element->delete();
+}
+
+while (my $element = $html->look_down("class","editsection")) {
+  #print "SERVED editsection\n";
+  $element->delete();
+}
+
+print $html->as_HTML;
+#$html->dump;
