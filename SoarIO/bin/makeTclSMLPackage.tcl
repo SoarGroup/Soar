a85b881ae5fec35613ba8126280306c27823eb3b
Jon Voigt <voigtjr@gmail.com>
2005-04-13 19:11:29 +0000

    more ignores

--- SoarIO/bin/makeTclSMLPackage.tcl
+++
@@ -1,7 +0,0 @@
-if {$tcl_platform(platform) == "windows"} {
-	set directory "."
-} else {
-	set directory ".libs"
-}
-
-pkg_mkIndex -verbose $directory *Tcl_sml_ClientInterface[info sharedlibextension]
