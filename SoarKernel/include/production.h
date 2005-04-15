1ac3e7648dce456649ddd6b02271be8d7f0e61d4
Bob Marinier <bob.marinier@soartech.com>
2005-04-15 15:17:37 +0000

    removed #defs from production structure

--- SoarKernel/include/production.h
+++ SoarKernel/include/production.h
@@ -105,10 +105,7 @@ typedef struct production_struct {
   struct instantiation_struct *instantiations; /* dll of inst's in MS */
   int OPERAND_which_assert_list;          /* RCHONG: 10.11 */
   byte interrupt;						  /* SW: 7.31.03 */
-#ifdef BUG_139_WORKAROUND
   bool already_fired;         /* RPM test workaround for bug #139 */
-#endif
-
 } production;
 
 /* ========================================================================
