ee548840e9fd427298d856797ae9df6d3f03e879
Bob Marinier <bob.marinier@soartech.com>
2005-04-15 15:17:07 +0000

    updated for new locations

--- SoarJavaDebugger/copySML.bat
+++ SoarJavaDebugger/copySML.bat
@@ -2,13 +2,14 @@
 @REM where we can build and work with them.
 @REM Later, we may choose to just copy around a JAR file containing the Java files
 set SOARIO=..\soario
+set SOARLIB=..\soar-library
 
-xcopy %SOARIO%\ClientSMLSWIG\java\*.java sml\*.java /y
-xcopy %SOARIO%\ClientSMLSWIG\java\*.dll *.dll /y
+xcopy %SOARIO%\ClientSMLSWIG\java\build\*.java sml\*.java /y
+xcopy %SOARIO%\ClientSMLSWIG\java\build\*.dll *.dll /y
 
 @REM We also need libraries that Java_sml_ClientInterface.dll is dependent on.
-xcopy %SOARIO%\bin\SoarKernelSML.dll *.dll /y
-xcopy %SOARIO%\bin\ElementXML.dll *.dll /y
+xcopy %SOARLIB%\SoarKernelSML.dll *.dll /y
+xcopy %SOARLIB%\ElementXML.dll *.dll /y
 
 
 
