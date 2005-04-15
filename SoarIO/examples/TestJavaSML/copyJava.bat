3483564a84a9076f8523567ce89531bce36d360c
Bob Marinier <bob.marinier@soartech.com>
2005-04-15 16:31:34 +0000

    functionality moved to buildjava

--- SoarIO/examples/TestJavaSML/copyJava.bat
+++
@@ -1,8 +0,0 @@
-@REM Need to run this to copy the Java files over to our project
-@REM where we can build and work with them.
-@REM Later, we may choose to just copy around a JAR file containing the Java files
-
-set SOARIO=..\..
-
-xcopy %SOARIO%\ClientSMLSWIG\java\build\*.java sml\*.java /y
-
