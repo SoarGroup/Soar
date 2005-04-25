1772dade9d0da7ade494499e5d6e417457ddea06
Jon Voigt <voigtjr@gmail.com>
2005-04-25 17:54:00 +0000

    added copysml contents instead of calling it

--- SoarJavaDebugger/buildDebugger.bat
+++ SoarJavaDebugger/buildDebugger.bat
@@ -1,5 +1,10 @@
 @REM Builds the debugger
-copySML.bat
+
+set SOARIO=..\soario
+set SOARLIB=..\soar-library
+
+xcopy %SOARIO%\ClientSMLSWIG\java\build\*.java sml\*.java /y
+
 javac.exe -classpath .;swt.jar debugger\Application.java
 jar.exe cvfm ..\soar-library\SoarJavaDebugger.jar JarManifest .
 pause
