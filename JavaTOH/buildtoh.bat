f0f09aab375b53961fdeb15ba98c62df0b14c86c
Bob Marinier <bob.marinier@soartech.com>
2005-05-02 12:44:14 +0000

    updated so builds on windows

--- JavaTOH/buildtoh.bat
+++ JavaTOH/buildtoh.bat
@@ -1,5 +1,4 @@
 xcopy ..\SoarIO\ClientSMLSWIG\Java\build\*.java src\sml /Y /I
-
 javac -classpath .;swt.jar -sourcepath src\edu\umich\toh;src\sml;.;src src\edu\umich\toh\TowersOfHanoi.java
-
 jar cvfm ..\soar-library\toh.jar tohJarManifest -C src .
+pause
