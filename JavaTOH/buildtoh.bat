34eaec4debffadaaff0ba09e1e2efb5f133b77e4
Bob Marinier <bob.marinier@soartech.com>
2005-05-06 12:18:09 +0000

    slightly simplified build process

--- JavaTOH/buildtoh.bat
+++ JavaTOH/buildtoh.bat
@@ -1,4 +1,4 @@
 xcopy ..\SoarIO\ClientSMLSWIG\Java\build\*.java src\sml /Y /I
-javac -classpath .;..\soar-library\swt.jar -sourcepath src\edu\umich\toh;src\sml;.;src src\edu\umich\toh\TowersOfHanoi.java
+javac -classpath .;..\soar-library\swt.jar -sourcepath src\sml;src src\edu\umich\toh\TowersOfHanoi.java
 jar cvfm ..\soar-library\toh.jar tohJarManifest -C src .
 pause
