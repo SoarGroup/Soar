acdb0c4bdaa55d01cc250d218914485e0f498e50
Bob Marinier <bob.marinier@soartech.com>
2005-05-16 17:58:06 +0000

    updated to only copy productions over (images are now in jar file)

--- JavaMissionaries/buildmac.bat
+++ JavaMissionaries/buildmac.bat
@@ -1,5 +1,5 @@
 xcopy ..\SoarIO\ClientSMLSWIG\Java\build\*.java src\sml /Y /I
 javac -classpath .;..\soar-library\swt.jar -sourcepath src\sml;src src\edu\umich\mac\MissionariesAndCannibals.java
 jar cvfm ..\soar-library\mac.jar macJarManifest mac -C src .
-xcopy mac ..\soar-library\mac /Y /I
+xcopy mac\mac.soar ..\soar-library\mac\* /Y
 pause
