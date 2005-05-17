34e3dbb3485b9d988a1c18eab7391e3f6217efd2
Bob Marinier <bob.marinier@soartech.com>
2005-05-17 19:25:55 +0000

    unnecessary

--- SoarJavaDebugger/copySML.sh
+++
@@ -1,8 +0,0 @@
-
-# Need to run this to copy the Java files over to our project
-# where we can build and work with them.
-# Later, we may choose to just copy around a JAR file containing the Java files
-export SOARIO=../SoarIO
-export SOARLIB=../soar-library
-
-cp -f $SOARIO/ClientSMLSWIG/Java/build/*.java sml
