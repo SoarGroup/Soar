dafbbe9a0c9c1dac4d7e9ade8081b869efcf7e7a
Bob Marinier <bob.marinier@soartech.com>
2005-05-17 18:33:31 +0000

    now builds using sml.jar

--- JavaMissionaries/buildmac.bat
+++ JavaMissionaries/buildmac.bat
@@ -1,5 +1,5 @@
-xcopy ..\SoarIO\ClientSMLSWIG\Java\build\*.java src\sml /Y /I
-javac -classpath .;..\soar-library\swt.jar -sourcepath src\sml;src src\edu\umich\mac\MissionariesAndCannibals.java
+rem xcopy ..\SoarIO\ClientSMLSWIG\Java\build\*.java src\sml /Y /I
+javac -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar -sourcepath src src\edu\umich\mac\MissionariesAndCannibals.java
 jar cvfm ..\soar-library\mac.jar macJarManifest mac -C src .
 xcopy mac\mac.soar ..\soar-library\mac\* /Y
 pause
