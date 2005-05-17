f4a4fc1745053e0b0f90d47a4b6437d609128596
Bob Marinier <bob.marinier@soartech.com>
2005-05-17 19:59:40 +0000

    now uses sml.jar

--- SoarIO/examples/TestJavaSML/buildJava.bat
+++ SoarIO/examples/TestJavaSML/buildJava.bat
@@ -1,20 +1,8 @@
 @REM *** MAKE SURE CHANGES TO THIS FILE ARE REFLECTED IN THE .SH FILE
-@REM 
-@REM Need to run this to copy the Java files over to our project
-@REM where we can build and work with them.
-@REM Later, we may choose to just copy around a JAR file containing the Java files
+
 set SOARLIB=..\..\..\soar-library
-set SOARIO=..\..
 
-xcopy %SOARIO%\ClientSMLSWIG\java\build\*.java sml\*.java /y
+javac.exe -classpath %SOARLIB%\sml.jar Application.java
+jar cvfm %SOARLIB%\TestJavaSML.jar JarManifest .
 
-@REM Assuming javac is in the path, builds Application and 
-@REM copies it to soar-library.
-
-javac Application.java
-
-@REM FIXME better way to jar this?
-jar cvfm TestJavaSML.jar JarManifest *.class sml/*.class
-
-xcopy *.jar %SOARLIB% /y
-
+pause
