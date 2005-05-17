a16d20dbeaef518fe9385bfde7f11b43b4792010
Bob Marinier <bob.marinier@soartech.com>
2005-05-17 18:39:23 +0000

    updated to build using sml.jar

--- JavaTOH/buildtoh.bat
+++ JavaTOH/buildtoh.bat
@@ -1,4 +1,3 @@
-xcopy ..\SoarIO\ClientSMLSWIG\Java\build\*.java src\sml /Y /I
-javac -classpath .;..\soar-library\swt.jar -sourcepath src\sml;src src\edu\umich\toh\TowersOfHanoi.java
+javac -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar -sourcepath src src\edu\umich\toh\TowersOfHanoi.java
 jar cvfm ..\soar-library\toh.jar tohJarManifest -C src .
 pause
--- SoarJavaDebugger/buildDebugger.bat
+++ SoarJavaDebugger/buildDebugger.bat
@@ -1,10 +1,5 @@
 @REM Builds the debugger
 
-set SOARIO=..\soario
-set SOARLIB=..\soar-library
-
-xcopy %SOARIO%\ClientSMLSWIG\java\build\*.java sml\*.java /y
-
-javac.exe -classpath .;swt.jar debugger\Application.java
+javac.exe -classpath .;swt.jar;sml.jar debugger\Application.java
 jar.exe cvfm ..\soar-library\SoarJavaDebugger.jar JarManifest .
 pause
