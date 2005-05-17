3262ec41d6414d5b8e28e6b2714513dac8aba2e2
Jon Voigt <voigtjr@gmail.com>
2005-05-17 19:45:52 +0000

    updated for new jar location

--- SoarIO/examples/TestJavaSML/buildJava.sh
+++ SoarIO/examples/TestJavaSML/buildJava.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
-
 # *** MAKE SURE CHANGES TO THIS FILE ARE REFLECTED IN THE .BAT FILE
 
-# Need to run this to copy the Java files over to our project
-# where we can build and work with them.
-# Later, we may choose to just copy around a JAR file containing the Java files
 SOARLIB=../../../soar-library
-SOARIO=../..
 
-mkdir sml
-cp -f $SOARIO/ClientSMLSWIG/Java/build/*.java sml
-
-javac Application.java
-
-jar cvfm TestJavaSML.jar JarManifest *.class sml/*.class
-
-cp -f *.jar $SOARLIB
-
+javac -classpath .:${SOARLIB}/swt.jar;${SOARLIB}/sml.jar Application.java
+jar cvfm ${SOARLIB}/TestJavaSML.jar JarManifest .
