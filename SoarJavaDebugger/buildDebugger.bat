67f0623dbb20f1c4dd773bde5c501054c9c8a8a8
Jon Voigt <voigtjr@gmail.com>
2005-05-23 20:42:55 +0000

    removed verbose

--- SoarJavaDebugger/buildDebugger.bat
+++ SoarJavaDebugger/buildDebugger.bat
@@ -1,5 +1,5 @@
 @REM Builds the debugger
 
 javac.exe -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar debugger\Application.java
-jar.exe cvfm ..\soar-library\SoarJavaDebugger.jar JarManifest .
+jar.exe cfm ..\soar-library\SoarJavaDebugger.jar JarManifest .
 pause
