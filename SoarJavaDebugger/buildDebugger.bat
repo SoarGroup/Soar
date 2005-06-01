af9617906facae03494c741632c1f3262ba45fda
Bob Marinier <bob.marinier@soartech.com>
2005-06-01 00:58:50 +0000

    added step to delete all .class files first

--- SoarJavaDebugger/buildDebugger.bat
+++ SoarJavaDebugger/buildDebugger.bat
@@ -1,5 +1,6 @@
 @REM Builds the debugger
 
+del /S /Q *.class 
 javac.exe -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar debugger\Application.java
 jar.exe cfm ..\soar-library\SoarJavaDebugger.jar JarManifest .
 pause
