511eec378e621a7e1b4bfb82bc54134c4f68da0b
Jon Voigt <voigtjr@gmail.com>
2005-06-01 17:22:48 +0000

    deletes class files now

--- SoarJavaDebugger/buildDebugger.sh
+++ SoarJavaDebugger/buildDebugger.sh
@@ -2,6 +2,11 @@
 # Builds the debugger
 SOARLIB="../soar-library"
 
+for file in `find . -name "*.class"`
+do
+  rm -f $file
+done
+
 if ! javac -classpath .:${SOARLIB}/swt.jar:${SOARLIB}/sml.jar debugger/Application.java; then
   echo "Build failed."
   exit 1;
