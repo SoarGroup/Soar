63e1f4bd3bec8f3df60f8e369bcd07b4e62a59d8
Bob Marinier <bob.marinier@soartech.com>
2005-06-03 15:06:24 +0000

    delete existing class files before build

--- JavaMissionaries/buildmac.bat
+++ JavaMissionaries/buildmac.bat
@@ -1,3 +1,4 @@
+del /S /Q *.class 
 javac -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar -sourcepath src src\edu\umich\mac\MissionariesAndCannibals.java
 jar cfm ..\soar-library\mac.jar macJarManifest mac -C src .
 xcopy mac\mac.soar ..\soar-library\mac\* /Y
--- JavaTOH/buildtoh.bat
+++ JavaTOH/buildtoh.bat
@@ -1,3 +1,4 @@
+del /S /Q *.class
 javac -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar -sourcepath src src\edu\umich\toh\TowersOfHanoi.java
 jar cfm ..\soar-library\toh.jar tohJarManifest -C src .
 IF NOT "%1"=="--nopause" pause
