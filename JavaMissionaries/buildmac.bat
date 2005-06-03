59653fbdc586782adfc704c1f4a9ec9d4a152f1a
Bob Marinier <bob.marinier@soartech.com>
2005-06-03 13:37:51 +0000

    remove verbose flag from jar command

--- JavaMissionaries/buildmac.bat
+++ JavaMissionaries/buildmac.bat
@@ -1,4 +1,4 @@
 javac -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar -sourcepath src src\edu\umich\mac\MissionariesAndCannibals.java
-jar cvfm ..\soar-library\mac.jar macJarManifest mac -C src .
+jar cfm ..\soar-library\mac.jar macJarManifest mac -C src .
 xcopy mac\mac.soar ..\soar-library\mac\* /Y
 pause
--- JavaTOH/buildtoh.bat
+++ JavaTOH/buildtoh.bat
@@ -1,3 +1,3 @@
 javac -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar -sourcepath src src\edu\umich\toh\TowersOfHanoi.java
-jar cvfm ..\soar-library\toh.jar tohJarManifest -C src .
+jar cfm ..\soar-library\toh.jar tohJarManifest -C src .
 pause
