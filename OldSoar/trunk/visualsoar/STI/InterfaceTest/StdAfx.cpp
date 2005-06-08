48d2e642a3fe6a5ea15e1a8a356e711cda622cc8
Bob Marinier <bob.marinier@soartech.com>
2005-06-08 19:57:10 +0000

    removing obsolete STI stuff

--- OldSoar/trunk/visualsoar/STI/InterfaceTest/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	InterfaceTest.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
