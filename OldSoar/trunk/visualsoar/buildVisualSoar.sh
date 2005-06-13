7af717df10ebbc67b87aa7bb2982bf38b16dec6a
Unknown mccullocht <soar-umich@umich.edu>
2005-06-13 15:45:47 +0000

    Modified mac packaging

--- OldSoar/trunk/visualsoar/buildVisualSoar.sh
+++ OldSoar/trunk/visualsoar/buildVisualSoar.sh
@@ -7,8 +7,23 @@ do
   rm -f $file
 done
 
-if ! javac -classpath .:${SOARLIB}/sml.jar -sourcepath Source Source\edu\umich\visualsoar\VisualSoar.java; then
+if ! javac -classpath .:${SOARLIB}/sml.jar -sourcepath Source Source/edu/umich/visualsoar/VisualSoar.java; then
   echo "Build failed."
   exit 1;
 fi
-jar cfm ${SOARLIB}/VisualSoar.jar Source\meta-inf\manifest.mf -C Source .
+jar cfm ${SOARLIB}/VisualSoar.jar Source/meta-inf/manifest.mf -C Source .
+
+if [ `uname -s`="Darwin" ] then
+    echo "on Mac OS X, building application package for VisualSoar..."
+
+    APP_PATH=$SOARLIB/VisualSoar.app/Contents
+    mkdir -p $APP_PATH/MacOS
+    mkdir -p $APP_PATH/Resources/Java
+
+    cp VisualSoar.plist $APP_PATH/Info.plist
+    cp $SOARLIB/icons/vs.icns $APP_PATH/Resources
+    cp $SOARLIB/visualsoar.jar $APP_PATH/Resources/Java
+    cp /System/Library/Frameworks/JavaVM.framework/Resources/MacOS/JavaApplicationStub $APP_PATH/MacOS
+    chmod a+x $APP_PATH/MacOS/JavaApplicationStub
+fi
+
