4813473a1102f2b8de5663faf982954ed53155bd
Bob Marinier <bob.marinier@soartech.com>
2005-06-29 15:05:45 +0000

    added -sourcepath to javac line for consistency with linux build script

--- SoarJavaDebugger/buildDebugger.bat
+++ SoarJavaDebugger/buildDebugger.bat
@@ -1,7 +1,7 @@
 @REM Builds the debugger
 
 del /S /Q *.class 
-javac.exe -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar debugger\Application.java
+javac.exe -classpath .;..\soar-library\swt.jar;..\soar-library\sml.jar -sourcepath . debugger\Application.java
 jar.exe cfm ..\soar-library\SoarJavaDebugger.jar JarManifest .
 
 IF NOT "%1"=="--nopause" pause
