5ef873bc3d41db4ac7e2198fe6a498d88a2897f6
Bob Marinier <bob.marinier@soartech.com>
2005-06-30 20:13:40 +0000

    updated paths for OS X

--- tclenvironments/eaters/AppMain.tcl
+++ tclenvironments/eaters/AppMain.tcl
@@ -1,10 +1,10 @@
-set env(LD_LIBRARY_PATH) "/Applications/Soar/lib"
-set env(DYLID_LIBRARY_PATH) "/Applications/Soar/lib"
-set env(TCLLIBPATH) "/Applications/Soar/lib"
-set auto_path "$auto_path /Applications/Soar/lib"
+set env(LD_LIBRARY_PATH) "/Applications/Soar-8.6.1/lib"
+set env(DYLID_LIBRARY_PATH) "/Applications/Soar-8.6.1/lib"
+set env(TCLLIBPATH) "/Applications/Soar-8.6.1/lib"
+set auto_path "$auto_path /Applications/Soar-8.6.1/lib"
 
 set argv ""
-cd /Applications/Soar/soar-library
-exec wish /Applications/Soar/tclenvironments/eaters/init-eaters.tcl &
-#source /Applications/Soar/tclenvironments/eaters/init-eaters.tcl
+cd /Applications/Soar-8.6.1/soar-library
+exec wish /Applications/Soar-8.6.1/tclenvironments/eaters/init-eaters.tcl &
+#source /Applications/Soar-8.6.1/tclenvironments/eaters/init-eaters.tcl
 exit
