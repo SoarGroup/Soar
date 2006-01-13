2ec0e74a87cbc3d7ee2ca689f4eaeb5009c103ab
Jon Voigt <voigtjr@gmail.com>
2006-01-13 23:12:21 +0000

    updated windows build for new paths

--- SoarSuite/Applications/JavaMissionaries/buildmac.bat
+++ SoarSuite/Applications/JavaMissionaries/buildmac.bat
@@ -1,5 +1,5 @@
 del /S /Q *.class 
-javac -classpath .;..\soar-library\swt-windows.jar;..\soar-library\sml.jar -sourcepath src src\edu\umich\mac\MissionariesAndCannibals.java
-jar cfm ..\soar-library\mac.jar JarManifest-windows -C src .
-xcopy src\mac\mac.soar ..\soar-library\mac\* /Y
+javac -classpath .;..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar -sourcepath src src\edu\umich\mac\MissionariesAndCannibals.java
+jar cfm ..\..\SoarLibrary\bin\mac.jar JarManifest-windows -C src .
+xcopy src\mac\mac.soar ..\..\SoarLibrary\bin\mac\* /Y
 IF NOT "%1"=="--nopause" pause
