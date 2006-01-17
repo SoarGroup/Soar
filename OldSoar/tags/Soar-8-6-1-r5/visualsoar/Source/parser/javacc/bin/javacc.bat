bca73d42a363a8e619b798c667196b7a5a5cff07
Jon Voigt <voigtjr@gmail.com>
2006-01-17 19:47:38 +0000

    Moved remotely

--- OldSoar/tags/Soar-8-6-1-r5/visualsoar/Source/parser/javacc/bin/javacc.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set MBACKUP=%CLASSPATH%
-set CLASSPATH=c:\txt\UMich\soar\soar-dev\visualsoar\source\parser\javacc\JavaCC.zip
-java COM.sun.labs.javacc.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
-set CLASSPATH=%MBACKUP%
