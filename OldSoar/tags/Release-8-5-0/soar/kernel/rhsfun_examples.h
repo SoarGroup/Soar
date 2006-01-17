20211967f6864911b6999d517200017cc9fd1f93
Jon Voigt <voigtjr@gmail.com>
2006-01-17 19:47:13 +0000

    Moved remotely

--- OldSoar/tags/Release-8-5-0/soar/kernel/rhsfun_examples.h
+++
@@ -1,2 +0,0 @@
-extern void add_bot_rhs_functions();
-extern void remove_bot_rhs_functions();
--- OldSoar/tags/Release-8-5-0/soar/kernel/soarapiCallbacks.c
+++
@@ -1,8 +0,0 @@
-#include "soarkernel.h"
-#include "soarapi.h"
-
-void cb_soarResult_AppendResult(agent * the_agent, soar_callback_data data, soar_call_data call_data)
-{
-    the_agent = the_agent;
-    appendSoarResultResult((soarResult *) data, "%s", (char *) call_data);
-}
