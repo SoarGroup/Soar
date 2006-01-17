5bbe6c950549f328de8002d93382a3579b80fb9d
Jon Voigt <voigtjr@gmail.com>
2006-01-17 19:47:26 +0000

    Moved remotely

--- OldSoar/tags/Release_8-5-2/visualsoar/Source/edu/umich/visualsoar/statemap_unused/StateMapEdge.java
+++
@@ -1,9 +0,0 @@
-package edu.umich.visualsoar.statemap;
-
-import edu.umich.visualsoar.graph.*;
-
-public class StateMapEdge extends NamedEdge {
-	public StateMapEdge(Vertex v0,Vertex v1) {
-		super(v0,v1,"");
-	}
-}
