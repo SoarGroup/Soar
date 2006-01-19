36d20ccae3cdd237325ec4b0d53ee7ddbef0ac90
Bob Marinier <bob.marinier@soartech.com>
2006-01-19 17:47:34 +0000

    fixed TestJavaSML so it builds/runs under new directory structure renamed buildJava.bat/sh to build.bat/sh for consistency

--- SoarSuite/Applications/TestJavaSML/build.sh
+++ SoarSuite/Applications/TestJavaSML/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # *** MAKE SURE CHANGES TO THIS FILE ARE REFLECTED IN THE .BAT FILE
 
-SOARLIB=../../../soar-library
+SOARLIB=../../SoarLibrary
 
 javac -classpath .:${SOARLIB}/swt.jar:${SOARLIB}/sml.jar -sourcepath . Application.java
 jar cfm ${SOARLIB}/TestJavaSML.jar JarManifest .
