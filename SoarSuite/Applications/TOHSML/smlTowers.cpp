b7b0aa9f8053b95dec64bf422d4339780e13b20e
Bob Marinier <bob.marinier@soartech.com>
2006-01-20 19:26:32 +0000

    fixed towersSML for new directory structure added simple timing

--- SoarSuite/Applications/TOHSML/smlTowers.cpp
+++ SoarSuite/Applications/TOHSML/smlTowers.cpp
@@ -249,7 +249,7 @@ private:
 		assert(pAgent);
 
 		//Load agent's productions
-		pAgent->LoadProductions("../tests/test-towers-of-hanoi-SML.soar");
+		pAgent->LoadProductions("../Tests/test-towers-of-hanoi-SML.soar");
 
 		// SML uses a more explicit error model, so we can get details about what failed.
 		if (pAgent->HadError())
