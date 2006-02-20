b8b5ad9e522cca010fa3dfb77724679ee13aa00a
Bob Marinier <bob.marinier@soartech.com>
2006-02-20 18:06:27 +0000

    modified to use more import statements

--- SoarSuite/Environments/JavaTOH/src/edu/umich/toh/GameListener.java
+++ SoarSuite/Environments/JavaTOH/src/edu/umich/toh/GameListener.java
@@ -6,6 +6,8 @@
 
 package edu.umich.toh;
 
+import sml.Kernel;
+
 
 /**
  * The <code>GameListener</code> interface provides delegation for two common events
@@ -17,7 +19,7 @@ package edu.umich.toh;
  * 
  * @see Game
  */
-public interface GameListener extends sml.Kernel.SystemEventInterface {
+public interface GameListener extends Kernel.SystemEventInterface {
     /**
      * Whenever a disk is moved from one tower to another, this method is called to
      * inform this listener where the disk was moved from and where it was moved to
