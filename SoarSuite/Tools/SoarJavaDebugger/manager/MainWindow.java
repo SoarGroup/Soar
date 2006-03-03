baefbaeab0a1a4a8ab24128aa0a3f5d32fe3f267
Doug Pearson <doug@threepenny.net>
2006-03-03 05:19:13 +0000

    Extended JavaElementXML to support comments.

--- SoarSuite/Tools/SoarJavaDebugger/manager/MainWindow.java
+++ SoarSuite/Tools/SoarJavaDebugger/manager/MainWindow.java
@@ -494,6 +494,7 @@ public class MainWindow
 	{
 		// Convert to XML but without storing all of the current text in the trace etc.
 		JavaElementXML root = convertToXML(false) ;
+		root.setComment("Debugger layout file") ;
 
 		try
 		{
@@ -517,7 +518,7 @@ public class MainWindow
 	{
 		String tagName = "sash" ;
 		JavaElementXML element = new JavaElementXML(tagName) ;
-		
+				
 		// Record the class and style of this composite so we can rebuild it later
 		element.addAttribute(JavaElementXML.kClassAttribute, SashForm.class.toString()) ;
 		
