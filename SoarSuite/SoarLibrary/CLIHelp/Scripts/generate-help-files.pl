befb6959d80484801cfddf39b3542cc3a591119a
Jon Voigt <voigtjr@gmail.com>
2006-03-08 22:46:04 +0000

    html and text files updated, script updated

--- SoarSuite/SoarLibrary/CLIHelp/Scripts/generate-help-files.pl
+++ SoarSuite/SoarLibrary/CLIHelp/Scripts/generate-help-files.pl
@@ -1,38 +1,55 @@
 #!/usr/bin/perl
+# Jonathan Voigt
+# 2006
 
 use strict;
 
-open NAMES, "../command-names" or die "Could not open command-names: $!";
-my @names = <NAMES>;
+my $cliSource = "../../../Core/CLI/src/cli_Commands.cpp";
+my $commandNames = "../command-names";
+my $htmlProcess = "./html-process.pl";
 
-open POSTPROCESSFILE, "html-process.pl" or die "Could not find required file html-process.pl: $!";
+# Generate command-names from source
+open COMMANDS, "<$cliSource" or die "Could not open $cliSource: $!";
+my @commandsSource = <COMMANDS>;
+close COMMANDS;
+
+open NAMES, ">$commandNames" or die "Could not open $commandNames: $!";
+
+print "Commands found: ";
+my @commands;
+
+foreach (@commandsSource) {
+	if (/char\s+const\s*\*\s*Commands::kCLI\w+\s*=\s*"([\w-]+)"/) {
+		push(@commands, $1);
+		print $1 . " ";
+		print NAMES $1 . "\n";
+	}
+}
+print "\n";
+
+close NAMES;
+
+open POSTPROCESSFILE, "$htmlProcess" or die "Could not find required file $htmlProcess: $!";
 close POSTPROCESSFILE;
 
-#make the dir, 
-if (!mkdir "../help") {
-  if ($! ne "File exists") {
-    die "Couldn't create directory: $!";	
-  }
+foreach my $command (@commands) {
+	chomp $command;
+	print "Processing $command\n";
+
+	my $commandWiki = "../$command.wiki.html";
+	my $commandHtml = "../$command.html";
+	my $commandFile = "../$command";
+
+	`wget -q -O $commandWiki http://winter.eecs.umich.edu/soarwiki/$command`;
+
+	`$htmlProcess $commandWiki > $commandHtml`;
+	unlink $commandWiki or die "Could not remove $commandWiki: $!";
+
+	`elinks -dump -no-numbering -no-references $commandHtml > $commandFile`;
 }
-
-#print "total: " . $#links . "\n";
-for (my $i = 0; $i <= $#names; $i++) {
-  chomp @names[$i];
-  #print @names[$i] . ": " . $i . "\n";
-  #next;
-  print "Processing @names[$i]\n";
-  
-  `wget -q -O ../help/@names[$i].wiki.html http://winter.eecs.umich.edu/soarwiki/@names[$i]`;
-  
-  `./html-process.pl ../help/@names[$i].wiki.html > ../help/@names[$i].html`;
-  unlink "../help/@names[$i].wiki.html" or die "Could not remove ../help/@names[$i].wiki.html: $!";
-  
-  `elinks -dump -no-numbering -no-references ../help/@names[$i].html > ../help/@names[$i]`;
-
-  chdir "../help" or die "Could not change to ../help directory: $!";
-  `html2latex --nopar @names[$i].html`;
-  `../help-scripts/latex-post-process.pl < @names[$i].tex > @names[$i].tex.new`;
-  rename "@names[$i].tex.new", "@names[$i].tex" or die "Could not rename @names[$i].tex.new: $!";
+#  chdir "../help" or die "Could not change to ../help directory: $!";
+#  `html2latex --nopar @names[$i].html`;
+#  `../help-scripts/latex-post-process.pl < @names[$i].tex > @names[$i].tex.new`;
+#  rename "@names[$i].tex.new", "@names[$i].tex" or die "Could not rename @names[$i].tex.new: $!";
   #`pdflatex --interaction=nonstopmode @names[$i].tex`;
-  chdir "../help-scripts" or die "Could not change back to ../help-scripts directory: $!";
-}
+#  chdir "../help-scripts" or die "Could not change back to ../help-scripts directory: $!";
