b44ad66ff62747efa10a5496237a975468620c4d
Jon Voigt <voigtjr@gmail.com>
2006-03-13 18:51:05 +0000

    latest install script updates

--- SoarSuite/generate-win32.pl
+++ SoarSuite/generate-win32.pl
@@ -7,13 +7,15 @@
 
 use strict;
 use File::Find::Rule;
+use File::Copy::Recursive "dircopy";
 use File::Copy;
 
 # Variables
 my $soarurl = "https://winter.eecs.umich.edu/svn/soar/trunk/SoarSuite";
 my $distdir = "SoarSuiteDist";
 my @removed = qw/.cvsignore *.so *.so.2 *.jnilib java_swt make-mac-app.sh *.plist *.doc *.ppt/;
-my @bincopies = qw/*.dll *.exe *.jar *.lib Tcl_sml_ClientInterface/;
+my @bincopydirs = qw/Tcl_sml_ClientInterface/;
+my @bincopyfiles = qw/*.dll *.exe *.jar *.lib/;
 
 # Step 1: Rebuild everything
 system "rebuild-all.bat";
@@ -28,8 +30,14 @@ foreach (@files) {
 	unlink $_ or die "Unable to remove $_: $!";
 }
 
-# Step 4: Copy binaries over to dist
-foreach (@bincopies) {
-	copy("SoarLibrary/bin/$_", "../%distdir%/SoarLibrary/bin/$_") or die "Unable to copy $_: $!";
+# Step 4: Copy stuff over to dist
+foreach (@bincopydirs) {
+	dircopy("SoarLibrary/bin/$_", "../$distdir/SoarLibrary/bin/Tcl_sml_ClientInterface");
+}
+
+my @copylist = File::Find::Rule->file()->name( @bincopyfiles )->in('SoarLibrary/bin');
+
+foreach (@copylist) {
+	copy("$_", "../$distdir/$_") or die "Unable to copy $_: $!";
 }
 
