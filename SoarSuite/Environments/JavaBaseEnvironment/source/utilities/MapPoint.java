74882f6543f9e99ae852dcc547d15547479b9bed
Jon Voigt <voigtjr@gmail.com>
2006-03-24 22:12:15 +0000

    added map point travel method

--- SoarSuite/Environments/JavaBaseEnvironment/source/utilities/MapPoint.java
+++ SoarSuite/Environments/JavaBaseEnvironment/source/utilities/MapPoint.java
@@ -1,5 +1,7 @@
 package utilities;
 
+import simulation.*;
+
 public class MapPoint {
 	public int x;
 	public int y;
@@ -12,5 +14,39 @@ public class MapPoint {
 	public MapPoint(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	public MapPoint(MapPoint p) {
+		this.x = p.x;
+		this.y = p.y;
+	}
+	
+	public void travel(int direction) {
+		x += getXIncrement(direction);
+		y += getYIncrement(direction);
+	}
+	
+	public static int getXIncrement(int direction) {
+		switch (direction) {
+		case WorldEntity.kWestInt:
+			return -1;
+		case WorldEntity.kEastInt:
+			return 1;
+		default:
+			break;
+		}		
+		return 0;
+	}
+	
+	public static int getYIncrement(int direction) {
+		switch (direction) {
+		case WorldEntity.kNorthInt:
+			return -1;
+		case WorldEntity.kSouthInt:
+			return 1;
+		default:
+			break;
+		}		
+		return 0;
 	}
 }
--- SoarSuite/Environments/JavaBaseEnvironment/source/utilities/RelativeDirections.java
+++ SoarSuite/Environments/JavaBaseEnvironment/source/utilities/RelativeDirections.java
@@ -7,8 +7,6 @@ public class RelativeDirections {
 	public int backward;
 	public int left;
 	public int right;
-	public int xIncrement;
-	public int yIncrement;
 	
 	public void calculate(int facing) {
 			switch (facing) {
@@ -17,32 +15,24 @@ public class RelativeDirections {
 				backward = WorldEntity.kSouthInt;
 				left = WorldEntity.kWestInt;
 				right = WorldEntity.kEastInt;
-				xIncrement = 0;
-				yIncrement = -1;
 				break;
 			case WorldEntity.kEastInt:
 				forward = WorldEntity.kEastInt;
 				backward = WorldEntity.kWestInt;
 				left = WorldEntity.kNorthInt;
 				right = WorldEntity.kSouthInt;
-				xIncrement = 1;
-				yIncrement = 0;
 				break;
 			case WorldEntity.kSouthInt:
 				forward = WorldEntity.kSouthInt;
 				backward = WorldEntity.kNorthInt;
 				left = WorldEntity.kEastInt;
 				right = WorldEntity.kWestInt;
-				xIncrement = 0;
-				yIncrement = 1;
 				break;
 			case WorldEntity.kWestInt:
 				forward = WorldEntity.kWestInt;
 				backward = WorldEntity.kEastInt;
 				left = WorldEntity.kSouthInt;
 				right = WorldEntity.kNorthInt;
-				xIncrement = -1;
-				yIncrement = 0;
 				break;
 		}
 	}
