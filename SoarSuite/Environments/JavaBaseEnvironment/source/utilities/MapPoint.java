bff2ec447363c689084072d92079d96a3ce4d4f8
Jon Voigt <voigtjr@gmail.com>
2006-03-24 21:53:55 +0000

    Meant to add this last commit.

---
+++ SoarSuite/Environments/JavaBaseEnvironment/source/utilities/MapPoint.java
@@ -0,0 +1,16 @@
+package utilities;
+
+public class MapPoint {
+	public int x;
+	public int y;
+
+	public MapPoint() {
+		x = -1;
+		y = -1;
+	}
+	
+	public MapPoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
