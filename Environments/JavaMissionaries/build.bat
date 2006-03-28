a9e8f44c1e765bcd99bcff8222045e2d6c74db92
Bob Marinier <bob.marinier@soartech.com>
2006-03-28 20:40:43 +0000

    JavaMissionaries now runs from its own directory

--- Environments/JavaMissionaries/build.bat
+++ Environments/JavaMissionaries/build.bat
@@ -1,5 +1,4 @@
 del /S /Q *.class 
 javac -classpath .;..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar -sourcepath src src\edu\umich\mac\MissionariesAndCannibals.java
-jar cfm ..\..\SoarLibrary\bin\mac.jar JarManifest -C src .
-xcopy src\mac\mac.soar ..\..\SoarLibrary\bin\mac\* /Y
+jar cfm mac.jar JarManifest -C src .
 IF NOT "%1"=="--nopause" pause
