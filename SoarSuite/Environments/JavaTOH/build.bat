c949ac6f1045f002c4134a3ebc53c9c5987bb8ae
Bob Marinier <bob.marinier@soartech.com>
2006-03-28 19:36:10 +0000

    first attempt to run toh in its own directory

--- SoarSuite/Environments/JavaTOH/build.bat
+++ SoarSuite/Environments/JavaTOH/build.bat
@@ -1,5 +1,4 @@
 del /S /Q *.class
 javac -classpath .;..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar -sourcepath src src\edu\umich\toh\TowersOfHanoi.java
-jar cfm ..\..\SoarLibrary\bin\toh.jar JarManifest -C src .
-xcopy towers-of-hanoi-SML.soar ..\..\SoarLibrary\bin\* /Y
+jar cfm toh.jar JarManifest -C src .
 IF NOT "%1"=="--nopause" pause
