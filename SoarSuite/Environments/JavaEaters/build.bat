df7aec52cb496c24f4f6b6fe885afb23d3fef897
Bob Marinier <bob.marinier@soartech.com>
2006-03-28 20:52:48 +0000

    eaters can now run from its own directory

--- SoarSuite/Environments/JavaEaters/build.bat
+++ SoarSuite/Environments/JavaEaters/build.bat
@@ -3,6 +3,6 @@ del /s /Q bin\*.class
 javac -d bin -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar;..\..\SoarLibrary\bin\JavaBaseEnvironment.jar -sourcepath source source\eaters\Eaters.java
 xcopy /y source\* bin
 
-jar cfm ..\..\SoarLibrary\bin\JavaEaters.jar JarManifest -C bin .
+jar cfm JavaEaters.jar JarManifest -C bin .
 
 IF NOT "%1"=="--nopause" pause
