b767effd3a7140098a43da44b47c82caf88d7991
Bob Marinier <bob.marinier@soartech.com>
2006-03-29 14:31:46 +0000

    added alternative (prefered but not working and thus commented out) methods for setting classpath

--- SoarSuite/Environments/JavaTOH/run-toh.bat
+++ SoarSuite/Environments/JavaTOH/run-toh.bat
@@ -1,3 +1,12 @@
 set SOARLIB=../../SoarLibrary/bin
 set PATH=%SOARLIB%;%PATH%
+
+rem I prefer this method (keeps the java line clean)
+rem set CLASSPATH=%SOARLIB%/sml.jar;%SOARLIB%/swt.jar;.
+rem java -jar toh.jar
+
+rem this is an alternative method that should also work
+rem java -cp %SOARLIB%/swt.jar;%SOARLIB%/sml.jar;. -jar toh.jar
+
+rem this will work if the classpath is specified in the JarManifest
 start javaw -jar toh.jar
