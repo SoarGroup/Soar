1e102352311443299342a6f91eb9640d9b0677a7
Jon Voigt <voigtjr@gmail.com>
2006-03-30 19:37:32 +0000

    Removed support for non-square maps.

--- Environments/JavaBaseEnvironment/source/simulation/World.java
+++ Environments/JavaBaseEnvironment/source/simulation/World.java
@@ -4,15 +4,10 @@ import utilities.*;
 
 public class World {
 	protected Logger m_Logger = Logger.logger;
-	protected int m_WorldWidth;
-	protected int m_WorldHeight;
+	protected int m_WorldSize;
 
-	public int getWidth() {
-		return this.m_WorldWidth;
-	}
-	
-	public int getHeight() {
-		return this.m_WorldHeight;
+	public int getSize() {
+		return this.m_WorldSize;
 	}
 	
 	protected boolean isInBounds(MapPoint location) {
@@ -20,6 +15,6 @@ public class World {
 	}
 
 	protected boolean isInBounds(int x, int y) {
-		return (x >= 0) && (y >= 0) && (x < m_WorldWidth) && (y < m_WorldWidth);
+		return (x >= 0) && (y >= 0) && (x < m_WorldSize) && (y < m_WorldSize);
 	}
 }
