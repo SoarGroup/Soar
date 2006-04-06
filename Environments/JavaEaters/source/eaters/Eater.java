d95cf74e1085f6201c6dfc40d0fc1a5e09a4f250
Jon Voigt <voigtjr@gmail.com>
2006-04-06 15:05:01 +0000

    moved some collision code from eaters back in to base, added much collision code to tanks

--- Environments/JavaEaters/source/eaters/Eater.java
+++ Environments/JavaEaters/source/eaters/Eater.java
@@ -27,7 +27,6 @@ public class Eater extends WorldEntity {
 	private SoarCell[][] m_Cells = new SoarCell[(kEaterVision * 2 ) + 1][(kEaterVision * 2 ) + 1];
 	private boolean m_Hungry = true;
 	private boolean m_Moved = true;
-	private boolean m_Colliding = false;
 
 	class SoarCell {
 		Identifier me;
@@ -197,14 +196,6 @@ public class Eater extends WorldEntity {
 		m_Moved = true;
 	}
 
-	public boolean isColliding() {
-		return m_Colliding;
-	}
-	
-	public void setColliding(boolean colliding) {
-		m_Colliding = colliding;
-	}
-	
 }
 
 
