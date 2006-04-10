58ef05919690669a33f4a8a34ad6131800cd79e5
Jon Voigt <voigtjr@gmail.com>
2006-04-10 17:54:16 +0000

    Widened score column again for bug 749

--- Environments/JavaEaters/source/eaters/visuals/AgentDisplay.java
+++ Environments/JavaEaters/source/eaters/visuals/AgentDisplay.java
@@ -15,7 +15,7 @@ public class AgentDisplay extends Composite {
 	public static final int kAgentMapCellSize = 20;
 	static final int kTableHeight = 120;
 	static final int kNameWidth = 75;
-	static final int kScoreWidth = 50;
+	static final int kScoreWidth = 55;
 	
 	Group m_Group;
 	Logger m_Logger = Logger.logger;
