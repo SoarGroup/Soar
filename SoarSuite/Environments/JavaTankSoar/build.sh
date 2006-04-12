f7bf3821a3b9d6a30845af0459fbba0dd13c48b8
Jon Voigt <voigtjr@gmail.com>
2006-04-12 20:08:29 +0000

    build script updates so there are no errors

--- SoarSuite/Environments/JavaTankSoar/build.sh
+++ SoarSuite/Environments/JavaTankSoar/build.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 SOARLIB="../../SoarLibrary/bin"
 
-mkdir bin
-rm bin/*.class
+if [ ! -e bin ]; then
+        mkdir bin
+	mkdir bin/images
+else
+        for file in `find bin -iname *.class`
+        do
+                rm $file
+        done
+        for file in `find bin -iname *.xml`
+        do
+                rm $file
+        done
+	if [ -e bin/images ]; then
+		rm -rf bin/images
+	fi
+	mkdir bin/images
+fi
 
 if ! javac -source 1.4 -d bin -classpath ${SOARLIB}/swt.jar:${SOARLIB}/sml.jar:${SOARLIB}/JavaBaseEnvironment.jar -sourcepath source source/tanksoar/TankSoar.java ; then
 	echo "Build failed."
 	exit 1;
 fi
-cp -f source/* bin
-mkdir bin/images
+
+cp -f source/*.xml bin
 cp -f source/images/* bin/images
+
 jar cfm JavaTankSoar.jar JarManifest -C bin .
 
-# Mac stuff deleted, see JavaEaters/build.sh
