020809835723170e3fbc4493836af81ae6e7cb3a
Jon Voigt <voigtjr@gmail.com>
2006-04-18 18:45:30 +0000

    Changed modified flag to redraw flag to clearly separate its purpose from an actually modified cell, fixed tanksoar input link so that it uses the actual modification now, bug 767

--- SoarSuite/Environments/JavaBaseEnvironment/source/simulation/Cell.java
+++ SoarSuite/Environments/JavaBaseEnvironment/source/simulation/Cell.java
@@ -2,24 +2,24 @@ package simulation;
 
 public class Cell {
 	protected int m_Type;
-	protected boolean m_Modified = true;
+	protected boolean m_Redraw = true;
 	private boolean m_Collision = false;
 	
 	public void setCollision(boolean setting) {
 		m_Collision = setting;
-		m_Modified = true;
+		m_Redraw = true;
 	}
 	
 	public boolean checkCollision() {
 		return m_Collision;
 	}
 	
-	public void clearModified() {
-		m_Modified = false;
+	public void clearRedraw() {
+		m_Redraw = false;
 	}
 	
-	public boolean isModified() {
-		return m_Modified;
+	public boolean needsRedraw() {
+		return m_Redraw;
 	}
 	
 	public String toString() {
