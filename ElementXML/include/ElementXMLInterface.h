1f2630213e04aa1f32ed9d0d04dbd9572b32fdf5
Jon Voigt <voigtjr@gmail.com>
2006-05-01 17:14:47 +0000

    Preprocessing code for EXPORT symbol redone using code from SWIG, see bug 694

--- ElementXML/include/ElementXMLInterface.h
+++ ElementXML/include/ElementXMLInterface.h
@@ -23,15 +23,28 @@
 
 #include "ElementXMLHandle.h"
 
-#ifdef _WIN32
-#ifdef _USRDLL
-#define EXPORT __declspec(dllexport)
-#else
-#define EXPORT __declspec(dllimport)
-#endif	// DLL
-#else
-#define EXPORT
-#endif	// WIN32
+/* Taken from SWIG output code, because they know what they are doing */
+#if (__GNUC__ >= 4) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+#  ifndef GCC_HASCLASSVISIBILITY
+#    define GCC_HASCLASSVISIBILITY
+#  endif
+#endif
+
+#ifndef EXPORT
+# if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+#   if defined(STATIC_LINKED)
+#     define EXPORT
+#   else
+#     define EXPORT __declspec(dllexport)
+#   endif
+# else
+#   if defined(__GNUC__) && defined(GCC_HASCLASSVISIBILITY)
+#     define EXPORT __attribute__ ((visibility("default")))
+#   else
+#     define EXPORT
+#   endif
+# endif
+#endif
 
 //DJP: A quick test of statically linked version of ElementXML
 /*
