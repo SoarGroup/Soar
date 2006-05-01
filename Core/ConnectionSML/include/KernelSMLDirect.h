3e94140aba196a5b084e11cf694d795bc4992570
Jon Voigt <voigtjr@gmail.com>
2006-05-01 17:14:47 +0000

    Preprocessing code for EXPORT symbol redone using code from SWIG, see bug 694

--- Core/ConnectionSML/include/KernelSMLDirect.h
+++ Core/ConnectionSML/include/KernelSMLDirect.h
@@ -24,19 +24,28 @@
 // For test
 //#define WIN_STATIC_LINK
 
-#ifdef _WIN32
-  #ifdef _USRDLL
-    #define EXPORT __declspec(dllexport)
-  #else
-    #ifndef WIN_STATIC_LINK
-      #define EXPORT __declspec(dllimport)
-    #else
-      #define EXPORT
-    #endif	// STATIC
-  #endif	// DLL
-#else
-  #define EXPORT
-#endif	// WIN32
+/* Taken from SWIG output code, because they know what they are doing */
+#if (__GNUC__ >= 4) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+#  ifndef GCC_HASCLASSVISIBILITY
+#    define GCC_HASCLASSVISIBILITY
+#  endif
+#endif
+
+#ifndef EXPORT
+# if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+#   if defined(STATIC_LINKED)
+#     define EXPORT
+#   else
+#     define EXPORT __declspec(dllexport)
+#   endif
+# else
+#   if defined(__GNUC__) && defined(GCC_HASCLASSVISIBILITY)
+#     define EXPORT __attribute__ ((visibility("default")))
+#   else
+#     define EXPORT
+#   endif
+# endif
+#endif
 
 #ifdef __cplusplus
 extern "C" {
