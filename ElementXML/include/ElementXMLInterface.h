5a06d0380f00317be1c829fbf958352cba9972a3
Bob Marinier <bob.marinier@soartech.com>
2006-05-03 16:46:12 +0000

    put EXPORT definition in Core/shared/include/Export.h modified various projects to include this directory

--- ElementXML/include/ElementXMLInterface.h
+++ ElementXML/include/ElementXMLInterface.h
@@ -23,28 +23,8 @@
 
 #include "ElementXMLHandle.h"
 
-/* Taken from SWIG output code, because they know what they are doing */
-#if (__GNUC__ >= 4) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
-#  ifndef GCC_HASCLASSVISIBILITY
-#    define GCC_HASCLASSVISIBILITY
-#  endif
-#endif
-
-#ifndef EXPORT
-# if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
-#   if defined(STATIC_LINKED)
-#     define EXPORT
-#   else
-#     define EXPORT __declspec(dllexport)
-#   endif
-# else
-#   if defined(__GNUC__) && defined(GCC_HASCLASSVISIBILITY)
-#     define EXPORT __attribute__ ((visibility("default")))
-#   else
-#     define EXPORT
-#   endif
-# endif
-#endif
+// get definition of EXPORT
+#include "Export.h"
 
 //DJP: A quick test of statically linked version of ElementXML
 /*
