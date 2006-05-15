07863cca6964e059d60f2afa92bd36420291dfd1
Jon Voigt <voigtjr@gmail.com>
2006-05-15 14:16:36 +0000

    Unnecessary and/or outdated files deleted.

--- SoarSuite/Core/gSKI/ifnames.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Author: Jonathan Voigt
-# Date: 2005
-# Simply calls ifnames and directs output to a file
-ifnames src/*.cpp src/*.h include/*.h lib_src/pcre-4.5/*.c lib_src/pcre-4.5/*.h > ifnames.txt
