6f41110b749607a4932a9dbb43cba2df053174fe
Jon Voigt <voigtjr@gmail.com>
2006-06-21 21:38:52 +0000

    Added simple test registration process

--- tsladder/src/functions.py
+++ tsladder/src/functions.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 
-def action_link(action=None, text=None):
+import smtplib
+
+def action_link(action=None):
+    if action == None:
+        return "index.cgi"
+    return "index.cgi?action=" + action
+
+def action_anchor(action=None, text=None):
     if (action != None) & (text == None):
         text = action
     elif text == None:
-        return "<a href='index.cgi'>Home</a>"
-    return "<a href='index.cgi?action=" + action + "'>" + text + "</a>"
+        text = "Home"
+    return "<a href='" + action_link(action) + "'>" + text + "</a>"
 
 def welcome_page(userid=None):
     if userid == None:
@@ -14,10 +21,45 @@ def welcome_page(userid=None):
     print "<p>Welcome, " + userid + ".</p>"
     
     if guest == True:
-        print action_link(action="login", text="Log in") + " or " + action_link(action="register") + "."
+        print action_anchor(action="login", text="Log in") + " or " + action_anchor(action="register") + "."
     else:
-        print action_link(action="logout", text="Log out") + "."
+        print action_anchor(action="logout", text="Log out") + "."
     return
+
+def register_page(userid=None, email=None):
+    print "<form method='POST' action='" + action_link("register") + "'>"
+    
+    print "User ID: <input type='text' name='userid'",
+    if userid != None:
+        print "value='" + userid + "'",
+    print " />"
+
+    print "Email address (required, must be valid, will not be published): <input type='text' name='email'",
+    if email != None:
+        print "value='" + email + "'",
+    print " />"
+    
+    print "<input type='hidden' name='confirm' />"
+    print "<input type='submit' value='Register' />"
+    print "</form>"
+    
+def send_confirmation(userid=None, email=None):
+    if (email == None) | (userid == None):
+        return False
+    
+    message = "From: Java TankSoar Ladder <tsladder@winter.eecs.umich.edu>\r\n"
+    message += "To: " + userid + " <" + email + ">\r\n"
+    message += "Subject: Registration\r\n\r\n"
+    message += "Confirmation code: abc123"
+    
+    server = smtplib.SMTP('localhost')
+    server.sendmail(from_addr="tsladder@winter.eecs.umich.edu", to_addrs=email, msg=message)
+
+def confirm_page():
+    print "<form method='POST' action='" + action_link("confirm") + "'>"
+    print "Enter confirmation code from email: <input type='text' name='code' />"
+    print "<input type='submit' value='Confirm' />"
+    print "</form>"
 
 def upload_page():
     print "<form method='POST' enctype='multipart/form-data' action='index.cgi'>"
--- tsladder/src/index.py
+++ tsladder/src/index.py
@@ -7,24 +7,68 @@ import cgitb; cgitb.enable()
 import functions
 
 print "Content-type: text/html\n\n"
-print "<h1>Java TankSoar Ladder</h1>"
+
+title = "Java TankSoar Ladder"
+
+print "<head><title>" + title + "</title></head>"
+print "<h1>" + title + "</h1>"
 
 form = cgi.FieldStorage()
 
 if not (form.has_key("action")):
 	functions.welcome_page()
+	raise SystemExit
 
-elif form["action"].value == "upload":
-	if form["userid"] == None:
-		print "<font color='red'>You must enter a User ID</font>"
-		upload_page()
-	elif form["tankid"] == None: 
-		print "<h1>User ID:", form["userid"], "</h1>"
-		save_tank()
-		print "<h1>Saved tank: ", form["upfile"].filename, "</h1>"
+action = form["action"].value
+
+if action == "login":
+	pass
+
+elif action == "logout":
+	pass
+
+elif action == "register":
+	if form.has_key("confirm"):
+		userid = None
+		if form.has_key("userid"):
+			userid = form["userid"]
+		
+		email = None
+		if form.has_key("email"):
+			email = form["email"]
+			
+		# TODO: Make sure userid is unique and legal
+		# if not legal: functions.register_page(userid, email)
+		# Send confirmation code to email address
+		if not functions.send_confirmation(userid, email):
+			print "<p><font color='red'>Sending of confirmation email failed. Check email address.</p>"
+			functions.register_page(userid, email)
+		else:
+			functions.confirm_page()
+	else:
+		functions.register_page()
+		
+elif action == "confirm":
+	if form.has_key("code"):
+		code = form["code"]
+		if code == "abc123":
+			print "<p>Confirmation successful.</p>"
+			functions.welcome_page()
+		else:
+			print "<p><font color='red'>Confirmation unsuccessful.</p>"
+	else:
+		functions.welcome_page()
 		
 else:
-	print "<font color='red'>Unknown action: ", form["action"], "</font>"
+	print "<p><font color='red'>Unknown action: ", action, "</font></p>"
+	functions.welcome_page()
 
-raise SystemExit
-
+#
+#if action == "upload":
+#	if form["userid"] == None:
+#		print "<font color='red'>You must enter a User ID</font>"
+#		upload_page()
+#	elif form["tankid"] == None: 
+#		print "<h1>User ID:", form["userid"], "</h1>"
+#		save_tank()
+#		print "<h1>Saved tank: ", form["upfile"].filename, "</h1>"
