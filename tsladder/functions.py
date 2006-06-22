d2e1cb323b178b37cf79fa57105ad7cadfe1a556
Jon Voigt <voigtjr@gmail.com>
2006-06-22 20:29:38 +0000

    Server side tweaks.

--- tsladder/functions.py
+++ tsladder/functions.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
 
+import sys
 from configobj import ConfigObj
 from pathutils import *
 from cgiutils import *
 
-def welcome_page():
-    newwelcomepage = readfile('templates/header.txt')
-    newwelcomepage += "<p>Don't touch anything, winter could explode.</p>"
-    newwelcomepage = readfile('templates/footer.txt')
-    print serverline + "\n" + newwelcomepage
+def account_links(userconfig):
+	ret = "<p><a href='/tsladder/index.cgi?login=logout'>Log out</a> | <a href='/tsladder/index.cgi?login=editaccount'>Edit account</a>"
+	if (userconfig != None) & (userconfig['admin'] == "3"):
+        	ret += " | <a href='/tsladder/index.cgi?login=admin'>Admin</a>"
+	ret += "</p>"
+	return ret
+	
+def welcome_page(action, userconfig):
+    page = readfile('templates/header.txt')
+    page += account_links(userconfig)
+    page += "<p>Don't touch anything, winter could explode.</p>"
+    page += readfile('templates/footer.txt')
+    print "Content-type: text/html\n"
+    print page
     sys.exit()
 
 def upload_page(action, userconfig):
--- tsladder/index.py
+++ tsladder/index.py
@@ -19,25 +19,10 @@ action, userconfig = login(theform, userdir)
 
 from functions import *
 
-
-print "Content-type: text/html\n\n"
-
-print "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">"
-print "<html lang='en'><head><title>JTSL: Home</title><link href='/tsladder/jtsl.css' rel='stylesheet' type='text/css'/></head>"
-print "<body><h1>Java TankSoar Ladder</h1>"
-print "<h2>Welcome " + userconfig['username'] + "</h2>"
-
-print "<p><a href='/tsladder/index.cgi?login=logout'>Log out</a> | <a href='/tsladder/index.cgi?login=editaccount'>Edit account</a>",
-if userconfig['admin'] == "3":
-	print " | <a href='/tsladder/index.cgi?login=admin'>Admin</a>"
-print "</p>"
-
 form = cgi.FieldStorage()
 
 if action == None:
-	welcome_page()
-
-print "<p>Action is '" + action + "'</p>"
+	welcome_page(action, userconfig)
 
 if action == "upload":
 	upload_page(action, userconfig)
