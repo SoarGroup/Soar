a6c2a62934c92e8ad8f95d6c5f80690d70df525c
Jon Voigt <voigtjr@gmail.com>
2006-06-23 19:48:43 +0000

    simple uploading sort of works

--- tsladder/functions.py
+++ tsladder/functions.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import shutil
 from configobj import ConfigObj
 from pathutils import *
 from cgiutils import *
@@ -58,21 +59,21 @@ def delete_tank(action, userconfig, tank):
 	#FIXME: currenly no way of removing tanks from file!
 	#userconfig.remove(tank)
 	top = "tanks/" + userconfig['username'] + "/" + tank
-	for root, dirs, files in os.walk(top, topdown=False):
-		for name in files:
-			os.remove(os.path.join(root, name))
-		for name in dirs:
-			os.rmdir(os.path.join(root, name))
-	os.rmdir(top)
+	shutil.rmtree(top)
 	userconfig.write()
+	return
+
+found = False
+def find(arg, dirname, names):
+	if arg in names:
+		found = True
+	return
 	
 def upload_page(action, userconfig, tankname, tankfilename, tankfile, source):
 	#verify no duplicate tank name exists
-	duplicate = True
-	try:
-		userconfig['tanks'].index(tankname)
-	except ValueError:
-		duplicate = False
+	duplicate = False
+	if tankname in userconfig['tanks']:
+		duplicate = True
 
 	page = readfile('templates/header.html')
 	page += account_links(userconfig)
@@ -81,14 +82,31 @@ def upload_page(action, userconfig, tankname, tankfilename, tankfile, source):
 		page += "<p><font color='red'>Tank name '" + tankname + "' already exists, hit back and select a new tank name.</font></p>"
 	else:
 		#create the tank directory
-		os.makedirs("tanks/" + userconfig['username'] + "/" + tankname)
+		tankdir = "tanks/" + userconfig['username'] + "/" + tankname
+		os.makedirs(tankdir)
 
 		#save the zip file
-		file = open("tanks/" + userconfig['username'] + "/" + tankname + "/" + tankfilename, 'w')
+		file = open(tankdir + "/" + tankfilename, 'w')
 		file.write(tankfile)
 		file.close()
 
-		#update the user config
+		#unzip the file
+		cwd = os.getcwd()
+		os.chdir(tankdir)
+		os.system("/usr/bin/unzip " + tankfilename)
+		#args = ['/usr/bin/unzip', tankfilename]
+		#os.spawnv(os.P_WAIT, '/usr/bin/unzip', args)
+		os.chdir(cwd)
+		
+		#find the source file
+		#found = False
+		#os.path.walk(tankdir, find, source)
+		#if not found:
+	#		shutil.rmtree(tankdir)
+#
+#			page += "<p><font color='red'>Could not find source file</font></p>"
+#		else:
+			#update the user config
 		userconfig['tanks'].append(tankname)
 		userconfig[tankname] = {}
 		userconfig[tankname]['source'] = source
@@ -96,6 +114,8 @@ def upload_page(action, userconfig, tankname, tankfilename, tankfile, source):
 
 		page += "<p>Tank name: " + tankname + "</p>"
 		page += "<p>Tank filename: " + tankfilename + "</p>"
+			
+		page += "<p><a href='" + thisscript + "?action=managetanks'>Manage my tanks</a></p>"
 		
 	page += readfile('templates/footer.html')
 
