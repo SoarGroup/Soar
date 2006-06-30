fa76f2f81c62480a65d72f23cb1bf35439a1ba94
Jon Voigt <voigtjr@gmail.com>
2006-06-30 20:00:06 +0000

    tanksoar logging overhauled

--- Environments/JavaBaseEnvironment/source/utilities/JonsFormatter.java
+++ Environments/JavaBaseEnvironment/source/utilities/JonsFormatter.java
@@ -1,21 +1,20 @@
 package utilities;
 
-import java.text.DateFormat;
+import java.text.*;
 import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class JonsFormatter extends Formatter {
-
+	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
+	
 	public JonsFormatter() {
 		super();
 	}
 
 	public String format(LogRecord record) {
 		Date d = new Date(record.getMillis());
-		String output = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG).format(d);
-		output += " ";
-		output += record.getLoggerName();
+		String output = format.format(d);
 		output += " ";
 		output += record.getLevel().getName();
 		output += " ";
