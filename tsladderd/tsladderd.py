c399156e52ba2a90d630842ce62868105ecbecc4
Jon Voigt <voigtjr@gmail.com>
2006-07-11 19:15:08 +0000

    more updates and tweaks

--- tsladderd/tsladderd.py
+++ tsladderd/tsladderd.py
@@ -10,133 +10,166 @@ import logging
 from pathutils import *
 import re
 
-pid = os.fork()
-if pid != 0:
-	pidfile = file('tsladderd.pid', 'w')
-	pidfile.write(str(pid))
-	pidfile.close()
-	sys.exit(0)
-
-logging.basicConfig(level=logging.DEBUG, 
-		    format='%(asctime)s %(levelname)s %(message)s',
-		    filename='tsladderd.log',
-		    filemode='w')
-
-logging.info('tsladderd started (%d).', pid)
-
-db = MySQLdb.connect(host="localhost", user="tsladder", passwd="75EbRL", db="tsladder")
-cursor = db.cursor()
-logging.info('Connected to database.')
-
-sleepytime = 5
-matchNumber = 1
-
-while True:
-	cursor.execute("SELECT * FROM tanks ORDER BY RAND(NOW()) LIMIT 2")
+def new_round(cursor):
+	cursor.execute("SELECT * FROM tanks ORDER BY RAND(NOW())")
 	if int(cursor.rowcount) < 2:
-		logging.warning('There is only one tank in the database.')
-		logging.info("Sleeping for a bit.")
-		time.sleep(sleepytime)
-		continue
+		return None
+	return cursor.fetchall()
 
-	redtank = None
-	reduser = None
-	bluetank = None
-	blueuser = None
-
-	for tank in cursor.fetchall():
-		cursor.execute("UPDATE tanks SET fighting=1 WHERE tankid=%s", (tank[0],))
-		cursor.execute("SELECT username FROM users WHERE userid=%s", (tank[1],))
-		username = cursor.fetchone()[0]
-		status = "active"
-		color = None
-		if not tank[8]:
-			status = "inactive"
+def fight(redtank, bluetank, round, matchNumber):
+	cursor.execute("UPDATE tanks SET fighting=1 WHERE tankid=%s", (redtank[0],))
+	cursor.execute("UPDATE tanks SET fighting=1 WHERE tankid=%s", (bluetank[0],))
 	
-		if redtank == None:
-			color = "Red"
-			redtank = tank
-			reduser = username
-		else:
-			color = "Blue"
-			bluetank = tank
-			blueuser = username
+	cursor.execute("SELECT username FROM users WHERE userid=%s", (redtank[1],))
+	reduser = cursor.fetchone()[0]
+	logging.info("red: %s.%s: %d points (%d-%d-%d)", reduser, redtank[2], redtank[11], redtank[3], redtank[4], redtank[5]) 
 
-		logging.info("%s: %s.%s: (%d-%d-%d) (%s)", 
-			     color, username,
-			     tank[2], tank[3], tank[4], tank[5], status) 
+	cursor.execute("SELECT username FROM users WHERE userid=%s", (bluetank[1],))
+	blueuser = cursor.fetchone()[0]
+	logging.info("blue: %s.%s: %d points (%d-%d-%d)", blueuser, bluetank[2], bluetank[11], bluetank[3], bluetank[4], bluetank[5]) 
 
 	settings = readfile("templates/settings.xml")
 
 	settings = settings.replace('**red tank name**', reduser + "." + redtank[2])
 	settings = settings.replace('**blue tank name**', blueuser + "." + bluetank[2])
-	settings = settings.replace('**red tank source**', "tanks/" + reduser + "/" + redtank[2] + "/" + redtank[7])
-	settings = settings.replace('**blue tank source**', "tanks/" + blueuser + "/" + bluetank[2] + "/" + bluetank[7])
+	settings = settings.replace('**red tank source**', "tanks/" + reduser + "/" + redtank[2] + "/" + redtank[8])
+	settings = settings.replace('**blue tank source**', "tanks/" + blueuser + "/" + bluetank[2] + "/" + bluetank[8])
 
-	settingsfile = open("JavaTankSoar/" + str(matchNumber) + ".xml", 'w')
+	settingsfile = open("JavaTankSoar/tanksoar-default-settings.xml", 'w')
 	settingsfile.write(settings)
 	settingsfile.close()
 
-	logging.info("Starting match " + str(matchNumber) + ".")
+	logging.info("Starting round " + str(round) + ", match " + str(matchNumber) + ".")
 	
 	cwd = os.getcwd()
 	os.chdir('JavaTankSoar')
-	#if os.path.exists("TankSoarLog.txt"):
-	#	os.unlink("TankSoarLog.txt")
-	os.system('java -jar JavaTankSoar.jar -quiet -notrandom -log tsladder-logs/' + str(matchNumber) + '.txt -settings ' + str(matchNumber) + '.xml')
+	#os.system('java -jar JavaTankSoar.jar -quiet -notrandom')
+	os.system('java -jar JavaTankSoar.jar -quiet -notrandom -log tsladder-logs/' + str(matchNumber) + '.txt')
 
-	logging.info("Match " + str(matchNumber) + " finished.")
+	logging.info("Round " + str(round) + ", match " + str(matchNumber) + " finished.")
 	
 	cursor.execute("UPDATE tanks SET fighting=0 WHERE tankid=%s", (redtank[0],))
 	cursor.execute("UPDATE tanks SET fighting=0 WHERE tankid=%s", (bluetank[0],))
 	
 	output = file("tsladder-logs/" + str(matchNumber) + ".txt");
-	matchNumber += 1;
+	#output = file("TankSoarLog.txt")
 	matched_something = False
+	finished = False
+	redid = 0
+	blueid = 0
+	winnerid = 0
 	for line in output.readlines():
 		match = re.match(r".+ INFO (.+): (-?\d+) \((\w+)\)", line)
 		if match == None:
 			continue
 		matched_something = True
-		logging.debug("Matched this line: %s", line)
+		#logging.debug("Matched this line: %s", line)
 		tankname, score, status = match.groups()
-		score = int(score)
-		if status == "winner":
-			sql = "UPDATE tanks SET wins=wins+1 WHERE tankid=%s";
-		elif status == "loser":
-			sql = "UPDATE tanks SET losses=losses+1 WHERE tankid=%s";
-		elif status == "draw":
-			sql = "UPDATE tanks SET draws=draws+1 WHERE tankid=%s";
-		else:
-			logging.warning("Unknown status %s for %s!", status, tankname)
-			continue
 
 		tankid = None
 		if tankname == reduser + "." + redtank[2]:
 			tankid = redtank[0]
+			redid = tankid
 		else:
 			tankid = bluetank[0]
+			blueid = tankid
+
+		score = int(score)
+		matchpoints = 0
+		if status == "winner":
+			matchpoints = 2
+			sql = "UPDATE tanks SET wins=wins+1 WHERE tankid=%s"
+			winnerid = tankid
+		elif status == "loser":
+			sql = "UPDATE tanks SET losses=losses+1 WHERE tankid=%s"
+		elif status == "draw":
+			matchpoints = 1
+			sql = "UPDATE tanks SET draws=draws+1 WHERE tankid=%s"
+		else:
+			logging.warning("Unknown status %s for %s!", status, tankname)
+			continue
+
+		if score >= 100:
+			finished = True
+			matchpoints += 1
+
 		cursor.execute(sql, (tankid,))
-		cursor.execute("SELECT pointspermatch, wins, losses, draws FROM tanks WHERE tankid=%s", (tankid,))
-		pointspermatch, wins, losses, draws = cursor.fetchone()
+		cursor.execute("SELECT pointspermatch, wins, losses, draws, ladderpoints FROM tanks WHERE tankid=%s", (tankid,))
+		pointspermatch, wins, losses, draws, ladderpoints = cursor.fetchone()
+		ladderpoints += matchpoints
 		matches = int(wins) + int(losses) + int(draws)
 		pointspermatch = ((pointspermatch*(matches-1)) + score) / matches
-		cursor.execute("UPDATE tanks SET pointspermatch=%s WHERE tankid=%s", (pointspermatch, tankid))
-		logging.info("%s (%s) scored %d points (%s), %f pointspermatch in %d matches.", tankname, tankid, score, status, pointspermatch, matches)
-		
+		cursor.execute("UPDATE tanks SET pointspermatch=%s, ladderpoints=%s WHERE tankid=%s", (pointspermatch, ladderpoints, tankid))
+		logging.info("%s (%s) scored %d points (%s), %f pointspermatch in %d matches, ladder points increase by %d to %d.", tankname, tankid, score, status, pointspermatch, matches, matchpoints, ladderpoints)
 		
 	output.close()
+	os.chdir('tsladder-logs')
+	os.system("gzip -f " + str(matchNumber) + ".txt")
 	os.chdir(cwd)
 
 	if not matched_something:
 		logging.warning("Failed to match status lines! Ignoring match!")
+	else:
+		cursor.execute("INSERT INTO matches (matchid, red, blue, winner) VALUES(%s, %s, %s, %s)", (matchNumber, redid, blueid, winnerid))
+		logging.info("Added match record %s", matchNumber)
+		if finished:
+			logging.info("The match was finished.")
+		else:
+			logging.info("The match was not finished.")
+			cursor.execute("UPDATE tanks SET unfinished=unfinished+1 WHERE tankid=%s", (redid,))
+			cursor.execute("UPDATE tanks SET unfinished=unfinished+1 WHERE tankid=%s", (blueid,))
 
-	
-	
-	logging.info("Sleeping for a bit.")
-	time.sleep(sleepytime)
+	return
 
-logging.info('Shutting down.')
-logging.shutdown()
-sys.exit(1)
+if __name__ == "__main__":
+
+	if len(sys.argv) > 1 and sys.argv[1].find("daemon") != -1:
+		pid = os.fork()
+		if pid != 0:
+			pidfile = file('tsladderd.pid', 'w')
+			pidfile.write(str(pid))
+			pidfile.close()
+			sys.exit(0)
+
+	logging.basicConfig(level=logging.DEBUG, 
+			    format='%(asctime)s %(levelname)s %(message)s',
+			    filename='tsladderd.log',
+			    filemode='w')
+
+	db = MySQLdb.connect(host="localhost", user="tsladder", passwd="75EbRL", db="tsladder")
+	cursor = db.cursor()
+	logging.info('Connected to database.')
+
+	sleepytime = 5
+	round = 0
+	matchNumber = 0
+	
+	while True:
+		#Old select two at random code
+		#cursor.execute("SELECT * FROM tanks ORDER BY RAND(NOW()) LIMIT 2")
+		
+		round += 1
+		tanks = new_round(cursor)
+		if tanks == None:
+			logging.warning('There is only one tank in the database.')
+			logging.info("Sleeping for a bit.")
+			time.sleep(sleepytime)
+			continue
+
+		j = 0
+		for i in range(len(tanks)):
+			j = i + 1
+			while j < len(tanks):
+				redtank = tanks[i]
+				bluetank = tanks[j]
+				matchNumber += 1
+				fight(tanks[i], tanks[j], round, matchNumber)
+				j += 1
+				logging.info("Sleeping for a bit.")
+				time.sleep(sleepytime)
+
+	logging.info('Shutting down.')
+	logging.shutdown()
+	sys.exit(1)
+
 
