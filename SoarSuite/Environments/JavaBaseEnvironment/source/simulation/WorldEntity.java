5af1fb46884b50b515f7ae465a01b75eafa27853
Jon Voigt <voigtjr@gmail.com>
2006-07-26 18:34:44 +0000

    Major data structure changes to base environment, tanksoar and eaters.

--- SoarSuite/Environments/JavaBaseEnvironment/source/simulation/WorldEntity.java
+++ SoarSuite/Environments/JavaBaseEnvironment/source/simulation/WorldEntity.java
@@ -2,52 +2,23 @@ package simulation;
 
 import java.util.logging.*;
 
-import org.eclipse.swt.graphics.*;
-
 import sml.*;
 import utilities.*;
 
 public class WorldEntity {
-	public final static String kNorth = "north";
-	public final static String kEast = "east";
-	public final static String kSouth = "south";
-	public final static String kWest = "west";
-	
-	public final static int kNorthInt = 1;
-	public final static int kEastInt = 2;
-	public final static int kSouthInt = 4;
-	public final static int kWestInt = 8;
-	
-	public static String directionToString(int direction) {
-		switch (direction) {
-			default:
-				break;
-			case kNorthInt:
-				return kNorth;
-			case kEastInt:
-				return kEast;
-			case kSouthInt:
-				return kSouth;
-			case kWestInt:
-				return kWest;
-		}
-		return null;
-	}
-	
 	private static Logger logger = Logger.getLogger("simulation");
 	protected Agent m_Agent;	
-	protected Color m_Color;
-	protected String m_Facing;
+	protected org.eclipse.swt.graphics.Color m_Color;
 	protected int m_FacingInt;
 	
 	private String m_Name;
 	private int m_Points = 0;
-	private MapPoint m_Location;
+	private java.awt.Point m_Location;
 	private String m_ColorString;
 	private String m_Productions;
 	private boolean m_Colliding = false;
 
-	public WorldEntity(Agent agent, String productions, String color, MapPoint location) {
+	public WorldEntity(Agent agent, String productions, String color, java.awt.Point location) {
 		m_Agent = agent;
 		m_Location = location;
 		m_ColorString = color;
@@ -99,7 +70,7 @@ public class WorldEntity {
 		m_Agent.InitSoar();
 	}
 	
-	public MapPoint getLocation() {
+	public java.awt.Point getLocation() {
 		return m_Location;
 	}
 	
@@ -108,30 +79,10 @@ public class WorldEntity {
 		m_Points += delta;
 		logger.info(getName() + " score: " + Integer.toString(previous) + " -> " + Integer.toString(m_Points) + " (" + comment + ")");
 	}
-	public void setLocation(MapPoint location) {
+	public void setLocation(java.awt.Point location) {
 		m_Location = location;
 	}
 	
-	public String getFacing() {
-		return m_Facing;
-	}
-	
-	protected void setFacingInt() {
-		if (m_Facing == null) {
-			m_FacingInt = 0;
-		}
-		
-		if (m_Facing.equalsIgnoreCase(WorldEntity.kNorth)) {
-			m_FacingInt = kNorthInt;
-		} else if (m_Facing.equalsIgnoreCase(WorldEntity.kSouth)) {
-			m_FacingInt = kSouthInt;
-		} else if (m_Facing.equalsIgnoreCase(WorldEntity.kEast)) {
-			m_FacingInt = kEastInt;
-		} else if (m_Facing.equalsIgnoreCase(WorldEntity.kWest)) {
-			m_FacingInt = kWestInt;
-		}					
-	}
-
 	public int getFacingInt() {
 		return m_FacingInt;
 	}
