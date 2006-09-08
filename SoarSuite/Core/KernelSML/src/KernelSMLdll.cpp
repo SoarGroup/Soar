ce00330908a1628f61ec8481960978abcbb3e6e8
Jon Voigt <voigtjr@gmail.com>
2006-09-08 19:01:54 +0000

    Quick fixes to portability.h breaks of windows build.

--- SoarSuite/Core/KernelSML/src/KernelSMLdll.cpp
+++ SoarSuite/Core/KernelSML/src/KernelSMLdll.cpp
@@ -1,7 +1,7 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif // HAVE_CONFIG_H
-#include <portability.h>
+//FIXME: #include <portability.h>
 
 /////////////////////////////////////////////////////////////////
 // KernelSMLdll.cpp : Defines the entry point for the DLL application.
