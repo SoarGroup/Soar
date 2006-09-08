01d27bd9a3d7107794867ade084dfc7b8b4c1924
Jon Voigt <voigtjr@gmail.com>
2006-09-08 18:38:54 +0000

    Quick fixes to portability.h breaks of windows build.

--- SoarSuite/Core/ConnectionSML/src/sock_SocketLib.cpp
+++ SoarSuite/Core/ConnectionSML/src/sock_SocketLib.cpp
@@ -1,7 +1,7 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif // HAVE_CONFIG_H
-#include <portability.h>
+//FIXME: #include <portability.h>
 
 /////////////////////////////////////////////////////////////////
 // SocketLib class
