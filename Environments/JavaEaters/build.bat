31542a0092a86edc7bc8d2a91fa4b96f1b4f3abc
Bob Marinier <bob.marinier@soartech.com>
2006-09-28 17:16:41 +0000

    attempt to clean up batch files

--- Environments/JavaEaters/build.bat
+++ Environments/JavaEaters/build.bat
@@ -3,11 +3,46 @@ rem %2 is an optional command line argument specifying the java bin directory to
 rem NOTE: if this directory is specified, the trailing slash must be included!
 rem Also, if there are spaces in the path, it must be wrapped in quotes
 
-mkdir bin
-del /s /Q bin\*.class
-%2javac -source 1.4 -d bin -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar;..\..\SoarLibrary\bin\JavaBaseEnvironment.jar -sourcepath source source\eaters\Eaters.java
-xcopy /y source\* bin
+set SOARBIN=..\..\SoarLibrary\bin
 
-%2jar cfm JavaEaters.jar JarManifest -C bin .
+IF NOT EXIST %SOARBIN%\swt.jar GOTO no_swt
+IF NOT EXIST %SOARBIN%\sml.jar GOTO no_sml
+IF NOT EXIST %SOARBIN%\JavaBaseEnvironment.jar GOTO no_JBE
 
-IF NOT "%1"=="--nopause" pause
+echo ------========Setting up tmp dir========---------
+IF EXIST tmp rmdir /S /Q tmp
+mkdir tmp
+mkdir tmp\images
+xcopy /q /y source\* tmp
+
+@echo ----------=========Compiling=========----------
+%2javac -source 1.4 -d tmp -classpath %SOARBIN%\swt.jar;%SOARBIN%\sml.jar;%SOARBIN%\JavaBaseEnvironment.jar -sourcepath source source\eaters\Eaters.java
+
+@echo ----------=========Jarring...=========-----------
+xcopy /y source\* tmp
+%2jar cfm JavaEaters.jar JarManifest -C tmp .
+
+GOTO success
+
+:no_swt
+echo ERROR:  %SOARBIN%\swt.jar is missing.  
+echo         Did you remember to download it from: http://winter.eecs.umich.edu/jars/
+echo Build Stopped
+goto end
+
+:no_sml
+echo ERROR:  %SOARBIN%\sml.jar is missing.  
+echo Build Stopped
+goto end
+
+:no_JBE
+echo ERROR:  %SOARBIN%\JavaBaseEnvironment.jar is missing.  
+echo           Did you remember to build it?
+echo Build Stopped
+goto end
+
+:success
+echo Build Complete
+
+:end
+IF NOT "%1" == "--nopause" pause
--- Environments/JavaTankSoar/build.bat
+++ Environments/JavaTankSoar/build.bat
@@ -3,38 +3,40 @@ rem %2 is an optional command line argument specifying the java bin directory to
 rem NOTE: if this directory is specified, the trailing slash must be included!
 rem Also, if there are spaces in the path, it must be wrapped in quotes
 
-IF NOT EXIST ..\..\SoarLibrary\bin\swt.jar GOTO no_swt
-IF NOT EXIST ..\..\SoarLibrary\bin\sml.jar GOTO no_sml
-IF NOT EXIST ..\..\SoarLibrary\bin\JavaBaseEnvironment.jar GOTO no_JBE
+set SOARBIN=..\..\SoarLibrary\bin
 
-IF NOT EXIST bin mkdir bin
+IF NOT EXIST %SOARBIN%\swt.jar GOTO no_swt
+IF NOT EXIST %SOARBIN%\sml.jar GOTO no_sml
+IF NOT EXIST %SOARBIN%\JavaBaseEnvironment.jar GOTO no_JBE
 
-%2javac -source 1.4 -d bin -classpath ..\..\SoarLibrary\bin\swt.jar;..\..\SoarLibrary\bin\sml.jar;..\..\SoarLibrary\bin\JavaBaseEnvironment.jar -sourcepath source source\tanksoar\TankSoar.java
+echo ------========Setting up tmp dir========---------
+IF EXIST tmp rmdir /S /Q tmp
+mkdir tmp
+mkdir tmp\images
+xcopy /q /y source\* tmp
+xcopy /q /y /s source\images\* tmp\images\
 
-xcopy /q /y source\* bin
-IF NOT EXIST bin\images mkdir bin\images
-xcopy /q /y /s source\images\* bin\images
+@echo ----------=========Compiling into tmp dir=========----------
+%2javac -source 1.4 -d tmp -classpath %SOARBIN%\swt.jar;%SOARBIN%\sml.jar;%SOARBIN%\JavaBaseEnvironment.jar -sourcepath source source\tanksoar\TankSoar.java
 
-@echo on
-%2jar cfm JavaTankSoar.jar JarManifest -C bin .
-@echo off
-
+@echo ----------=========Jarring contents of tmp dir=========-----------
+%2jar cfm JavaTankSoar.jar JarManifest -C tmp .
 
 GOTO success
 
 :no_swt
-echo ERROR:  ..\..\SoarLibrary\bin\swt.jar is missing.  
+echo ERROR:  %SOARBIN%\swt.jar is missing.  
 echo         Did you remember to download it from: http://winter.eecs.umich.edu/jars/
 echo Build Stopped
 goto end
 
 :no_sml
-echo ERROR:  ..\..\SoarLibrary\bin\sml.jar is missing.  
+echo ERROR:  %SOARBIN%\sml.jar is missing.  
 echo Build Stopped
 goto end
 
 :no_JBE
-echo ERROR:  ..\..\SoarLibrary\bin\JavaBaseEnvironment.jar is missing.  
+echo ERROR:  %SOARBIN%\JavaBaseEnvironment.jar is missing.  
 echo           Did you remember to build it?
 echo Build Stopped
 goto end
