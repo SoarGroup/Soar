5e09269e9a3f87c55189c16c681ecff323e6a018
Jon Voigt <voigtjr@gmail.com>
2006-10-05 15:38:13 +0000

    added this here because I'm not sure where I'm going to put it yet

---
+++ tsladderd/tsladderz.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#create the rete
+
+import socket
+import cPickle
+import time
+
+def send(message):
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	response = None
+	try:
+		client.connect(('localhost', 54423))
+		client.sendall(cPickle.dumps(message))
+		client.shutdown(1) #socket.SHUT_WR)
+
+		pickled_response = ""
+		while True:
+			data = client.recv(1024)
+			if not data:
+				break
+			pickled_response += data
+		response = cPickle.loads(pickled_response)
+
+	except socket.error, e:
+		print 'Error:', e
+		raise
+
+	client.close()
+	return response
+
+def create_rete(soar_rules_zip, primary_file):
+	response = send(('rete', soar_rules_zip, primary_file))
+	return response
+
+def stop_tournament(tournament_name):
+	response = send(('stop', tournament_name))
+	return response
+	
+def start_tournament(tournament_name, max_duration):
+	response = send(('start', tournament_name, max_duration))
+	return response
+
+if __name__ == '__main__':
+	print 'sending start'
+	print start_tournament('elo', 600)
+	print 'sleeping'
+	time.sleep(5)
+	print 'sending stop'
+	print stop_tournament('elo')
