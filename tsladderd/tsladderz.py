429d6fa0ec73a0b24adde66edbf74f0309c2bc5d
Jon Voigt <voigtjr@gmail.com>
2006-10-25 17:31:59 +0000

    Latest ladder updates

--- tsladderd/tsladderz.py
+++ tsladderd/tsladderz.py
@@ -4,6 +4,11 @@
 import socket
 import cPickle
 import time
+import bz2
+import urllib
+import tempfile
+import shutil
+import os
 
 def send(message):
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,20 +35,75 @@ def send(message):
 	return response
 
 def stop_tournament(tournament_name):
-	response = send(('stop', tournament_name))
+	message = {}
+	message['command'] = 'stop'
+	message['tournament_name'] = tournament_name
+	
+	response = send(message)
 	return response
 	
 def start_tournament(tournament_name, max_duration):
-	response = send(('start', tournament_name, max_duration))
+	message = {}
+	message['command'] = 'start'
+	message['tournament_name'] = tournament_name
+	message['max_duration'] = max_duration
+	
+	response = send(message)
 	return response
 
-import bz2
-import urllib
+def tsladderd_status():
+	message = {}
+	message['command'] = 'status'
+	
+	response = send(message)
+	return response
+
 def decompress(url):
 	match_file = urllib.urlopen('http://tsladder:cdQdpjG@localhost:54424/%s' % url)
 	content = match_file.read()
 	match_file.close()
 	return bz2.decompress(content)
+
+def check_productions(zip_data, primary_file):
+	result = None
+	
+	# create a temporary folder and change to it
+	topdir = tempfile.mkdtemp()
+	oldcwd = os.getcwd()
+	os.chdir(topdir)
+
+	# save the file
+	zip_file = open("tank.zip", 'w')
+	zip_file.write(zip_data)
+	zip_file.close()
+
+	# unzip the file
+	os.system("/usr/bin/unzip -q tank.zip -d tank")
+
+	# make everything readable
+	os.system("/bin/chmod -R u+w tank")
+
+	try:
+		# Make sure the file exists
+		tank_source_file_name = os.path.join("tank", primary_file)
+		if not os.path.exists(tank_source_file_name):
+			raise ValueError("Failed to find primary file")
+
+		# verify no bad commands
+		grepresult = os.system("/bin/grep -Rq cmd tank/*")
+		if grepresult == 0:
+			raise ValueError("Found illegal commands")
+	except ValueError, v:
+		result = str(v)
+
+	# change out of the temp dir
+	os.chdir(oldcwd)
+
+	# remove the temporary files
+	shutil.rmtree(topdir)
+
+	# a result of none indicates success, otherwise it is an error message
+	return result
 
 if __name__ == '__main__':
 	print 'sending start'
