58d609130c0f57e7d5433e9108455d8242f08d3f
Jon Voigt <voigtjr@gmail.com>
2006-12-11 17:09:35 +0000

    Fixed redraw bug.

--- SoarSuite/Environments/Soar2D/src/soar2d/visuals/VisualWorld.java
+++ SoarSuite/Environments/Soar2D/src/soar2d/visuals/VisualWorld.java
@@ -28,38 +28,38 @@ public class VisualWorld extends Canvas implements PaintListener {
 
 	public static boolean internalRepaint = false;
 	
-	java.awt.Point m_AgentLocation;
+	java.awt.Point agentLocation;
 	
-	protected Display m_Display;
-	protected int m_CellSize;
-	protected boolean m_Disabled = false;
-	protected boolean m_Painted = false;
-	protected int m_LastX = 0;
-	protected int m_LastY = 0;
+	protected Display display;
+	protected int cellSize;
+	protected boolean disabled = false;
+	protected boolean painted = false;
+	protected int lastX = 0;
+	protected int lastY = 0;
 	
 	public VisualWorld(Composite parent, int style, int cellSize) {
 		super(parent, style | SWT.NO_BACKGROUND);
-		m_Display = parent.getDisplay();
-		m_CellSize = cellSize;
+		display = parent.getDisplay();
+		this.cellSize = cellSize;
 
 		addPaintListener(this);		
 	}
 
 	public int getMiniWidth() {
-		return m_CellSize * ((Soar2D.config.kEaterVision * 2) + 1);
+		return cellSize * ((Soar2D.config.kEaterVision * 2) + 1);
 	}
 	
 	public int getMiniHeight() {
-		return m_CellSize * ((Soar2D.config.kEaterVision * 2) + 1);
+		return cellSize * ((Soar2D.config.kEaterVision * 2) + 1);
 	}
 	
 	public void setAgentLocation(java.awt.Point location) {
-		m_AgentLocation = location;
+		agentLocation = location;
 	}
 	
 	Player getPlayerAtPixel(int x, int y) {
-		x /= m_CellSize;
-		y /= m_CellSize;
+		x /= cellSize;
+		y /= cellSize;
 		Cell cell = Soar2D.simulation.world.getCell(x, y);
 		if (cell.getPlayer() != null) {
 			return cell.getPlayer();
@@ -68,9 +68,9 @@ public class VisualWorld extends Canvas implements PaintListener {
 	}
 
 	public void paintControl(PaintEvent e){
-		if (m_AgentLocation != null || m_LastX != e.x || m_LastY != e.y || internalRepaint) {
-			m_LastX = e.x;
-			m_LastY = e.y;
+		if (agentLocation != null || lastX != e.x || lastY != e.y || internalRepaint) {
+			lastX = e.x;
+			lastY = e.y;
 			setRepaint();
 		}
 		
@@ -78,11 +78,11 @@ public class VisualWorld extends Canvas implements PaintListener {
         gc.setForeground(WindowManager.black);
 		gc.setLineWidth(1);
 
-		if (m_Disabled || !m_Painted) {
+		if (disabled || !painted) {
 			gc.setBackground(WindowManager.widget_background);
 			gc.fillRectangle(0,0, this.getWidth(), this.getHeight());
-			if (m_Disabled) {
-				m_Painted = true;
+			if (disabled) {
+				painted = true;
 				return;
 			}
 		}
@@ -90,28 +90,30 @@ public class VisualWorld extends Canvas implements PaintListener {
 		// Draw world
 		int fill1, fill2, xDraw, yDraw;
 		for(int x = 0; x < Soar2D.simulation.world.getSize(); ++x){
-			if (m_AgentLocation != null) {
-				if ((x < m_AgentLocation.x - Soar2D.config.kEaterVision) || (x > m_AgentLocation.x + Soar2D.config.kEaterVision)) {
+			if (agentLocation != null) {
+				if ((x < agentLocation.x - Soar2D.config.kEaterVision) || (x > agentLocation.x + Soar2D.config.kEaterVision)) {
 					continue;
 				} 
-				xDraw = x + Soar2D.config.kEaterVision - m_AgentLocation.x;
+				xDraw = x + Soar2D.config.kEaterVision - agentLocation.x;
 			} else {
 				xDraw = x;
 			}
 			
 			for(int y = 0; y < Soar2D.simulation.world.getSize(); ++y){
-				if (m_AgentLocation != null) {
-					if ((y < m_AgentLocation.y - Soar2D.config.kEaterVision) || (y > m_AgentLocation.y + Soar2D.config.kEaterVision)) {
+				if (agentLocation != null) {
+					if ((y < agentLocation.y - Soar2D.config.kEaterVision) || (y > agentLocation.y + Soar2D.config.kEaterVision)) {
 						continue;
 					} 
-					yDraw = y + Soar2D.config.kEaterVision - m_AgentLocation.y;
+					yDraw = y + Soar2D.config.kEaterVision - agentLocation.y;
 				} else {
 					yDraw = y;
 				}
 				
 				Cell cell = Soar2D.simulation.world.getCell(x, y);
-				if ((cell.removeObject(Names.kRedraw) == null) && m_Painted) {
-					continue;
+				if (agentLocation == null) {
+					if ((cell.removeObject(Names.kRedraw) == null) && painted) {
+						continue;
+					}
 				}
 				
 				ArrayList<CellObject> drawList = cell.getAllWithProperty(Names.kPropertyShape);
@@ -120,7 +122,7 @@ public class VisualWorld extends Canvas implements PaintListener {
 				
 				if (!cell.enterable()) {
 				    gc.setBackground(WindowManager.black);
-				    gc.fillRectangle(m_CellSize*xDraw + 1, m_CellSize*yDraw + 1, m_CellSize - 2, m_CellSize - 2);
+				    gc.fillRectangle(cellSize*xDraw + 1, cellSize*yDraw + 1, cellSize - 2, cellSize - 2);
 					
 				} else {
 					boolean empty = true;
@@ -131,7 +133,7 @@ public class VisualWorld extends Canvas implements PaintListener {
 						Player eater = cell.getPlayer();
 						
 						gc.setBackground(playerColors.get(eater));
-						gc.fillOval(m_CellSize*xDraw, m_CellSize*yDraw, m_CellSize, m_CellSize);
+						gc.fillOval(cellSize*xDraw, cellSize*yDraw, cellSize, cellSize);
 						gc.setBackground(WindowManager.widget_background);
 						
 						
@@ -159,7 +161,7 @@ public class VisualWorld extends Canvas implements PaintListener {
 						
 						if (empty) {
 							gc.setBackground(WindowManager.widget_background);
-							gc.fillRectangle(m_CellSize*xDraw, m_CellSize*yDraw, m_CellSize, m_CellSize);
+							gc.fillRectangle(cellSize*xDraw, cellSize*yDraw, cellSize, cellSize);
 						}
 						empty = false;
 					    
@@ -171,22 +173,22 @@ public class VisualWorld extends Canvas implements PaintListener {
 						
 						Shape shape = Shape.getShape(object.getStringProperty(Names.kPropertyShape));
 						if (shape.equals(Shape.ROUND)) {
-							fill1 = (int)(m_CellSize/2.8);
-							fill2 = m_CellSize - fill1 + 1;
-							gc.fillOval(m_CellSize*xDraw + fill1, m_CellSize*yDraw + fill1, m_CellSize - fill2, m_CellSize - fill2);
-							gc.drawOval(m_CellSize*xDraw + fill1, m_CellSize*yDraw + fill1, m_CellSize - fill2 - 1, m_CellSize - fill2 - 1);
+							fill1 = (int)(cellSize/2.8);
+							fill2 = cellSize - fill1 + 1;
+							gc.fillOval(cellSize*xDraw + fill1, cellSize*yDraw + fill1, cellSize - fill2, cellSize - fill2);
+							gc.drawOval(cellSize*xDraw + fill1, cellSize*yDraw + fill1, cellSize - fill2 - 1, cellSize - fill2 - 1);
 							
 						} else if (shape.equals(Shape.SQUARE)) {
-							fill1 = (int)(m_CellSize/2.8);
-							fill2 = m_CellSize - fill1 + 1;
-							gc.fillRectangle(m_CellSize*xDraw + fill1, m_CellSize*yDraw + fill1, m_CellSize - fill2, m_CellSize - fill2);
-							gc.drawRectangle(m_CellSize*xDraw + fill1, m_CellSize*yDraw + fill1, m_CellSize - fill2, m_CellSize - fill2);
+							fill1 = (int)(cellSize/2.8);
+							fill2 = cellSize - fill1 + 1;
+							gc.fillRectangle(cellSize*xDraw + fill1, cellSize*yDraw + fill1, cellSize - fill2, cellSize - fill2);
+							gc.drawRectangle(cellSize*xDraw + fill1, cellSize*yDraw + fill1, cellSize - fill2, cellSize - fill2);
 						}
 					}
 					
 					if (empty) {
 						gc.setBackground(WindowManager.widget_background);
-						gc.fillRectangle(m_CellSize*xDraw, m_CellSize*yDraw, m_CellSize, m_CellSize);
+						gc.fillRectangle(cellSize*xDraw, cellSize*yDraw, cellSize, cellSize);
 					}
 				}
 				
@@ -198,20 +200,20 @@ public class VisualWorld extends Canvas implements PaintListener {
 				}
 			}
 		}
-		m_Painted = true;
+		painted = true;
 	}
 	
 	void drawExplosion(GC gc, int x, int y) {
 		gc.setBackground(WindowManager.red);
-		int offCenter = m_CellSize/4;
-		int xBase = m_CellSize*x;
-		int yBase = m_CellSize*y;
-		int halfCell = m_CellSize/2;
+		int offCenter = cellSize/4;
+		int xBase = cellSize*x;
+		int yBase = cellSize*y;
+		int halfCell = cellSize/2;
 		
 		java.awt.Point center = new java.awt.Point(xBase + halfCell, yBase + halfCell);
 		java.awt.Point north = new java.awt.Point(center.x, yBase);
-		java.awt.Point east = new java.awt.Point(xBase + m_CellSize, center.y);
-		java.awt.Point south = new java.awt.Point(center.x, yBase + m_CellSize);
+		java.awt.Point east = new java.awt.Point(xBase + cellSize, center.y);
+		java.awt.Point south = new java.awt.Point(center.x, yBase + cellSize);
 		java.awt.Point west = new java.awt.Point(xBase, center.y);
 		
 		gc.fillPolygon(new int[] {center.x, center.y, north.x, north.y, center.x + offCenter, center.y});
@@ -223,69 +225,69 @@ public class VisualWorld extends Canvas implements PaintListener {
 	void drawEaterMouth(int x, int y, int x_mult, int y_mult, int cx_mult, int cy_mult, GC gc){		
 	    switch(Soar2D.simulation.world.getWorldCount() % 8){
 			case(0):{
-			    gc.fillPolygon(new int[]{m_CellSize*x, m_CellSize*y,
-			            m_CellSize*x + x_mult*m_CellSize, m_CellSize * y + y_mult*m_CellSize,
-			            m_CellSize*x + cx_mult*m_CellSize/2, m_CellSize*y + cy_mult*m_CellSize/2});
+			    gc.fillPolygon(new int[]{cellSize*x, cellSize*y,
+			            cellSize*x + x_mult*cellSize, cellSize * y + y_mult*cellSize,
+			            cellSize*x + cx_mult*cellSize/2, cellSize*y + cy_mult*cellSize/2});
 			    break;
 			}
 			case(1):{
-			    gc.fillPolygon(new int[]{m_CellSize*x + x_mult*m_CellSize/8, m_CellSize*y + y_mult*m_CellSize/8,
-			            m_CellSize*x + x_mult*(m_CellSize - m_CellSize/8), m_CellSize * y + y_mult*(m_CellSize - m_CellSize/8),
-			            m_CellSize*x + cx_mult*m_CellSize/2, m_CellSize*y + cy_mult*m_CellSize/2});
+			    gc.fillPolygon(new int[]{cellSize*x + x_mult*cellSize/8, cellSize*y + y_mult*cellSize/8,
+			            cellSize*x + x_mult*(cellSize - cellSize/8), cellSize * y + y_mult*(cellSize - cellSize/8),
+			            cellSize*x + cx_mult*cellSize/2, cellSize*y + cy_mult*cellSize/2});
 			    break;
 			}
 			case(2):{
-			    gc.fillPolygon(new int[]{m_CellSize*x + x_mult*m_CellSize/4, m_CellSize*y + y_mult*m_CellSize/4,
-			            m_CellSize*x + x_mult*(m_CellSize - m_CellSize/4), m_CellSize * y + y_mult*(m_CellSize - m_CellSize/4),
-			            m_CellSize*x + cx_mult * m_CellSize/2, m_CellSize*y + cy_mult * m_CellSize/2});
+			    gc.fillPolygon(new int[]{cellSize*x + x_mult*cellSize/4, cellSize*y + y_mult*cellSize/4,
+			            cellSize*x + x_mult*(cellSize - cellSize/4), cellSize * y + y_mult*(cellSize - cellSize/4),
+			            cellSize*x + cx_mult * cellSize/2, cellSize*y + cy_mult * cellSize/2});
 			    break;
 			}
 			case(3):{
-			    gc.fillPolygon(new int[]{m_CellSize*x + x_mult * 3*m_CellSize/8, m_CellSize*y + y_mult * 3*m_CellSize/8,
-			            m_CellSize*x + x_mult*(m_CellSize - 3*m_CellSize/8), m_CellSize * y + y_mult*(m_CellSize - 3*m_CellSize/8),
-			            m_CellSize*x + cx_mult*m_CellSize/2, m_CellSize*y + cy_mult*m_CellSize/2});
+			    gc.fillPolygon(new int[]{cellSize*x + x_mult * 3*cellSize/8, cellSize*y + y_mult * 3*cellSize/8,
+			            cellSize*x + x_mult*(cellSize - 3*cellSize/8), cellSize * y + y_mult*(cellSize - 3*cellSize/8),
+			            cellSize*x + cx_mult*cellSize/2, cellSize*y + cy_mult*cellSize/2});
 			    break;
 			}
 			case(4): break;
 			case(5):{
-			    gc.fillPolygon(new int[]{m_CellSize*x + x_mult * 3*m_CellSize/8, m_CellSize*y + y_mult * 3*m_CellSize/8,
-			            m_CellSize*x + x_mult*(m_CellSize - 3*m_CellSize/8), m_CellSize * y + y_mult*(m_CellSize - 3*m_CellSize/8),
-			            m_CellSize*x + cx_mult*m_CellSize/2, m_CellSize*y + cy_mult*m_CellSize/2});
+			    gc.fillPolygon(new int[]{cellSize*x + x_mult * 3*cellSize/8, cellSize*y + y_mult * 3*cellSize/8,
+			            cellSize*x + x_mult*(cellSize - 3*cellSize/8), cellSize * y + y_mult*(cellSize - 3*cellSize/8),
+			            cellSize*x + cx_mult*cellSize/2, cellSize*y + cy_mult*cellSize/2});
 			    break;
 			}
 			case(6):{
-			    gc.fillPolygon(new int[]{m_CellSize*x + x_mult*m_CellSize/4, m_CellSize*y + y_mult*m_CellSize/4,
-			            m_CellSize*x + x_mult*(m_CellSize - m_CellSize/4), m_CellSize * y + y_mult*(m_CellSize - m_CellSize/4),
-			            m_CellSize*x + cx_mult * m_CellSize/2, m_CellSize*y + cy_mult * m_CellSize/2});
+			    gc.fillPolygon(new int[]{cellSize*x + x_mult*cellSize/4, cellSize*y + y_mult*cellSize/4,
+			            cellSize*x + x_mult*(cellSize - cellSize/4), cellSize * y + y_mult*(cellSize - cellSize/4),
+			            cellSize*x + cx_mult * cellSize/2, cellSize*y + cy_mult * cellSize/2});
 			    break;
 			}
 			case(7):{
-			    gc.fillPolygon(new int[]{m_CellSize*x + x_mult*m_CellSize/8, m_CellSize*y + y_mult*m_CellSize/8,
-			            m_CellSize*x + x_mult*(m_CellSize - m_CellSize/8), m_CellSize * y + y_mult*(m_CellSize - m_CellSize/8),
-			            m_CellSize*x + cx_mult*m_CellSize/2, m_CellSize*y + cy_mult*m_CellSize/2});
+			    gc.fillPolygon(new int[]{cellSize*x + x_mult*cellSize/8, cellSize*y + y_mult*cellSize/8,
+			            cellSize*x + x_mult*(cellSize - cellSize/8), cellSize * y + y_mult*(cellSize - cellSize/8),
+			            cellSize*x + cx_mult*cellSize/2, cellSize*y + cy_mult*cellSize/2});
 			    break;	
 			}
 		}
 	}
 	
 	public void setRepaint() {
-		m_Painted = false;
+		painted = false;
 	}
 	
 	public void disable() {
-		m_Disabled = true;
+		disabled = true;
 	}
 	
 	public void enable() {
-		m_Disabled = false;
+		disabled = false;
 	}
 	
 	public int getWidth() {
-		return m_CellSize * Soar2D.simulation.world.getSize();
+		return cellSize * Soar2D.simulation.world.getSize();
 	}
 	
 	public int getHeight() {
-		return m_CellSize * Soar2D.simulation.world.getSize();
+		return cellSize * Soar2D.simulation.world.getSize();
 	}
 	
 }
