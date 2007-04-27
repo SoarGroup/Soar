168ae1d178915ad87d7899116def16c55789b5fe
Bob Marinier <bob.marinier@soartech.com>
2007-04-27 18:57:51 +0000

    removed old code changes to make calls to old code call new code

--- SoarSuite/Core/gSKI/src/gSKI_DoNotTouch.cpp
+++ SoarSuite/Core/gSKI/src/gSKI_DoNotTouch.cpp
@@ -373,109 +373,6 @@ namespace gSKI
 			print_string (thisAgent, buf);
 		}
 
-		void print_augs_of_id (agent* agnt,
-			Symbol *id, 
-			int depth, 
-			bool internal,
-			int indent, 
-			tc_number tc) 
-		{
-			slot *s;
-			wme *w;
-
-			wme **list;    /* array of WME pointers, AGR 652 */
-			int num_attr;  /* number of attributes, AGR 652 */
-			int attr;      /* attribute index, AGR 652 */
-
-			/* AGR 652  The plan is to go through the list of WMEs and find out how
-			many there are.  Then we malloc an array of that many pointers.
-			Then we go through the list again and copy all the pointers to that array.
-			Then we qsort the array and print it out.  94.12.13 */
-
-			if (id->common.symbol_type != IDENTIFIER_SYMBOL_TYPE) return;
-			if (id->id.tc_num==tc && id->id.depth >= depth) return;  // this has already been printed at an equal-or-lower depth, RPM 4/07 bug 988
-			
-			id->id.depth = depth; // set the depth of this id
-
-			if(id->id.tc_num != tc) { // if we haven't printed this id yet, then print it, RPM 4/07 bug 988
-				id->id.tc_num = tc;
-
-				/* --- first, count all direct augmentations of this id --- */
-				num_attr = 0;
-				for (w=id->id.impasse_wmes; w!=NIL; w=w->next) num_attr++;
-				for (w=id->id.input_wmes; w!=NIL; w=w->next) num_attr++;
-				for (s=id->id.slots; s!=NIL; s=s->next) {
-					for (w=s->wmes; w!=NIL; w=w->next) num_attr++;
-					for (w=s->acceptable_preference_wmes; w!=NIL; w=w->next) num_attr++;
-				}
-
-				/* --- next, construct the array of wme pointers and sort them --- */
-				list = (wme**)allocate_memory(agnt, num_attr*sizeof(wme *), MISCELLANEOUS_MEM_USAGE);
-				attr = 0;
-				for (w=id->id.impasse_wmes; w!=NIL; w=w->next)
-					list[attr++] = w;
-				for (w=id->id.input_wmes; w!=NIL; w=w->next)
-					list[attr++] = w;
-				for (s=id->id.slots; s!=NIL; s=s->next) {
-					for (w=s->wmes; w!=NIL; w=w->next)
-						list[attr++] = w;
-					for (w=s->acceptable_preference_wmes; w!=NIL; w=w->next)
-						list[attr++] = w;
-				}
-				qsort (list, num_attr, sizeof (wme *), compare_attr); 
-
-				/* --- finally, print the sorted wmes and deallocate the array --- */
-				if (internal) {
-					for (attr=0; attr < num_attr; attr++) {
-						w = list[attr];
-						print_spaces (agnt, indent);
-						print_wme (agnt, w);
-					}
-				} else {
-					print_spaces (agnt, indent);
-					print_with_symbols (agnt, "(%y", id);
-
-					// XML format of an <id> followed by a series of <wmes> each of which shares the original ID.
-					// <id id="s1"><wme tag="123" attr="foo" attrtype="string" val="123" valtype="string"></wme><wme attr="bar" ...></wme></id>
-					gSKI_MakeAgentCallbackXML(agnt, kFunctionBeginTag, kWME_Id);
-					gSKI_MakeAgentCallbackXML(agnt, kFunctionAddAttribute, kWME_Id, symbol_to_string (agnt, id, true, 0, 0));
-
-					for (attr=0; attr < num_attr; attr++) {
-						w = list[attr];
-						neatly_print_wme_augmentation_of_id (agnt, w, indent);
-					}
-					
-					gSKI_MakeAgentCallbackXML(agnt, kFunctionEndTag, kWME_Id);
-
-					print (agnt, ")\n");
-				}
-				free_memory(agnt, list, MISCELLANEOUS_MEM_USAGE);
-				/* AGR 652 end */
-			}
-			/* --- if depth<=1, we're done --- */
-			if (depth<=1) return;
-
-			/* --- call this routine recursively --- */
-			for (w=id->id.input_wmes; w!=NIL; w=w->next) {
-				print_augs_of_id (agnt, w->attr, depth-1, internal, indent+2, tc);
-				print_augs_of_id (agnt, w->value, depth-1, internal, indent+2, tc);
-			}
-			for (w=id->id.impasse_wmes; w!=NIL; w=w->next) {
-				print_augs_of_id (agnt, w->attr, depth-1, internal, indent+2, tc);
-				print_augs_of_id (agnt, w->value, depth-1, internal, indent+2, tc);
-			}
-			for (s=id->id.slots; s!=NIL; s=s->next) {
-				for (w=s->wmes; w!=NIL; w=w->next) {
-					print_augs_of_id (agnt, w->attr, depth-1, internal, indent+2, tc);
-					print_augs_of_id (agnt, w->value, depth-1, internal, indent+2, tc);
-				}
-				for (w=s->acceptable_preference_wmes; w!=NIL; w=w->next) {
-					print_augs_of_id (agnt, w->attr, depth-1, internal, indent+2, tc);
-					print_augs_of_id (agnt, w->value, depth-1, internal, indent+2, tc);
-				}
-			}
-		}
-
 		/* RPM 4/07 bug 988
 			This function traverses the ids we are going to print and marks each with its shallowest depth
 			That is, if an id can be reached by multiple paths, this will find the shortest one and save
@@ -525,113 +422,8 @@ namespace gSKI
 			}
 		}
 
-		void print_augs_of_id2 (agent* agnt,
-			Symbol *id, 
-			int depth,
-			int maxdepth,
-			bool internal,
-			tc_number tc) 
-		{
-			slot *s;
-			wme *w;
-
-			wme **list;    /* array of WME pointers, AGR 652 */
-			int num_attr;  /* number of attributes, AGR 652 */
-			int attr;      /* attribute index, AGR 652 */
-
-			/* AGR 652  The plan is to go through the list of WMEs and find out how
-			many there are.  Then we malloc an array of that many pointers.
-			Then we go through the list again and copy all the pointers to that array.
-			Then we qsort the array and print it out.  94.12.13 */
-
-			if (id->common.symbol_type != IDENTIFIER_SYMBOL_TYPE) return;
-			if (id->id.tc_num==tc) return;  // this has already been printed, so return RPM 4/07 bug 988
-			if (id->id.depth > depth) return;  // this can be reached via an equal or shorter path, so return without printing RPM 4/07 bug 988
-
-			// if we're here, then we haven't printed this id yet, so print it
-
-			depth = id->id.depth; // set the depth to the depth via the shallowest path, RPM 4/07 bug 988
-			int indent = (maxdepth-depth)*2; // set the indent based on how deep we are, RPM 4/07 bug 988
-
-			id->id.tc_num = tc;  // mark id as printed
-
-			/* --- first, count all direct augmentations of this id --- */
-			num_attr = 0;
-			for (w=id->id.impasse_wmes; w!=NIL; w=w->next) num_attr++;
-			for (w=id->id.input_wmes; w!=NIL; w=w->next) num_attr++;
-			for (s=id->id.slots; s!=NIL; s=s->next) {
-				for (w=s->wmes; w!=NIL; w=w->next) num_attr++;
-				for (w=s->acceptable_preference_wmes; w!=NIL; w=w->next) num_attr++;
-			}
-
-			/* --- next, construct the array of wme pointers and sort them --- */
-			list = (wme**)allocate_memory(agnt, num_attr*sizeof(wme *), MISCELLANEOUS_MEM_USAGE);
-			attr = 0;
-			for (w=id->id.impasse_wmes; w!=NIL; w=w->next)
-				list[attr++] = w;
-			for (w=id->id.input_wmes; w!=NIL; w=w->next)
-				list[attr++] = w;
-			for (s=id->id.slots; s!=NIL; s=s->next) {
-				for (w=s->wmes; w!=NIL; w=w->next)
-					list[attr++] = w;
-				for (w=s->acceptable_preference_wmes; w!=NIL; w=w->next)
-					list[attr++] = w;
-			}
-			qsort (list, num_attr, sizeof (wme *), compare_attr); 
-
-			/* --- finally, print the sorted wmes and deallocate the array --- */
-			if (internal) {
-				for (attr=0; attr < num_attr; attr++) {
-					w = list[attr];
-					print_spaces (agnt, indent);
-					print_wme (agnt, w);
-				}
-			} else {
-				print_spaces (agnt, indent);
-				print_with_symbols (agnt, "(%y", id);
-
-				// XML format of an <id> followed by a series of <wmes> each of which shares the original ID.
-				// <id id="s1"><wme tag="123" attr="foo" attrtype="string" val="123" valtype="string"></wme><wme attr="bar" ...></wme></id>
-				gSKI_MakeAgentCallbackXML(agnt, kFunctionBeginTag, kWME_Id);
-				gSKI_MakeAgentCallbackXML(agnt, kFunctionAddAttribute, kWME_Id, symbol_to_string (agnt, id, true, 0, 0));
-
-				for (attr=0; attr < num_attr; attr++) {
-					w = list[attr];
-					neatly_print_wme_augmentation_of_id (agnt, w, indent);
-				}
-				
-				gSKI_MakeAgentCallbackXML(agnt, kFunctionEndTag, kWME_Id);
-
-				print (agnt, ")\n");
-			}
-			free_memory(agnt, list, MISCELLANEOUS_MEM_USAGE);
-			/* AGR 652 end */
-
-			/* --- if depth<=1, we're done --- */
-			if (depth<=1) return;
-
-			/* --- call this routine recursively --- */
-			for (w=id->id.input_wmes; w!=NIL; w=w->next) {
-				print_augs_of_id2 (agnt, w->attr, depth-1, maxdepth, internal, tc);
-				print_augs_of_id2 (agnt, w->value, depth-1, maxdepth, internal, tc);
-			}
-			for (w=id->id.impasse_wmes; w!=NIL; w=w->next) {
-				print_augs_of_id2 (agnt, w->attr, depth-1, maxdepth, internal, tc);
-				print_augs_of_id2 (agnt, w->value, depth-1, maxdepth, internal, tc);
-			}
-			for (s=id->id.slots; s!=NIL; s=s->next) {
-				for (w=s->wmes; w!=NIL; w=w->next) {
-					print_augs_of_id2 (agnt, w->attr, depth-1, maxdepth, internal, tc);
-					print_augs_of_id2 (agnt, w->value, depth-1, maxdepth, internal, tc);
-				}
-				for (w=s->acceptable_preference_wmes; w!=NIL; w=w->next) {
-					print_augs_of_id2 (agnt, w->attr, depth-1, maxdepth, internal, tc);
-					print_augs_of_id2 (agnt, w->value, depth-1, maxdepth, internal, tc);
-				}
-			}
-		}
-
-		void print_augs_of_id3 (agent* agnt,
+		// RPM 4/07: Note, mark_depths_augs_of_id must be called before the root call to print_augs_of_id
+		void print_augs_of_id (agent* agnt,
 			Symbol *id, 
 			int depth,
 			int maxdepth,
@@ -699,8 +491,8 @@ namespace gSKI
 
 					if (depth>1) { // we're not done yet
 						/* --- call this routine recursively --- */
-						print_augs_of_id3 (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
-						print_augs_of_id3 (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
+						print_augs_of_id (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
+						print_augs_of_id (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
 					}
 				}
 			// RPM 4/07 This is not a tree print, so for each wme in the list, print it
@@ -736,98 +528,14 @@ namespace gSKI
 					for (attr=0; attr < num_attr; attr++) {
 						w = list[attr];
 						/* --- call this routine recursively --- */
-						print_augs_of_id3 (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
-						print_augs_of_id3 (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
+						print_augs_of_id (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
+						print_augs_of_id (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
 					}
 				}
 			}
 
 			// deallocate the array
 			free_memory(agnt, list, MISCELLANEOUS_MEM_USAGE);
-
-
-
-
-			/* --- finally, print the sorted wmes and deallocate the array --- */
-			//if (tree) {
-			//	for (attr=0; attr < num_attr; attr++) {
-			//		w = list[attr];
-			//		print_spaces (agnt, indent);
-			//		if (internal) print_wme (agnt, w);
-			//		else print_wme_without_timetag (agnt, w);
-
-			//		if (depth>1) { // we're not done yet
-			//			/* --- call this routine recursively --- */
-			//			print_augs_of_id3 (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
-			//			print_augs_of_id3 (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
-			//		}
-			//	}
-			//	free_memory(agnt, list, MISCELLANEOUS_MEM_USAGE);
-			//	return;
-			//}
-			//
-			//if (internal && !tree) {
-			//	for (attr=0; attr < num_attr; attr++) {
-			//		w = list[attr];
-			//		print_spaces (agnt, indent);
-			//		print_wme (agnt, w);
-			//	}
-			//} else {
-			//	print_spaces (agnt, indent);
-			//	print_with_symbols (agnt, "(%y", id);
-
-			//	// XML format of an <id> followed by a series of <wmes> each of which shares the original ID.
-			//	// <id id="s1"><wme tag="123" attr="foo" attrtype="string" val="123" valtype="string"></wme><wme attr="bar" ...></wme></id>
-			//	gSKI_MakeAgentCallbackXML(agnt, kFunctionBeginTag, kWME_Id);
-			//	gSKI_MakeAgentCallbackXML(agnt, kFunctionAddAttribute, kWME_Id, symbol_to_string (agnt, id, true, 0, 0));
-
-			//	for (attr=0; attr < num_attr; attr++) {
-			//		w = list[attr];
-			//		neatly_print_wme_augmentation_of_id (agnt, w, indent);
-			//	}
-			//	
-			//	gSKI_MakeAgentCallbackXML(agnt, kFunctionEndTag, kWME_Id);
-
-			//	print (agnt, ")\n");
-			//}
-
-			//if (depth > 1) {
-			//	for (attr=0; attr < num_attr; attr++) {
-			//		w = list[attr];
-			//		/* --- call this routine recursively --- */
-			//		print_augs_of_id3 (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
-			//		print_augs_of_id3 (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
-			//	}
-			//}
-
-			//free_memory(agnt, list, MISCELLANEOUS_MEM_USAGE);
-
-
-
-			///* AGR 652 end */
-
-			///* --- if depth<=1, we're done --- */
-			//if (depth<=1) return;
-
-			///* --- call this routine recursively --- */
-			//for (w=id->id.input_wmes; w!=NIL; w=w->next) {
-			//	print_augs_of_id3 (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
-			//	print_augs_of_id3 (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
-			//}
-			//for (w=id->id.impasse_wmes; w!=NIL; w=w->next) {
-			//	print_augs_of_id3 (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
-			//	print_augs_of_id3 (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
-			//}
-			//for (s=id->id.slots; s!=NIL; s=s->next) {
-			//	for (w=s->wmes; w!=NIL; w=w->next) {
-			//		print_augs_of_id3 (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
-			//		print_augs_of_id3 (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
-			//	}
-			//	for (w=s->acceptable_preference_wmes; w!=NIL; w=w->next) {
-			//		print_augs_of_id3 (agnt, w->attr, depth-1, maxdepth, internal, tree, tc);
-			//		print_augs_of_id3 (agnt, w->value, depth-1, maxdepth, internal, tree, tc);
-			//	}
-			//}
 		}
 
 		void do_print_for_identifier (agent* agnt, Symbol *id, int depth, bool internal, bool tree) {
@@ -838,15 +546,7 @@ namespace gSKI
 			tc = get_new_tc_number(agnt);
 			mark_depths_augs_of_id (agnt, id, depth, tc);
 			tc = get_new_tc_number(agnt);
-			print_augs_of_id3 (agnt, id, depth, depth, internal, tree, tc);
-		}
-
-
-		void do_print_for_identifier_OLD (agent* agnt, Symbol *id, int depth, bool internal) {
-			tc_number tc;
-
-			tc = get_new_tc_number(agnt);
-			print_augs_of_id (agnt, id, depth, internal, 0, tc);
+			print_augs_of_id (agnt, id, depth, depth, internal, tree, tc);
 		}
 
 		void do_print_for_production_name (agent* agnt,
@@ -876,7 +576,7 @@ namespace gSKI
 			}
 		}
 
-		void do_print_for_wme (agent* agnt, wme *w, int depth, bool internal) {
+		void do_print_for_wme (agent* agnt, wme *w, int depth, bool internal, bool tree) {
 			tc_number tc;
 
 			if (internal && (depth==0)) {
@@ -884,7 +584,9 @@ namespace gSKI
 				print (agnt, "\n");
 			} else {
 				tc = get_new_tc_number(agnt);
-				print_augs_of_id (agnt, w->id, depth, internal, 0, tc);
+				mark_depths_augs_of_id (agnt, w->id, depth, tc);
+				tc = get_new_tc_number(agnt);
+				print_augs_of_id (agnt, w->id, depth, depth, internal, tree, tc);
 			}
 		}
 
@@ -1145,7 +847,7 @@ namespace gSKI
 						break;
 				if (w) 
 				{
-					do_print_for_wme (agnt, w, depth, internal);
+					do_print_for_wme (agnt, w, depth, internal, tree);
 				} else 
 				{
 					print(agnt, "No wme %ld in working memory.", agnt->lexeme.int_val);
@@ -1173,7 +875,7 @@ namespace gSKI
 				soar_alternate_input(agnt, NIL, NIL, FALSE); 
 				agnt->current_char = ' ';
 				for (c = wmes; c != NIL; c = c->rest)
-					do_print_for_wme (agnt, (wme *)c->first, depth, internal);
+					do_print_for_wme (agnt, (wme *)c->first, depth, internal, tree);
 				free_list (agnt, wmes);
 				break;
 
