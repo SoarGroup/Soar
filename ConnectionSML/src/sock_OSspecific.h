0453e49d193d4895eabe075230befef372b4d379
Jon Voigt <voigtjr@gmail.com>
2007-06-14 17:01:51 +0000

    Changed many files to use portability.h to centralize non-portable includes and defines. Untested and probably broken on linux.

--- ConnectionSML/src/sock_OSspecific.h
+++ ConnectionSML/src/sock_OSspecific.h
@@ -21,16 +21,5 @@ bool MakeSocketNonBlocking(SOCKET hSock) ;
 bool SleepSocket(long secs, long msecs) ;
 } // Namespace
 
-// Map certain functions depending on the OS
-#ifdef _WIN32
-#define STRICMP	   stricmp
-#define VSNSPRINTF _vsnprintf
-#define ENABLE_NAMED_PIPES
-#else
-#define STRICMP    strcasecmp
-#define VSNSPRINTF vsnprintf
-#define ENABLE_LOCAL_SOCKETS
-#endif
-
 #endif // CT_OS_SPECIFIC_H
 
--- ConnectionSML/src/sock_Socket.cpp
+++ ConnectionSML/src/sock_Socket.cpp
@@ -1,7 +1,7 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif // HAVE_CONFIG_H
-//FIXME: #include <portability.h>
+#include <portability.h>
 
 /////////////////////////////////////////////////////////////////
 // Socket class
