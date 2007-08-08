9071c9d08ae0aced72c6a56f5b257daa7b2170b0
Jon Voigt <voigtjr@gmail.com>
2007-08-08 19:52:17 +0000

    Implemented some new logic for cooking, mixing, and movement

--- Environments/Soar2D/src/soar2d/player/kitchen/SoarCook.java
+++ Environments/Soar2D/src/soar2d/player/kitchen/SoarCook.java
@@ -1,11 +1,39 @@
 package soar2d.player.kitchen;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.logging.*;
+
+import sml.*;
+import soar2d.Direction;
+import soar2d.Names;
+import soar2d.Simulation;
+import soar2d.Soar2D;
+import soar2d.map.CellObject;
+import soar2d.player.MoveInfo;
+import soar2d.player.Player;
 import soar2d.player.PlayerConfig;
+import soar2d.world.World;
 
 public class SoarCook extends Cook {
-
-	public SoarCook(PlayerConfig playerConfig) {
+	private Agent agent;
+	
+	public SoarCook(Agent agent, PlayerConfig playerConfig) {
 		super(playerConfig);
+
+		this.agent = agent;
+		
+		Identifier self = agent.CreateIdWME(agent.GetInputLink(), "self");
+		Identifier position = agent.CreateIdWME(self, "position");
+		IntElement x = agent.CreateIntWME(position, "x", 0);
+		IntElement y = agent.CreateIntWME(position, "y", 0);
+		IntElement reward = agent.CreateIntWME(self, "reward", 0);
+	}
+
+	@Override
+	public void moveWithObjectFailed() {
+		// TODO: set move-with-object command to status:error
 		
 	}
 }
