813d8282ebd9b2f905b8c0dcf0a5ce91d6eaba83
Sam Wintermute <sam.wintermute@gmail.com>
2007-10-01 17:22:47 +0000

    re-fix dos2unix thing

--- GGP/scripts/runMapper.pl
+++ GGP/scripts/runMapper.pl
@@ -11,8 +11,8 @@ $ENV{"PYTHONPATH"}="./pyparser/:.";
 $tmp1 = "$kifOne.tmp.map";
 $tmp2 = "$kifTwo.tmp.map";
 
-print `sed 's/^M\$//' $kifOne |grep -v init > $tmp1`;
-print `sed 's/^M\$//' $kifTwo |grep -v init > $tmp2`;
+print `sed 's/\$//' $kifOne |grep -v init > $tmp1`;
+print `sed 's/\$//' $kifTwo |grep -v init > $tmp2`;
 
 #$mapper = "python ../analogy/const_match/const_match3.py";
 $mapper = "./newConstantMapper.pl";
@@ -23,6 +23,8 @@ checkFor($mapper);
 
 print lc `$mapper $tmp1 $tmp2 | grep "map predicate\\|map constant"`;
 
+print `rm $tmp1`;
+print `rm $tmp2`;
 
 sub checkFor() {
   $file = shift;
