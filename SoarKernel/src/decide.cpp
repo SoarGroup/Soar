7a39ba5f76d11a698fdc55485874b9a08689c2fb
Nate Derbinsky <nlderbin@umich.edu>
2008-06-20 16:19:37 +0000

    reward-link is now created as an input-wme, as opposed to impasse-wme, which should allow justifications to work properly

--- SoarKernel/src/decide.cpp
+++ SoarKernel/src/decide.cpp
@@ -1253,7 +1253,7 @@ Symbol *create_new_impasse (agent* thisAgent, Bool isa_goal, Symbol *object, Sym
     add_impasse_wme (thisAgent, id, thisAgent->superstate_symbol, object, NIL);
 
 	id->id.reward_header = make_new_identifier( thisAgent, 'R', level );	
-	add_impasse_wme( thisAgent, id, thisAgent->reward_link_symbol, id->id.reward_header, NIL );
+	add_input_wme( thisAgent, id, thisAgent->reward_link_symbol, id->id.reward_header );	
   }
   else
     add_impasse_wme (thisAgent, id, thisAgent->object_symbol, object, NIL);
