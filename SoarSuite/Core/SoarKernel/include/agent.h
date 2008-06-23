f082fe5bb2abf3b7521cf3e9d573a61c686cb943
Nate Derbinsky <nlderbin@umich.edu>
2008-06-23 14:20:59 +0000

    comment/spacing changes

--- SoarSuite/Core/SoarKernel/include/agent.h
+++ SoarSuite/Core/SoarKernel/include/agent.h
@@ -49,6 +49,7 @@ typedef struct rhs_function_struct rhs_function;
 typedef struct rl_parameter_struct rl_parameter;
 typedef struct rl_stat_struct rl_stat;
 
+// select types
 typedef struct select_info_struct select_info;
 
 #ifdef __cplusplus
