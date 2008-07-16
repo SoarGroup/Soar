a3617743426b5c9fd5ae08168311ba690fb5c147
Bob Marinier <bob.marinier@soartech.com>
2008-07-16 19:17:37 +0000

    removed multiple agents, added output after run command returns

--- soar-robotics/player-client/RunThread.hxx
+++ soar-robotics/player-client/RunThread.hxx
@@ -2,10 +2,12 @@
 #define RUN_THREAD_HXX
 
 #include "RunThread.h"
+#include <iostream>
 
 void RunThread::Run()
 {
-    m_kernel->RunAllAgentsForever();
+	m_kernel->RunAllAgentsForever();
+	std::cout << "Run finished." << std::endl;
 }
 
 #endif // RUN_THREAD_HXX
