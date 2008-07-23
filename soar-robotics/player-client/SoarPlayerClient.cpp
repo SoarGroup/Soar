a99559224c31a3cfaa74498425ffe6ed7369c109
Jon Voigt <voigtjr@gmail.com>
2008-07-23 15:36:30 +0000

    added configuration file support

--- soar-robotics/player-client/SoarPlayerClient.cpp
+++ soar-robotics/player-client/SoarPlayerClient.cpp
@@ -2,49 +2,83 @@
 
 #include "InputLinkManager.h"
 #include "OutputLinkManager.h"
+#include "RunThread.h"
 
 #include <cassert>
-#include <exception>
+#include <iostream>
+#include <sstream>
 
 using namespace sml;
 using namespace PlayerCc;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::ostringstream;
+
+template < class T >
+inline string to_string( const T& t ) 
+{
+	ostringstream o;
+	o << t;
+	return o.str();
+}
 
 void updateHandler( smlUpdateEventId, void* pUserData, Kernel*, smlRunFlags )
 {
+	assert( pUserData );
+	
     SoarPlayerClient* pPlayerClient = static_cast< SoarPlayerClient* >( pUserData );
     pPlayerClient->update();
 }
 
 void agentHandler( smlAgentEventId id, void* pUserData, Agent* )
 {
+	assert( pUserData );
+	
     SoarPlayerClient* pPlayerClient = static_cast< SoarPlayerClient* >( pUserData );
     pPlayerClient->agent_event( id );
 }
 
-std::string rhsPrintHandler( smlRhsEventId, void*, Agent* pAgent, char const*, char const* pArgument )
+string rhsPrintHandler( smlRhsEventId, void*, Agent* pAgent, char const*, char const* pArgument )
 {
-	std::cout << pAgent->GetAgentName() << ": " << pArgument << std::endl;
+	assert( pAgent );
+	assert( pArgument );
+	
+	cout << pAgent->GetAgentName() << ": " << pArgument << endl;
 	return pArgument;
 }
 
-SoarPlayerClient::SoarPlayerClient( const std::string& productions, int number_of_bots )
-: m_kernel( 0 )
+SoarPlayerClient::SoarPlayerClient( ConfigFile& config )
+: m_config( config )
+, m_kernel( 0 )
 , m_bot_count( 0 )
 , m_run_thread( 0 )
 , m_stop_issued( false )
 {
+	int number_of_bots = config.read<int>( "bots" );
+
     m_kernel = sml::Kernel::CreateKernelInNewThread();
-	assert( m_kernel );
+    if ( m_kernel->HadError() )
+    {
+    	throw soar_error( m_kernel->GetLastErrorDescription() );
+    }
     
     while ( m_bot_count < number_of_bots )
     {
-	    std::stringstream agent_name;
-	    agent_name << "player" << m_bot_count;
+	    string suffix( to_string< int >( m_bot_count ) );
+	    string agent_name = config.read( "name" + suffix, "robot" + suffix );
     
-	    Agent* agent = m_kernel->CreateAgent( agent_name.str().c_str() );
+	    Agent* agent = m_kernel->CreateAgent( agent_name.c_str() );
+	    if ( m_kernel->HadError() )
+	    {
+	    	throw soar_error( m_kernel->GetLastErrorDescription() );
+	    }
 	    assert( agent );
 	    
-	    m_bot_list.push_back( new SoarPlayerBot( 6666 + m_bot_count, *agent, productions ) );
+	    int port = config.read( "port" + suffix, 6666 + m_bot_count );
+	    
+	    m_bot_list.push_back( new SoarPlayerBot( port, *agent, config.read< string >( "productions" + suffix ) ) );
 	    m_bot_count += 1;
     }
     
@@ -56,7 +90,7 @@ SoarPlayerClient::SoarPlayerClient( const std::string& productions, int number_o
 
 SoarPlayerClient::~SoarPlayerClient()
 {
-	for ( std::vector< SoarPlayerBot* >::iterator iter = m_bot_list.begin(); iter != m_bot_list.end(); ++iter )
+	for ( vector< SoarPlayerBot* >::iterator iter = m_bot_list.begin(); iter != m_bot_list.end(); ++iter )
 	{
 		delete *iter;
 	}
@@ -72,7 +106,7 @@ SoarPlayerClient::~SoarPlayerClient()
 
 void SoarPlayerClient::update()
 {
-	for ( std::vector< SoarPlayerBot* >::iterator iter = m_bot_list.begin(); iter != m_bot_list.end(); ++iter )
+	for ( vector< SoarPlayerBot* >::iterator iter = m_bot_list.begin(); iter != m_bot_list.end(); ++iter )
 	{
 		(*iter)->update();
 	}
--- soar-robotics/player-client/SoarPlayerClient.h
+++ soar-robotics/player-client/SoarPlayerClient.h
@@ -2,20 +2,22 @@
 #define SOAR_PLAYER_CLIENT_H
 
 #include "sml_Client.h"
-#include "RunThread.h"
 #include "SoarRunThreadInterface.h"
+#include "ConfigFile.h"
 
 #include <libplayerc++/playerc++.h>
 #include <string>
+#include <exception>
 
 class InputLinkManager;
 class OutputLinkManager;
 class SoarPlayerBot;
+class RunThread;
 
 class SoarPlayerClient: public SoarRunThreadInterface
 {
 public:
-    SoarPlayerClient( const std::string& productions, int number_of_bots );
+    SoarPlayerClient( ConfigFile& config );
     virtual ~SoarPlayerClient();
     
     virtual std::string command_run();
@@ -31,6 +33,7 @@ public:
 private:
     bool update_and_check_running();
 
+	ConfigFile& m_config;
     sml::Kernel* m_kernel;
     
 	int m_bot_count;	
@@ -39,6 +42,15 @@ private:
     RunThread* m_run_thread;
     
     bool m_stop_issued;
+    
+public:
+	struct soar_error: public std::exception
+	{
+		const char* message;
+		soar_error( const char* error_message )
+		: message( error_message ) {}
+		virtual const char* what() { return message; }
+	};
 };
 
 #endif // SOAR_PLAYER_CLIENT_H
