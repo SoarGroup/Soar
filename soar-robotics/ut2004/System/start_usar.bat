801a9937971dcf84d7c30128fcb66afe856887de
Jon Voigt <voigtjr@gmail.com>
2008-08-01 20:42:34 +0000

    ut files

---
+++ soar-robotics/ut2004/System/start_usar.bat
@@ -0,0 +1,2 @@
+start c:\ut2004\system\ut2004 big_island?game=USARBot.USARDeathMatch?spectatoronly=1?TimeLimit=0?quickstart=true -ini=usarsim.ini
+rem start c:\ut2004\system\ut2004 DM-TallTestWorld_250?game=USARBot.USARDeathMatch?spectatoronly=1?TimeLimit=0?quickstart=true -ini=usarsim.ini
