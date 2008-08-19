cfb7fc4c3a93477f5b89196dd438b580437321da
Nate Derbinsky <nlderbin@umich.edu>
2008-08-19 16:38:32 +0000

    moved/altered Soar-RL demos, modified tutorial, modified debugger menu to add RL waterjug

--- SoarSuite/Tools/SoarJavaDebugger/src/menu/DemoMenu.java
+++ SoarSuite/Tools/SoarJavaDebugger/src/menu/DemoMenu.java
@@ -36,6 +36,8 @@ public class DemoMenu
 	private AbstractAction m_TOH_Recursive  = new AbstractAction("Towers of Hanoi Recursive")
 				{ public void actionPerformed(ActionEvent e) { loadDemo(new File("towers-of-hanoi","towers-of-hanoi-recursive.soar")) ; } } ;
 
+	private AbstractAction m_Waterjug_RL = new AbstractAction("Water Jug RL")	
+				{ public void actionPerformed(ActionEvent e) { loadDemo(new File("water-jug","water-jug-rl.soar")) ; } } ;
 	private AbstractAction m_Waterjug_Lookahead = new AbstractAction("Water Jug Lookahead")	
 				{ public void actionPerformed(ActionEvent e) { loadDemo(new File("water-jug","water-jug-look-ahead.soar")) ; } } ;
 	private AbstractAction m_Waterjug = new AbstractAction("Water Jug")	
@@ -98,6 +100,7 @@ public class DemoMenu
 		BaseMenu water = menu.addSubmenu("Water Jug") ;
 		water.add(m_Waterjug) ;
 		water.add(m_Waterjug_Lookahead) ;
+		water.add(m_Waterjug_RL) ;
 
 		menu.add(m_Arithmetic) ;
 		
