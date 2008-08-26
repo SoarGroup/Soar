f24870fdf4c9ddb5e46d74ecef756a1cd8f69910
Nate Derbinsky <nlderbin@umich.edu>
2008-08-26 20:47:22 +0000

    darwin sensitive

--- Resources/run-Eaters.sh
+++ Resources/run-Eaters.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+platform=$(uname)
+
 cd SoarSuite/Environments/Soar2D
 export DYLD_LIBRARY_PATH="../../SoarLibrary/lib"
-java -XstartOnFirstThread -jar Soar2D.jar eaters.xml
 
+if [ $platform = "Darwin" ]
+then
+	java -XstartOnFirstThread -jar Soar2D.jar eaters.xml
+else
+	java -jar Soar2D.jar eaters.xml
+fi
