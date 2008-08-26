49f5102a01e05a4c944235c560b4e30110f667b0
Jon Voigt <voigtjr@gmail.com>
2008-08-26 16:21:21 +0000

    updated

--- buildJavaApps.bat
+++ buildJavaApps.bat
@@ -6,16 +6,34 @@ rem Also, if there are spaces in the path, it must be wrapped in quotes
 
 cd Environments\JavaMissionaries
 call build.bat --nopause %1
+if errorlevel 1 goto fail
+
 cd ..\JavaTOH
 call build.bat --nopause %1
+if errorlevel 1 goto fail
+
 cd ..\Soar2D
 call build.bat --nopause %1
+if errorlevel 1 goto fail
+
 cd ..\..\Tools\LoggerJava
 call build.bat --nopause %1
+if errorlevel 1 goto fail
+
 cd ..\SoarJavaDebugger
 call build.bat --nopause %1
+if errorlevel 1 goto fail
+
 cd ..\TestJavaSML
 call build.bat --nopause %1
+if errorlevel 1 goto fail
+
 cd ..\VisualSoar
 call build.bat --nopause %1
+if errorlevel 1 goto fail
+
 cd ..\..
+exit /b 0
+
+:fail
+exit /b 1
--- rebuild-all.bat
+++ rebuild-all.bat
@@ -1,12 +1,19 @@
+@echo off
 @rem TODO: exit with non-zero value on failure!
 
 @rem Step 1: Set up build environment
 call "%VS80COMNTOOLS%\vsvars32.bat"
 
 @rem Step 2: Build C++
-devenv /rebuild Distribution SML.sln
+devenv /rebuild "Distribution SCU" SML.sln
+if not errorlevel 0 goto fail
 devenv /build Release Tools\TestCSharpSML\TestCSharpSML.sln
-@rem devenv /rebuild Distribution-libs SML.sln
+if not errorlevel 0 goto fail
 
 @rem Step 3: Build Java
 call buildJavaApps.bat
+if not errorlevel 0 goto fail
+exit /b 0
+
+:fail
+exit /b 1
