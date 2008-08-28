e81c1f9d2ec8beca2f89b0f935c8283677bc29d7
Jon Voigt <voigtjr@gmail.com>
2008-08-28 18:42:38 +0000

    Added parameter for custom java version

--- rebuild-all.bat
+++ rebuild-all.bat
@@ -1,5 +1,8 @@
 @echo off
-@rem TODO: exit with non-zero value on failure!
+
+rem %1 is an optional command line argument specifying the java bin directory to use to build everything
+rem NOTE: if this directory is specified, the trailing slash must be included!
+rem Also, if there are spaces in the path, it must be wrapped in quotes
 
 @rem Step 1: Set up build environment
 call "%VS80COMNTOOLS%\vsvars32.bat"
@@ -11,7 +14,7 @@ devenv /build Release Tools\TestCSharpSML\TestCSharpSML.sln
 if not errorlevel 0 goto fail
 
 @rem Step 3: Build Java
-call buildJavaApps.bat
+call buildJavaApps.bat %1
 if not errorlevel 0 goto fail
 exit 0
 
