dfa2c670df47a5540a7c3a0b27ccef0e4c1bc1a3
Sam Wintermute <sam.wintermute@gmail.com>
2008-09-12 18:12:22 +0000

    map update

--- soar-robotics/ut2004/System/start_usar.bat
+++ soar-robotics/ut2004/System/start_usar.bat
@@ -1,2 +1,2 @@
-start c:\ut2004\system\ut2004 big_island?game=USARBot.USARDeathMatch?spectatoronly=1?TimeLimit=0?quickstart=true -ini=usarsim.ini
+start c:\ut2004\system\ut2004 big_island_block_hide_3?game=USARBot.USARDeathMatch?spectatoronly=1?TimeLimit=0?quickstart=true -ini=usarsim.ini
 rem start c:\ut2004\system\ut2004 DM-TallTestWorld_250?game=USARBot.USARDeathMatch?spectatoronly=1?TimeLimit=0?quickstart=true -ini=usarsim.ini
