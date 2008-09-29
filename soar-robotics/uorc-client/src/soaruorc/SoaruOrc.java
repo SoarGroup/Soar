1dbd7e87f54afcbca0cad9af5d4ad6a1b2c04033
Jon Voigt <voigtjr@gmail.com>
2008-09-29 16:02:29 +0000

    updates

--- soar-robotics/uorc-client/src/soaruorc/SoaruOrc.java
+++ soar-robotics/uorc-client/src/soaruorc/SoaruOrc.java
@@ -18,10 +18,17 @@ public class SoaruOrc implements Kernel.UpdateEventInterface
 	private FloatElement override_move_left;
 	private FloatElement override_move_right;
 	private FloatElement self_motor_left_current;
+	private FloatElement self_motor_left_position;
+	private FloatElement self_motor_left_velocity;
 	private FloatElement self_motor_right_current;
+	private FloatElement self_motor_right_position;
+	private FloatElement self_motor_right_velocity;
 	
 	private boolean useGamePad = true;
 	private boolean useRobot = true;
+	
+	private static final double tickMeters = 0.0000429250;
+	private static final double baselineMeters = 0.4475;
 	
 	public SoaruOrc()
 	{
@@ -43,6 +50,8 @@ public class SoaruOrc implements Kernel.UpdateEventInterface
 			System.exit(1);
 		}
 
+		kernel.SetAutoCommit( false );
+		
 		agent = kernel.CreateAgent( "soar" );
 		if ( kernel.HadError() )
 		{
@@ -68,20 +77,36 @@ public class SoaruOrc implements Kernel.UpdateEventInterface
 		}
 		
 		// self
+		//     geometry
+		//         baseline
 		//     motor
 		//         left
 		//             current
+		//             position
+		//             velocity
 		//         right 
 		//             current
+		//             position
+		//             velocity
 		{
 			Identifier self = agent.CreateIdWME( agent.GetInputLink(), "self" );
+
+			Identifier self_geometry = agent.CreateIdWME( self, "geometry" );
+			agent.CreateFloatWME( self_geometry, "baseline", baselineMeters );
+
 			Identifier self_motor = agent.CreateIdWME( self, "motor" );
 			Identifier self_motor_left = agent.CreateIdWME( self_motor, "left" );
 			Identifier self_motor_right = agent.CreateIdWME( self_motor, "right" );
 
 			self_motor_left_current = agent.CreateFloatWME( self_motor_left, "current", 0 );
+			self_motor_left_position = agent.CreateFloatWME( self_motor_left, "position", 0 );
+			self_motor_left_velocity = agent.CreateFloatWME( self_motor_left, "velocity", 0 );
 			self_motor_right_current = agent.CreateFloatWME( self_motor_right, "current", 0 );
+			self_motor_right_position = agent.CreateFloatWME( self_motor_right, "position", 0 );
+			self_motor_right_velocity = agent.CreateFloatWME( self_motor_right, "velocity", 0 );
 		}
+		
+		agent.Commit();
 
 		kernel.RegisterForUpdateEvent( smlUpdateEventId.smlEVENT_AFTER_ALL_GENERATED_OUTPUT, this, null );
 		
@@ -133,14 +158,33 @@ public class SoaruOrc implements Kernel.UpdateEventInterface
 
 		if ( useRobot )
 		{
-			double leftCurrent, rightCurrent;
+			double leftCurrent, leftPosition, leftVelocity;
+			double rightCurrent, rightPosition, rightVelocity;
+			
 			synchronized ( botState )
 			{
 				leftCurrent = botState.leftCurrent;
+				leftPosition = botState.leftPosition;
+				leftVelocity = botState.leftVelocity;
+
 				rightCurrent = botState.rightCurrent;
+				rightPosition = botState.rightPosition;
+				rightVelocity = botState.rightVelocity;
 			}
+			
+			leftPosition = leftPosition * tickMeters;
+			leftVelocity = leftVelocity * tickMeters;
+			
 			agent.Update( self_motor_left_current, leftCurrent );
+			agent.Update( self_motor_left_position, leftPosition );
+			agent.Update( self_motor_left_velocity, leftVelocity );
+
+			rightPosition = rightPosition * tickMeters;
+			rightVelocity = rightVelocity * tickMeters;
+			
 			agent.Update( self_motor_right_current, rightCurrent );
+			agent.Update( self_motor_right_position, rightPosition );
+			agent.Update( self_motor_right_velocity, rightVelocity );
 		}
 		
 		// process output
@@ -217,6 +261,7 @@ public class SoaruOrc implements Kernel.UpdateEventInterface
 		}
 		
 		//System.out.printf( "%15f %15f %15f %15f\r", leftInput, rightInput, leftCommand, rightCommand );
+		agent.Commit();
 	}
 	
 	public static void main(String args[])
--- soar-robotics/uorc-client/src/soaruorc/uOrcThread.java
+++ soar-robotics/uorc-client/src/soaruorc/uOrcThread.java
@@ -8,6 +8,8 @@ public class uOrcThread extends Thread
 	private Orc orc;
 	private Motor leftMotor;
 	private Motor rightMotor;
+	private QuadratureEncoder leftOdom;
+	private QuadratureEncoder rightOdom;
 
 	public class BotState
 	{
@@ -17,7 +19,12 @@ public class uOrcThread extends Thread
 		
 		// state output
 		public double leftCurrent = 0;
+		public int leftPosition = 0;
+		public double leftVelocity = 0;
+		
 		public double rightCurrent = 0;
+		public int rightPosition = 0;
+		public double rightVelocity = 0;
 	}
 	private BotState state = new BotState();
 	
@@ -26,23 +33,39 @@ public class uOrcThread extends Thread
 	public uOrcThread()
 	{
 		orc = Orc.makeOrc();
+		
 		leftMotor = new Motor( orc, 1, true );
+		leftOdom = new QuadratureEncoder( orc, 1, true);
+		
 		rightMotor = new Motor( orc, 0, false );
+		rightOdom = new QuadratureEncoder( orc, 0, false);
 	}
 	
 	public void run()
 	{
 		while ( running ) {
 			double left, right;
+
 			double leftCurrent = leftMotor.getCurrent();
+			int leftPosition = leftOdom.getPosition();
+			double leftVelocity = leftOdom.getVelocity();
+
 			double rightCurrent = rightMotor.getCurrent();
+			int rightPosition = rightOdom.getPosition();
+			double rightVelocity = rightOdom.getVelocity();
 			
 			synchronized ( state )
 			{
 				left = state.left;
 				right = state.right;
+				
 				state.leftCurrent = leftCurrent;
+				state.leftPosition = leftPosition;
+				state.leftVelocity = leftVelocity;
+				
 				state.rightCurrent = rightCurrent;
+				state.rightPosition = rightPosition;
+				state.rightVelocity = rightVelocity;
 			}
 			
 			// write new commands
