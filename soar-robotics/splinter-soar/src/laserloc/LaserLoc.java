19d103632847bb6cb2baa942b1d2c5753ef23b55
Jon Voigt <voigtjr@gmail.com>
2008-10-03 17:25:45 +0000

    lcm layer partially implemented, untested

--- soar-robotics/splinter-soar/src/laserloc/LaserLoc.java
+++ soar-robotics/splinter-soar/src/laserloc/LaserLoc.java
@@ -1,13 +1,16 @@
 package laserloc;
 
 import java.awt.Point;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
+import lcm.lcm.*;
 import lcmtypes.*;
 
-public class LaserLoc extends Thread 
+// TODO: don't assert
+public class LaserLoc implements LCMSubscriber
 {
-	
 	static
 	{
 		initializeLaserData();
@@ -26,6 +29,8 @@ public class LaserLoc extends Thread
 	public static double laser_yaw_adjust = 0; // amount to adjust for laser's yaw = 90 - laser's yaw = 0 if laser is facing positive y directly
 	public static double laser_dist_adjustment = 0; // radius of tube?
 	public static double translation_threshold = 0.05; // minimum translation distance to update x,y location, 0.05 total guess
+	public static String laser_channel = "LASER";
+	public static String pose_channel = "POSE";
 
 	// Even though this comes in each message, it may help the compiler optimize 
 	// if we make it a separate constant (can assert on it not matching the message)
@@ -33,86 +38,86 @@ public class LaserLoc extends Thread
 	// end constants
 	
 	// regular state
-	pose_t old_estimated_pose = new pose_t();
-	pose_t new_estimated_pose = new pose_t();
+	pose_t estimated_pose;
 	// TEMPORARY: TODO: remove, incorporate in to new_estimated_pose
 	double estimated_pose_yaw = 0;	// no old estimated yaw, we don't use the old one to calculate new 
 
-	boolean running = true;
+	static boolean testing = false;
+	
+	LCM lcm;
 	
 	public LaserLoc( /* config params? */ )
 	{
 		// ?? initialize?
-	}
-
-	
-	public void run()
-	{
-//		UpdatePose:
-//			robot_yaw = robot_starting_yaw
-		estimated_pose_yaw = robot_starting_yaw;
 		
-//			[old_robot_x, old_robot_y] = GetRobotXY()
-		old_estimated_pose = getRobotXY( getLaserData() );
-		System.out.print( "initial: " );
-		printOldPose();
-		
-		while ( running )
+		if ( !testing )
 		{
-//			loop forever (or until shutdown)
-//			[new_robot_x, new_robot_y] = GetRobotXY()
-			new_estimated_pose = getRobotXY( getLaserData() );
-			if ( new_estimated_pose == null )
-			{
-				// TODO: possibly sleep?
-				continue;
-			}
-			
-//			translation_dist = distance(old_robot_x, old_robot_y, new_robot_x, new_robot_y)
-			double translation_dist = Point.distance( old_estimated_pose.pos[ 0 ], old_estimated_pose.pos[ 1 ], new_estimated_pose.pos[ 0 ], new_estimated_pose.pos[ 1 ] );
-			System.out.print( "Moved " + translation_dist + " meters. " );
-			
-//			// only update location if moved enough. Don't want Soar to thrash on constantly changing x,y due to noise
-//			// this also means this loop can run as fast as it can and we'll still get reasonable updates
-//			// (i.e., we don't have to worry about robot not moving enough between updates)
-			if( translation_dist >= translation_threshold )
-			{
-//				robot_yaw = tan-1 (new_robot_y - old_robot_y)/(new_robot_x - old_robot_x)
-				estimated_pose_yaw = Math.atan2( new_estimated_pose.pos[ 1 ] - old_estimated_pose.pos[ 1 ], new_estimated_pose.pos[ 0 ] - old_estimated_pose.pos[ 0 ] );
-//				old_robot_x = new_robot_x
-//				old_robot_y = new_robot_y
-				
-				// TODO: figure out best way to clone
-				 old_estimated_pose.pos[ 0 ] =  new_estimated_pose.pos[ 0 ];
-				 old_estimated_pose.pos[ 1 ] =  new_estimated_pose.pos[ 1 ];
-				 
-//				generate lcm message
-				 // convert yaw to pose structure
-				 // FIXME: lcm publish new_estimated_pose
-				 printOldPose();
-			}
-			else
-			{
-				System.out.println( "Skipping update (beneath threshold)" );
-			}
+			lcm = LCM.getSingleton();
+			lcm.subscribe( laser_channel, this );
 		}
-		System.out.println( "LaserLoc done" );
 	}
-
-	private void printOldPose() {
-		System.out.println( "x,y,a: " + old_estimated_pose.pos[ 0 ] + "," + old_estimated_pose.pos[ 1 ] + "," + Math.toDegrees( estimated_pose_yaw ) );
-	}
-
-	//	laser_data = reading from sick
-	// FIXME: lcm.receive laser data
-	public pose_t getRobotXY( laser_t laser_data )
+	
+	private void updatePose( laser_t laser_data )
 	{
 		if ( laser_data == null )
 		{
-			return null;
+			assert false;
+			return;
 		}
+		
+		if ( estimated_pose == null )
+		{
+			// initialize
+			estimated_pose_yaw = robot_starting_yaw;
 			
-//		[laser_index, laser_dist] = min distance from data
+			estimated_pose = getRobotXY( laser_data );
+			System.out.print( "initial: " );
+			printOldPose();
+			return;
+		}
+		
+		pose_t new_estimated_pose = getRobotXY( laser_data );
+		if ( new_estimated_pose == null )
+		{
+			return;
+		}
+		
+		double translation_dist = Point.distance( estimated_pose.pos[ 0 ], estimated_pose.pos[ 1 ], new_estimated_pose.pos[ 0 ], new_estimated_pose.pos[ 1 ] );
+		System.out.print( "Moved " + translation_dist + " meters. " );
+		
+		// only update location if moved enough. Don't want Soar to thrash on constantly changing x,y due to noise
+		// this also means this loop can run as fast as it can and we'll still get reasonable updates
+		// (i.e., we don't have to worry about robot not moving enough between updates)
+		if( translation_dist >= translation_threshold )
+		{
+			estimated_pose_yaw = Math.atan2( new_estimated_pose.pos[ 1 ] - estimated_pose.pos[ 1 ], new_estimated_pose.pos[ 0 ] - estimated_pose.pos[ 0 ] );
+			
+			// TODO: figure out best way to clone
+			estimated_pose.pos[ 0 ] =  new_estimated_pose.pos[ 0 ];
+			estimated_pose.pos[ 1 ] =  new_estimated_pose.pos[ 1 ];
+			 
+			printOldPose();
+			
+			// TODO: convert yaw to pose structure
+			// TODO: publish lcm message\
+			estimated_pose.utime = System.nanoTime() / 1000;
+			lcm.publish( "POSE", estimated_pose );
+		}
+		else
+		{
+			System.out.println( "Skipping update (beneath threshold)" );
+		}
+	}
+
+	private void printOldPose() 
+	{
+		System.out.println( "x,y,a: " + estimated_pose.pos[ 0 ] + "," + estimated_pose.pos[ 1 ] + "," + Math.toDegrees( estimated_pose_yaw ) );
+	}
+
+	private pose_t getRobotXY( laser_t laser_data )
+	{
+		assert laser_data != null;
+			
 		double smallest_range = Double.MAX_VALUE;
 		int smallest_range_index = -1;
 		for ( int index = 0; index < laser_data.nranges; ++index )
@@ -125,27 +130,36 @@ public class LaserLoc extends Thread
 		}
 		assert smallest_range_index != -1;
 		
-//		laser_angle = laser_yaw_adjust + laser_base_angle + laser_delta_angle*laser_index
 		double laser_angle = laser_yaw_adjust + laser_data.rad0 + laser_data.radstep * smallest_range_index;
 		
-//		laser_dist = laser_dist + laser_dist_adjustment
 		double laser_dist = smallest_range + laser_dist_adjustment;
 		
 		pose_t new_pose = new pose_t();
-//		robot_x = laser_x + laser_dist * cos(laser_angle)
 		new_pose.pos[ 0 ] = laser_x + laser_dist * Math.cos( laser_angle );
-
-//		robot_y = laser_y + laser_dist * sin(laser_angle)
 		new_pose.pos[ 1 ] = laser_y + laser_dist * Math.sin( laser_angle );
 		
 		return new_pose;
+	}
+
+	@Override
+	public void messageReceived( LCM lcm, String channel, DataInputStream ins ) 
+	{
+		if ( channel.equals( laser_channel ) )
+		{
+	         try 
+	         {
+	        	 updatePose( new laser_t( ins ) );
+	          } 
+	         catch ( IOException ex ) 
+	          {
+	             System.out.println( "Error decoding laser message: " + ex );
+	          }
+		}
 		
 	}
 
 	static laser_t[][] test_data;
-	static int current_test_data_index = 0; 
-	static int current_test_index = 0; 
-	public static void initializeLaserData()
+	private static void initializeLaserData()
 	{
 		test_data = new laser_t[2][];
 		for ( int test_index = 0; test_index < test_data.length; ++test_index )
@@ -202,10 +216,10 @@ public class LaserLoc extends Thread
 		long time = -1;
 		int nranges = 180;
 
-		int start_range_index = 45;
+		int start_range_index = 60;
 		float data_range = 1;
 		
-		test_data[ THIS_TEST ] = new laser_t[ 90 ]; 
+		test_data[ THIS_TEST ] = new laser_t[ 60 ]; 
 		for ( int index = 0; index < test_data[ THIS_TEST ].length; ++index )
 		{
 			laser_t data = new laser_t();
@@ -222,44 +236,33 @@ public class LaserLoc extends Thread
 		}
 	}
 
-	public laser_t getLaserData() 
-	{
-		if ( current_test_data_index >= test_data[ current_test_index ].length )
-		{
-			running = false;
-			return null;
-		}
-		
-		System.out.print( "Test data " + current_test_data_index + ": ");
-		return test_data[ current_test_index ][ current_test_data_index++ ];
-	}
-
 	public static void main(String[] args) 
 	{
-		// TODO: configure:
-		//  robot_starting_yaw
+		// TODO: configuration of constants from command line params
+		// TODO: testing switch from command line params
 
-		// TODO: currently defaulting to test mode, should have some kind of switch
 		LaserLoc lloc;
-		while ( current_test_index < test_data.length )
+		if ( testing )
 		{
-			current_test_data_index = 0;
-			System.out.println( "Starting test " + ( current_test_index + 1 ) );
-			lloc = new LaserLoc();
-			lloc.start();
-			
-			while ( lloc.isAlive() )
+			for ( int current_test_index = 0; current_test_index < test_data.length; ++current_test_index )
 			{
-				try 
+				System.out.println( "Starting test " + ( current_test_index + 1 ) );
+				lloc = new LaserLoc();
+
+				for ( int current_test_data_index = 0; current_test_data_index < test_data[ current_test_index ].length; ++current_test_data_index )
 				{
-					Thread.sleep( 500 );
-				} 
-				catch ( InterruptedException ignored ) 
-				{}
+					System.out.print( "Test data " + current_test_data_index + ": ");
+					lloc.updatePose( test_data[ current_test_index ][ current_test_data_index ] );
+				}
+				
 			}
-			++current_test_index;
+			System.out.println( "All tests done." );
+			System.exit( 0 );
 		}
-		System.out.println( "All tests done." );
+		
+		// Not testing
+		new LaserLoc();
+		System.exit( 0 );
 	}
 
 }
