d0503f90320b5ede9eb2d141b5175c92c82a00a2
Jon Voigt <voigtjr@gmail.com>
2009-02-07 19:17:27 +0000

    commented out session slave to remove warnings

--- soar-robotics/sps/src/sps/sm/SessionSlave.java
+++ soar-robotics/sps/src/sps/sm/SessionSlave.java
@@ -1,21 +1,21 @@
 package sps.sm;
 
-import java.io.IOException;
-import java.net.Socket;
+//import java.io.IOException;
+//import java.net.Socket;
 
 import org.apache.log4j.Logger;
 
 public class SessionSlave {
-	private static Logger logger = Logger.getLogger(SessionSlave.class);
+//	private static Logger logger = Logger.getLogger(SessionSlave.class);
 
 	SessionSlave(String component, String hostname) {
 		// connect to a SessionManager master at hostname
-		logger.info("Connecting as " + component + "@" + hostname + ":" + SessionManager.PORT);
-		try {
-			Socket sock = new Socket(hostname, SessionManager.PORT);
-		} catch (IOException e) {
-			logger.error(e.getMessage());
-			System.exit(1);
-		}
+//		logger.info("Connecting as " + component + "@" + hostname + ":" + SessionManager.PORT);
+//		try {
+//			Socket sock = new Socket(hostname, SessionManager.PORT);
+//		} catch (IOException e) {
+//			logger.error(e.getMessage());
+//			System.exit(1);
+//		}
 	}
 }
