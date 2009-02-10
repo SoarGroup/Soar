7bce611e52545ba86c73b2961e05caf1c61470f8
Jon Voigt <voigtjr@gmail.com>
2009-02-10 21:20:13 +0000

    updated to make conversion easier

--- Scripts/src/map-cleanup.py
+++ Scripts/src/map-cleanup.py
@@ -1,18 +1,34 @@
 import os
 import time
+import sys
 
 print os.getcwd()
 filename = raw_input("filename:")
 
 file = open(filename, "r")
 
+count = 0
+newRow = False;
 for line in file:
     line = line.strip()
+    
     if len(line) == 0:
         continue;
-    found_row = line.find(r'<row')
-    found_cell = line.find(r'<cell>')
-    found_object = line.find(r'<object')
-    
-    if found_row != -1 or found_cell != -1 or found_object != -1:
-        print line
+    if line.find(r'<row') != -1:
+    	newRow = True;
+    	print "];"
+    	print count, "=","[",
+    	count += 1
+    elif line.find(r'<cell') != -1:
+    	if not newRow:
+	    	sys.stdout.write(',')
+    elif line.find(r'<object') != -1:
+    	newRow = False
+        if line.find('trees') != -1:
+	    	sys.stdout.write('t')
+        elif line.find('rocks') != -1:
+        	sys.stdout.write('r')
+        elif line.find('ground') != -1:
+        	sys.stdout.write('g')
+
+print "];"
