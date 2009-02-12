53ccb32b80d13b425807398483e4f5356f300cd2
Jon Voigt <voigtjr@gmail.com>
2009-02-12 22:29:03 +0000

    added old odometry function, used it in plotter

--- soar-robotics/sps/src/org/msoar/sps/Odometry.java
+++ soar-robotics/sps/src/org/msoar/sps/Odometry.java
@@ -51,4 +51,37 @@ public class Odometry {
 			logger.trace(String.format("odom: n(%d,%d) o(%d,%d) p(%5.2f,%5.2f,%5.1f)", newOdom.left, newOdom.right, oldOdom.left, oldOdom.right, pose.pos[0], pose.pos[1], theta));
 		}
 	}
+	
+	public void propagate2(odom_t newOdom, odom_t oldOdom, pose_t pose) {
+		//System.out.println("delta left ticks: " + (newOdom.left - oldOdom.left));
+		//System.out.println("delta right ticks: " + (newOdom.right - oldOdom.right));
+		double dleft = (newOdom.left - oldOdom.left) * tickMeters;
+		double dright = (newOdom.right - oldOdom.right) * tickMeters;
+		
+		// phi
+		deltaxyt[2] = MathUtil.mod2pi((dright - dleft) / baselineMeters);
+		
+		double dCenter = (dleft + dright) / 2;
+		
+		// our current theta
+		double theta = LinAlg.quatToRollPitchYaw(pose.orientation)[2];
+		
+		// delta x, delta y
+		deltaxyt[0] = dCenter * Math.cos(theta);
+		deltaxyt[1] = dCenter * Math.sin(theta);
+		
+		// calculate and store new xyt
+		pose.pos[0] += deltaxyt[0];
+		pose.pos[1] += deltaxyt[1];
+		theta += deltaxyt[2];
+		
+		// convert theta to quat and store
+		pose.orientation = LinAlg.rollPitchYawToQuat(new double[] {0, 0, theta});
+		
+		if (logger.isTraceEnabled()) {
+			theta = MathUtil.mod2pi(theta);
+			theta = Math.toDegrees(theta);
+			logger.trace(String.format("odom: n(%d,%d) o(%d,%d) p(%5.2f,%5.2f,%5.1f)", newOdom.left, newOdom.right, oldOdom.left, oldOdom.right, pose.pos[0], pose.pos[1], theta));
+		}
+	}
 }
