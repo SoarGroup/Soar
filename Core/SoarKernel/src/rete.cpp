920b6a3d6771eb241c6a37a5fa3f022b70d2bc0c
Nate Derbinsky <nlderbin@umich.edu>
2009-03-27 13:15:02 +0000

    tabbing

--- Core/SoarKernel/src/rete.cpp
+++ Core/SoarKernel/src/rete.cpp
@@ -1637,7 +1637,7 @@ void remove_wme_from_rete (agent* thisAgent, wme *w) {
   {
     if ( thisAgent->epmem_db_status != EPMEM_DB_CLOSED )
 	{
-		if ( thisAgent->epmem_params->mode->get_value() == epmem_param_container::graph )
+	  if ( thisAgent->epmem_params->mode->get_value() == epmem_param_container::graph )
 	  {
 	    if ( w->value->common.symbol_type == IDENTIFIER_SYMBOL_TYPE )
 	    {
