e20ec527a4838a0bcf4f01b1085b1a2ab1884069
Nate Derbinsky <nlderbin@umich.edu>
2009-03-28 17:23:06 +0000

    implemented wme addition/removal for soar modules.  impasse wmes work only one level deep (in auto garbage collection).  input_wmes are really intended for environments/input-phase.  module_wmes are intended as a way for future modules within the kernel to add/remove easily and get all the benefits of rule-generated wmes.
    reward-link is now added this way.

--- SoarSuite/Core/SoarKernel/src/soar_module.cpp
+++ SoarSuite/Core/SoarKernel/src/soar_module.cpp
@@ -22,6 +22,30 @@
 namespace soar_module
 {
 	/////////////////////////////////////////////////////////////
+	// Utility functions
+	/////////////////////////////////////////////////////////////
+
+	wme *add_module_wme( agent *my_agent, Symbol *id, Symbol *attr, Symbol *value )
+	{
+		slot *my_slot = make_slot( my_agent, id, attr );
+		wme *w = make_wme( my_agent, id, attr, value, false );
+
+		insert_at_head_of_dll( my_slot->wmes, w, next, prev );
+		add_wme_to_wm( my_agent, w );
+
+		return w;
+	}
+
+	void remove_module_wme( agent *my_agent, wme *w )
+	{
+		slot *my_slot = find_slot( w->id, w->attr );
+
+		remove_from_dll( my_slot->wmes, w, next, prev );
+		remove_wme_from_wm( my_agent, w );
+	}
+	
+	
+	/////////////////////////////////////////////////////////////
 	// predicate
 	/////////////////////////////////////////////////////////////
 	template <typename T>
