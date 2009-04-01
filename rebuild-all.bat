4f225e6815e554fd543cb3f468b397c5d2243427
Jon Voigt <voigtjr@gmail.com>
2009-04-01 14:43:59 +0000

    Merged ant branch to trunk: -ant is now used as the java builder instead of windows batch files -ant is used instead of scons on scons platforms -ant build files manage jar creation, including sml.jar -visual studio java swig project calls ant in post build step making the whole build one step -rebuild all updated to not call java build -old java build files removed -old jar manifest and other metadata removed--included in ant build files now -cygwin platform officially unsupported again -old, unused classpaths removed -library jars moved to SoarLibrary/lib -eclipse project updated for new jar locations -wget.exe added so that windows build automatically fetches swt.jar if it doesn't exist -visual soar source folder moved to match other java projects -cosmetic bug with java build folder svn:ignore, will fix in future revision

--- rebuild-all.bat
+++ rebuild-all.bat
@@ -1,20 +1,13 @@
 @echo off
 
-rem %1 is an optional command line argument specifying the java bin directory to use to build everything
-rem NOTE: if this directory is specified, the trailing slash must be included!
-rem Also, if there are spaces in the path, it must be wrapped in quotes
-
-@rem Step 1: Set up build environment
 call "%VS80COMNTOOLS%\vsvars32.bat"
 
-@rem Step 2: Build C++
+call %ANT_HOME%\bin\ant clean
+
 devenv /rebuild "Distribution SCU" SML.sln
 if not errorlevel 0 goto fail
-devenv /build Release Tools\TestCSharpSML\TestCSharpSML.sln
-if not errorlevel 0 goto fail
 
-@rem Step 3: Build Java
-call buildJavaApps.bat %1
+devenv /build Release Tools\TestCSharpSML\TestCSharpSML.sln
 if not errorlevel 0 goto fail
 exit 0
 
