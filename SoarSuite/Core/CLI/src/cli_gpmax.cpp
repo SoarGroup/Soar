972f944a12fc859c0c284884c24fe8bc8a8047ae
Jon Voigt <voigtjr@gmail.com>
2009-05-06 19:00:45 +0000

    - moved around atomic operations code a bit - reduced number of locks to 16 from one per element xml logic - reduced redundancy in lock/unlock code - created HAVE_ATOMICS symbol that is defined if proper atomic ops get defined, otherwise atomic ops defined without locks - added gp-max command to limit the delay of the gp command for insanely large rule sets - added logic to gp command to test this limit - added error messages to gp command - added testgp.soar to misctests, removed gp that generates 540000 productions

---
+++ SoarSuite/Core/CLI/src/cli_gpmax.cpp
@@ -0,0 +1,48 @@
+/////////////////////////////////////////////////////////////////
+// gp command file.
+//
+// Author: Jonathan Voigt, voigtjr@gmail.com, Bob Marinier
+// Date  : 2008
+//
+/////////////////////////////////////////////////////////////////
+
+#include <portability.h>
+
+#include "cli_CommandLineInterface.h"
+
+#include "cli_Commands.h"
+#include "cli_CLIError.h"
+
+using namespace cli;
+
+bool CommandLineInterface::ParseGPMax(std::vector<std::string>& argv) {
+	// n defaults to 0 (print current value)
+	long n = -1;
+
+	if (argv.size() > 2) return SetError(CLIError::kTooManyArgs);
+
+	// one argument, figure out if it is a positive integer
+	if (argv.size() == 2) {
+		from_string(n, argv[1]);
+		if (n < 0) return SetError(CLIError::kIntegerMustBeNonNegative);
+	}
+
+	return DoGPMax(n);
+}
+
+bool CommandLineInterface::DoGPMax(const long& maximum) {
+	if (maximum < 0) {
+		// query
+		if (m_RawOutput) {
+			m_Result << m_GPMax;
+		} else {
+			std::string temp;
+			AppendArgTagFast(sml_Names::kParamValue, sml_Names::kTypeInt, to_string(m_GPMax, temp));
+		}
+		return true;
+	}
+
+	m_GPMax = maximum;
+	return true;
+
+}
