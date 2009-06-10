cf6f758ad6b0b7d9f8f3091126ae9713a3e21a90
Jon Voigt <voigtjr@gmail.com>
2009-06-10 14:46:31 +0000

    Added comments about the Java build procedure to the rebuild-all batch files.

--- SoarSuite/rebuild-all-vs9-x64.bat
+++ SoarSuite/rebuild-all-vs9-x64.bat
@@ -2,6 +2,8 @@
 
 call "%VS90COMNTOOLS%\vsvars32.bat"
 
+rem Cleaning Java projects here, they are built as a post-build
+rem step inside of the ClientSMLJava project.
 call %ANT_HOME%\bin\ant clean
 
 devenv /rebuild "Distribution SCU|x64" SML9.sln
