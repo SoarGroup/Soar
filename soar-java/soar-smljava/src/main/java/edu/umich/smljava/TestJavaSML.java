5ec2134b2279101d9e74e2850530e6f0afaea843
Jon Voigt <voigtjr@gmail.com>
2009-07-20 19:59:41 +0000

    Created soar-java folder to house Java projects. Moved Java projects to soar-java folder: debugger, soar2d, loggerjava, missionaries, sps, testjavasml, toh, visualsoar Created new smljava project folder to house shared code and dependencies. Moved many jars from soarlibrary/lib to new smljava project. Created simple hello world for smljava project. Adjusted some build.xml files to use new Java build scheme. Deactivated Java projects that are not switched over yet. Moved source folders around in soar-debugger project to fit new build scheme.

---
+++ soar-java/soar-smljava/src/main/java/edu/umich/smljava/TestJavaSML.java
@@ -0,0 +1,12 @@
+package edu.umich.smljava;
+
+public class TestJavaSML {
+	public TestJavaSML() {
+		System.out.println("edu.umich.smljava.TestJavaSML says Hello!");
+	}
+
+	public static void main(String[] args) {
+		new TestJavaSML();
+	}
+}
+
