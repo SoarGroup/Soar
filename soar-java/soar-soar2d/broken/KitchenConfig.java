58db44686f58d7978765d425fc2862fef2a1ffcd
Jon Voigt <voigtjr@gmail.com>
2009-07-21 19:55:48 +0000

    removed old broken code tree

--- soar-java/soar-soar2d/broken/KitchenConfig.java
+++
@@ -1,8 +0,0 @@
-package org.msoar.gridmap2d.config;
-
-
-public class KitchenConfig implements GameConfig {
-	public String title() {
-		return "Kitchen";
-	}
-}
--- soar-java/soar-soar2d/broken/run-Kitchen.bat
+++
@@ -1,3 +0,0 @@
-set SOARLIB=../../SoarLibrary/bin
-set PATH=%SOARLIB%;%PATH%
-start javaw -cp log4j-1.2.15.jar;soar2d.jar soar2d.Soar2D kitchen.cnf
