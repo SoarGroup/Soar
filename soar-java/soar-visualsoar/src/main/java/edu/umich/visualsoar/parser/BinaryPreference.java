8d95f434d1fb2cd07014f0fbad0a98f3ff5ff189
Jon Voigt <voigtjr@gmail.com>
2009-07-21 20:23:15 +0000

    * Added resources to build path * Refactored to edu.umich.soar.visualsoar * Moved test agents to doc

--- soar-java/soar-visualsoar/src/main/java/edu/umich/visualsoar/parser/BinaryPreference.java
+++
@@ -1,7 +0,0 @@
-package edu.umich.visualsoar.parser;
-
-public final class BinaryPreference extends PreferenceSpecifier {
-	public BinaryPreference(int type,RHSValue rhs) {
-		super(type,rhs);
-	}
-}
