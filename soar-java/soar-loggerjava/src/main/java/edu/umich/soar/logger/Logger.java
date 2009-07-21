ee29f5892e94d691db35eeaeff0a1e9b4845391e
Jon Voigt <voigtjr@gmail.com>
2009-07-21 19:26:12 +0000

    * Refactored to edu.umich.soar.logger package. * Added resources to build path.

--- soar-java/soar-loggerjava/src/main/java/edu/umich/soar/logger/Logger.java
+++ soar-java/soar-loggerjava/src/main/java/edu/umich/soar/logger/Logger.java
@@ -9,7 +9,7 @@
 * 
 * Developed by ThreePenny Software <a href="http://www.threepenny.net">www.threepenny.net</a>
 ********************************************************************************************/
-package soarLogger;
+package edu.umich.soar.logger;
 
 import sml.* ;
 
