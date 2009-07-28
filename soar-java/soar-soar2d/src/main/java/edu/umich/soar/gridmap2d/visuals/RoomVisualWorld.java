4475a5259a8536c27746ce6a7b39bc952a815cef
Jon Voigt <voigtjr@gmail.com>
2009-07-28 18:41:55 +0000

    * OffsetPose offset is now a pose object, allowing x, y, z and rotational offsets to be set in Configure command * Messing around with project settings trying to get soar-smljava to work right. * Removed totalTime from controller, this is added up externally now. * Changed barrier centerpoints to pose_t * Changed object locations to pose_t * Moved angleOff to SoarRobot, uses pose_t instead of Player now. * Simplified area description class a bit * Started scaling of x, y positions to meters * Broke objects out of area description * Flipped draw angles so that coordinate system (x forward y left increasing angles left) works right.

--- soar-java/soar-soar2d/src/main/java/edu/umich/soar/gridmap2d/visuals/RoomVisualWorld.java
+++ soar-java/soar-soar2d/src/main/java/edu/umich/soar/gridmap2d/visuals/RoomVisualWorld.java
@@ -1,7 +1,5 @@
 package edu.umich.soar.gridmap2d.visuals;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -17,13 +15,17 @@ import edu.umich.soar.gridmap2d.Names;
 import edu.umich.soar.gridmap2d.map.CellObject;
 import edu.umich.soar.gridmap2d.players.Player;
 import edu.umich.soar.gridmap2d.players.RoomPlayer;
+import edu.umich.soar.gridmap2d.world.RoomWorld;
+import edu.umich.soar.gridmap2d.world.World;
 
 public class RoomVisualWorld extends VisualWorld {
 
 	private boolean colored_rooms = false;
+	RoomWorld world;
 	
-	public RoomVisualWorld(Composite parent, int style, int cellSize) {
+	public RoomVisualWorld(Composite parent, int style, int cellSize, World world) {
 		super(parent, style, cellSize);
+		this.world = (RoomWorld)world;
 	}
 
 	public void paintControl(PaintEvent e) {
@@ -53,18 +55,19 @@ public class RoomVisualWorld extends VisualWorld {
 			painted = false;
 
 		// Draw world
-		ArrayList<int []> playerLocs = new ArrayList<int []>();
 		int [] location = new int [2];
 		HashSet<Integer> roomIds = new HashSet<Integer>();
 		for(location[0] = 0; location[0] < map.size(); ++location[0]){
 			for(location[1] = 0; location[1] < map.size(); ++location[1]){
+				int [] drawLocation = new int [] { cellSize*location[0], cellSize*(map.size() - location[1] - 1) };
+				
 				if (!this.map.getCell(location).checkAndResetRedraw() && painted) {
 					continue;
 				}
 				
 				if (this.map.getCell(location).hasAnyWithProperty(Names.kPropertyBlock)) {
 				    gc.setBackground(WindowManager.black);
-				    gc.fillRectangle(cellSize*location[0], cellSize*location[1], cellSize, cellSize);
+				    gc.fillRectangle(drawLocation[0], drawLocation[1], cellSize, cellSize);
 					
 				} else {
 					
@@ -90,23 +93,19 @@ public class RoomVisualWorld extends VisualWorld {
 					} else {
 						gc.setBackground(WindowManager.white);
 					}
-					gc.fillRectangle(cellSize*location[0], cellSize*location[1], cellSize, cellSize);
-
-					if (this.map.getCell(location).getPlayer() != null) {
-						playerLocs.add(Arrays.copyOf(location, location.length));
-					}
+					gc.fillRectangle(drawLocation[0], drawLocation[1], cellSize, cellSize);
 				}
 
 				List<CellObject> objectIds = map.getCell(location).getAllWithProperty("object-id");
 				if (objectIds != null) {
 					gc.setForeground(WindowManager.white);
-					gc.drawString(objectIds.get(0).getProperty("object-id"), cellSize*location[0], cellSize*location[1]);
+					gc.drawString(objectIds.get(0).getProperty("object-id"), drawLocation[0], drawLocation[1]);
 				} else  {
 					List<CellObject> numbers = map.getCell(location).getAllWithProperty("number");
 					if (numbers!= null) {
 						if (!roomIds.contains(numbers.get(0).getIntProperty("number", -1))) {
 							gc.setForeground(WindowManager.black);
-							gc.drawString(numbers.get(0).getProperty("number"), cellSize*location[0], cellSize*location[1]);
+							gc.drawString(numbers.get(0).getProperty("number"), drawLocation[0], drawLocation[1]);
 							roomIds.add(numbers.get(0).getIntProperty("number", -1));
 						}
 					}
@@ -115,46 +114,51 @@ public class RoomVisualWorld extends VisualWorld {
 		}
 		
 		// draw entities now so they appear on top
-		for (int [] playerLoc : playerLocs) {
-			Player player = this.map.getCell(playerLoc).getPlayer();
-			//FIXME:
-			if (player == null) {
-				continue;
-			}
-			assert player != null;
-
-			RoomPlayer roomPlayer = (RoomPlayer)player;
-			pose_t pose = roomPlayer.getState().getPose();
-			double [] center = new double [] { pose.pos[0], pose.pos[1] };
-			double [] offset = new double [] { 0, 0 };
+		for (Player p : world.getPlayers()) {
+			RoomPlayer player = (RoomPlayer)p;
+			
+			pose_t pose = player.getState().getPose();
+			float [] center = new float [] { (float)pose.pos[0], (float)pose.pos[1] };
+			float [] offset = new float [] { 0, 0 };
 			
 			Path path = new Path(gc.getDevice());
-			double heading = roomPlayer.getState().getYaw();
+			float heading = (float)player.getState().getYaw();
 
 			// first, move to the point representing the tip of the chevron
-			offset[1] = (float)kDotSize * (float)Math.sin(heading);
-			offset[0] = (float)kDotSize * (float)Math.cos(heading);
-			double [] original = new double [] { offset[0], offset[1] };
-			path.moveTo((float)(center[0] + offset[0]), (float)(center[1] + offset[1]));
+			offset[1] = kDotSize * (float)Math.sin(heading);
+			offset[0] = kDotSize * (float)Math.cos(heading);
+			float [] original = new float [] { offset[0], offset[1] };
+			path.moveTo((center[0] + offset[0]), map.size() * cellSize - (center[1] + offset[1]));
 			//System.out.println("First: " + offset);
 
 			// next draw a line to the corner
 			offset[1] = kDotSize/2.0f * (float)Math.sin(heading + (3*Math.PI)/4);
 			offset[0] = kDotSize/2.0f * (float)Math.cos(heading + (3*Math.PI)/4);
-			path.lineTo((float)(center[0] + offset[0]), (float)(center[1] + offset[1]));
+			path.lineTo((center[0] + offset[0]), map.size() * cellSize - (center[1] + offset[1]));
 			//System.out.println("Second: " + offset);
 
 			// next draw a line to the other corner
 			offset[1] = kDotSize/2.0f * (float)Math.sin(heading - (3*Math.PI)/4);
 			offset[0] = kDotSize/2.0f * (float)Math.cos(heading - (3*Math.PI)/4);
-			path.lineTo((float)(center[0] + offset[0]), (float)(center[1] + offset[1]));				
+			path.lineTo((center[0] + offset[0]), map.size() * cellSize - (center[1] + offset[1]));				
 			//System.out.println("Third: " + offset);
 
 			// finally a line back to the original
-			path.lineTo((float)(center[0] + original[0]), (float)(center[1] + original[1]));
+			path.lineTo((center[0] + original[0]), map.size() * cellSize - (center[1] + original[1]));
 			
 			gc.setForeground(WindowManager.getColor(player.getColor()));
 			gc.drawPath(path);
 		}
 	}
+
+	@Override
+	Player getPlayerAtPixel(int [] loc) {
+		loc[1] = map.size() * cellSize - loc[1];
+		int[] xy = getCellAtPixel(loc);
+		if (xy == null) {
+			return null;
+		}
+		return this.map.getCell(xy).getPlayer();
+	}
+	
 }
