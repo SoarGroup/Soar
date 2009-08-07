a4e2662d032d949f04b1501708775ea65f04a6a3
Jon Voigt <voigtjr@gmail.com>
2009-08-07 17:23:05 +0000

    Moved map's string constants up to top level.

--- SoarSuite/soar-java/soar-soar2d/src/main/java/edu/umich/soar/gridmap2d/Names.java
+++ SoarSuite/soar-java/soar-soar2d/src/main/java/edu/umich/soar/gridmap2d/Names.java
@@ -219,4 +219,32 @@ public class Names {
 	
 	public static final String kRoomObjectMovable = "movable";
 	
+	static final String kTagMap = "map";
+	static final String kTagCellObject = "cell-object";
+	static final String kTagCells = "cells";
+	static final String kTagMetadata = "metadata";
+	static final String kTagProperty = "property";
+	static final String kTagApply = "apply";
+	static final String kTagUpdate = "update";
+	static final String kTagPoints = "points";
+	static final String kTagEnergy = "energy";
+	static final String kTagHealth = "health";
+	static final String kTagMissiles = "missiles";
+	static final String kTagFuel = "fuel";
+	static final String kTagRow = "row";
+	static final String kTagCell = "cell";
+	static final String kTagObject = "object";
+	static final String kTagDecay = "decay";
+	static final String kTagFlyMissile = "fly-missile";
+	static final String kTagLinger = "linger";
+	
+	static final String kAttrWorldSize = "world-size";
+	static final String kAttrRandomWalls = "random-walls";
+	static final String kAttrRandomFood = "random-food";
+	static final String kAttrName = "name";
+	static final String kAttrValue = "value";
+	static final String kAttrShields = "shields";
+	static final String kAttrShieldsDown = "shields-down";
+	static final String kAttrQuantity = "quantity";
+
 }
