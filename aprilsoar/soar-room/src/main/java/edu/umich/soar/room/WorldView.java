43dd2742eda1067b92d4c5650c45f236c26b1dd8
Jon Voigt <voigtjr@gmail.com>
2009-12-03 20:22:53 +0000

    experimental vis window, checking in for testing on other platforms

--- aprilsoar/soar-room/src/main/java/edu/umich/soar/room/WorldView.java
+++ aprilsoar/soar-room/src/main/java/edu/umich/soar/room/WorldView.java
@@ -1,14 +1,20 @@
 package edu.umich.soar.room;
 
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.Color;
+//import java.awt.event.MouseAdapter;
+//import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.flexdock.docking.DockingConstants;
 
-import edu.umich.soar.room.core.Simulation;
+import vis.VisCanvas;
+import vis.VisData;
+import vis.VisDataPointStyle;
+import vis.VisWorld;
+
+//import edu.umich.soar.room.core.Simulation;
 import edu.umich.soar.room.map.Robot;
 import edu.umich.soar.room.selection.SelectionManager;
 import edu.umich.soar.room.selection.SelectionProvider;
@@ -17,15 +23,17 @@ public class WorldView extends AbstractAdaptableView implements SelectionProvide
 	
 	private static final long serialVersionUID = 4612350578042898852L;
 	
-	private final GridMapPanel gridMapPanel;
-	private SelectionManager manager;
-	private final Simulation sim;
+//	private final GridMapPanel gridMapPanel;
+//	private SelectionManager manager;
+//	private final Simulation sim;
 	private Robot selectedPlayer;
+    private VisWorld vw = new VisWorld();
+    private VisCanvas vc = new VisCanvas(vw);
 
     public WorldView(Adaptable app) {
         super("worldView", "World View");
 
-		sim = Adaptables.adapt(app, Simulation.class);
+//		sim = Adaptables.adapt(app, Simulation.class);
 		ActionManager am = Adaptables.adapt(app, ActionManager.class);
 		am.getAction(ClearBreadcrumbsAction.class).setBreadcrumbsProvider(this);
 		am.getAction(ToggleBreadcrumbsAction.class).setBreadcrumbsProvider(this);
@@ -33,61 +41,75 @@ public class WorldView extends AbstractAdaptableView implements SelectionProvide
         addAction(DockingConstants.PIN_ACTION);
         addAction(DockingConstants.CLOSE_ACTION);
 
-        gridMapPanel = new RoomPanel(app);
-        setContentPane(gridMapPanel);
-        
-        gridMapPanel.addMouseListener(new MouseAdapter() {
-        	@Override
-        	public void mousePressed(MouseEvent e) {
-        		int[] xy = gridMapPanel.getCellAtPixel(e.getX(), e.getY());
-        		if (sim.getMap().isInBounds(xy)) {
-            		
-            		if (e.isPopupTrigger()) {
-            			showPopupMenu(e, xy);
-            		} else {
-                		selectPlayer(getPlayerAtCell(xy));
-            			
-            		}
-        		}
-        	}
+//        gridMapPanel = new RoomPanel(app);
+//        setContentPane(gridMapPanel);
 
-        	public void mouseReleased(MouseEvent e) {
-        		int[] xy = gridMapPanel.getCellAtPixel(e.getX(), e.getY());
-        		if (sim.getMap().isInBounds(xy)) {
-            		
-            		if (e.isPopupTrigger()) {
-            			showPopupMenu(e, xy);
-            		}
-        		}
-        	}
-        });
+        setContentPane(vc);
+
+    	ArrayList<double[]> points = new ArrayList<double[]>();
+    	points.add(new double[] {0, 0});
+    	points.add(new double[] {0, 1});
+    	points.add(new double[] {1, 1});
+    	
+    	VisData vd = new VisData(points,
+    				 new VisDataPointStyle(Color.blue, 4));
+
+    	VisWorld.Buffer vb = vw.getBuffer("mybuffer");
+    	vb.addBuffered(vd);
+    	vb.switchBuffer();
+
+//        gridMapPanel.addMouseListener(new MouseAdapter() {
+//        	@Override
+//        	public void mousePressed(MouseEvent e) {
+//        		int[] xy = gridMapPanel.getCellAtPixel(e.getX(), e.getY());
+//        		if (sim.getMap().isInBounds(xy)) {
+//            		
+//            		if (e.isPopupTrigger()) {
+//            			showPopupMenu(e, xy);
+//            		} else {
+//                		selectPlayer(getPlayerAtCell(xy));
+//            			
+//            		}
+//        		}
+//        	}
+//
+//        	public void mouseReleased(MouseEvent e) {
+//        		int[] xy = gridMapPanel.getCellAtPixel(e.getX(), e.getY());
+//        		if (sim.getMap().isInBounds(xy)) {
+//            		
+//            		if (e.isPopupTrigger()) {
+//            			showPopupMenu(e, xy);
+//            		}
+//        		}
+//        	}
+//        });
     }
 
-	private Robot getPlayerAtCell(int[] xy) {
-		return this.sim.getMap().getCell(xy).getFirstPlayer();
-	}
-	
-	private void selectPlayer(Robot player) {
-		selectedPlayer = player;
-		if (this.manager != null) {
-			manager.fireSelectionChanged();
-		}
-	}
-
-	private void showPopupMenu(MouseEvent e, int[] xy)
-    {
-		final WorldMenu menu = new WorldMenu(this, this.sim, xy);
-		menu.getPopupMenu().show(e.getComponent(), e.getX(), e.getY());
-    }
+//	private Robot getPlayerAtCell(int[] xy) {
+//		return this.sim.getMap().getCell(xy).getFirstPlayer();
+//	}
+//	
+//	private void selectPlayer(Robot player) {
+//		selectedPlayer = player;
+//		if (this.manager != null) {
+//			manager.fireSelectionChanged();
+//		}
+//	}
+//
+//	private void showPopupMenu(MouseEvent e, int[] xy)
+//    {
+//		final WorldMenu menu = new WorldMenu(this, this.sim, xy);
+//		menu.getPopupMenu().show(e.getComponent(), e.getX(), e.getY());
+//    }
 
 	@Override
 	public void activate(SelectionManager manager) {
-		this.manager = manager;
+//		this.manager = manager;
 	}
 
 	@Override
 	public void deactivate() {
-		this.manager = null;
+//		this.manager = null;
 	}
 
 	@Override
@@ -104,22 +126,70 @@ public class WorldView extends AbstractAdaptableView implements SelectionProvide
 
 	@Override
 	public void refresh() {
-		this.gridMapPanel.repaint();
+//		this.gridMapPanel.repaint();
 	}
 
 	@Override
 	public boolean areBreadcrumbsEnabled() {
-		return gridMapPanel.areBreadcrumbsEnabled();
+//		return gridMapPanel.areBreadcrumbsEnabled();
+		return false;
 	}
 
 	@Override
 	public void clearBreadcrumbs() {
-		gridMapPanel.clearBreadcrumbs();
+//		gridMapPanel.clearBreadcrumbs();
 	}
 
 	@Override
 	public void setBreadcrumbsEnabled(boolean setting) {
-		gridMapPanel.setBreadcrumbsEnabled(setting);
+//		gridMapPanel.setBreadcrumbsEnabled(setting);
 	}
 	
 }
+
+/*
+package edu.umich.soar.sps;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import vis.VisCanvas;
+import vis.VisData;
+import vis.VisDataPointStyle;
+import vis.VisWorld;
+
+public class Application {
+
+
+    JFrame jf;
+
+    public Application() {
+    	jf = new JFrame("VisExample");
+    	jf.setLayout(new BorderLayout());
+    	jf.add(vc, BorderLayout.CENTER);
+
+    	jf.setSize(600,400);
+    	jf.setVisible(true);
+
+    	ArrayList<double[]> points = new ArrayList<double[]>();
+    	points.add(new double[] {0, 0});
+    	points.add(new double[] {0, 1});
+    	points.add(new double[] {1, 1});
+    	
+    	VisData vd = new VisData(points,
+    				 new VisDataPointStyle(Color.blue, 4));
+
+    	VisWorld.Buffer vb = vw.getBuffer("mybuffer");
+    	vb.addBuffered(vd);
+    	vb.switchBuffer();
+	}
+	
+	public static void main(String[] args) {
+		new Application();
+	}
+
+}
+*/
