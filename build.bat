5a018f9e2684f1c27c0b5bb4ae3e26a2d0362210
Jon Voigt <voigtjr@gmail.com>
2010-02-01 20:31:33 +0000

    added clean argument to build bat

--- build.bat
+++ build.bat
@@ -3,17 +3,25 @@ setlocal
 
 set SOAR_HOME=%CD%\out
 set SOAR_VERSION=9.2.0
+if "%1" == "" (
+	set TARGET=build
+) else (
+	set TARGET=%1
+)
 
 echo Make sure the following environment variables are set correctly:
-echo
+echo.
 echo ANT_HOME: %ANT_HOME%
 echo JAVA_HOME: %JAVA_HOME%
 echo PYTHON_HOME: %PYTHON_HOME%
 echo SWIG_HOME: %SWIG_HOME%
-
+echo.
 echo Edit the build.bat file to check these variables:
 echo SOAR_HOME: %SOAR_HOME%
 echo SOAR_VERSION: %SOAR_VERSION%
+echo.
+echo TARGET: %TARGET%
+echo.
 
 if not defined ANT_HOME goto fail
 if not defined JAVA_HOME goto fail
@@ -24,20 +32,20 @@ call "%VS90COMNTOOLS%\vsvars32.bat"
 
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto x64
 
-devenv /build "Release SCU|Win32" Core\Core.sln
+devenv /%TARGET% "Release SCU|Win32" Core\Core.sln
 if not errorlevel 0 goto fail
 
 goto built
 
 :x64
-devenv /build "Release SCU|x64" Core\Core.sln
+devenv /%TARGET% "Release SCU|x64" Core\Core.sln
 if not errorlevel 0 goto fail
 
 :built
 
 for /f "tokens=* delims= " %%a in ('dir/b/ad') do @(
 	if exist "%%a\build.xml" (
-		ant "-Dsoarprefix=%SOAR_HOME%" "-Dversion=%SOAR_VERSION%" -f "%%a\build.xml"
+		ant "-Dsoarprefix=%SOAR_HOME%" "-Dversion=%SOAR_VERSION%" -f "%%a\build.xml" %TARGET%
 	)
 )
 
