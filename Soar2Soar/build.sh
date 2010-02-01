9d239b3cc662c12cc49dacb1a2ce69a3e4c520c6
Jon Voigt <voigtjr@gmail.com>
2010-02-01 19:19:11 +0000

    added soar2soar to build

--- Soar2Soar/build.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-SOAR_PATH=$DYLD_LIBRARY_PATH/../..
-
-g++ -L$SOAR_PATH/SoarLibrary/lib/ -B $SOAR_PATH/Core/ClientSML -B $SOAR_PATH/Core/ElementXML -B $SOAR_PATH/Core/ConnectionSML -o soar2soar soar2soar.cpp -lClientSML -lConnectionSML -lElementXML
