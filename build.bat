361692ac85929ff7dc5cb4497d4f4bece932e400
Jon Voigt <voigtjr@gmail.com>
2010-03-03 19:21:24 +0000

    Updated build file to build Java before other java components

--- build.bat
+++ build.bat
@@ -43,9 +43,17 @@ if not errorlevel 0 goto fail
 
 :built
 
-for /f "tokens=* delims= " %%a in ('dir/b/ad') do @(
-	if exist "%%a\build.xml" (
-		ant "-Dsoarprefix=%SOAR_HOME%" "-Dversion=%SOAR_VERSION%" -f "%%a\build.xml" %TARGET%
+rem Need to build Java if it exists, first.
+if exist "Java\build.xml" @(
+
+	ant "-Dsoarprefix=%SOAR_HOME%" "-Dversion=%SOAR_VERSION%" -f "Java\build.xml" %TARGET%
+
+	for /f "tokens=* delims= " %%a in ('dir/b/ad') do (
+		if not %%a == Java (
+			if exist "%%a\build.xml" (
+				ant "-Dsoarprefix=%SOAR_HOME%" "-Dversion=%SOAR_VERSION%" -f "%%a\build.xml" %TARGET%
+			)
+		)
 	)
 )
 
