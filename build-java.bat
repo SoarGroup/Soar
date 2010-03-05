878e6b1b097d08db6527278fcd61b8a87c8b020d
Jon Voigt <voigtjr@gmail.com>
2010-03-05 21:06:37 +0000

    * split up windows build files so that java could be built separately, also scu/noscu * fixed logic error in get listener port * moved sproom's build xml file to different name since it doesn't work

---
+++ build-java.bat
@@ -0,0 +1,17 @@
+@echo off
+
+call soar-vars.bat %1
+
+rem Need to build Java if it exists, first.
+if exist "Java\build.xml" @(
+
+	ant -q "-Dsoarprefix=%SOAR_HOME%" "-Dversion=%SOAR_VERSION%" -f "Java\build.xml" %TARGET%
+
+	for /f "tokens=* delims= " %%a in ('dir/b/ad') do @(
+		if not %%a == Java (
+			if exist "%%a\build.xml" (
+				ant -q "-Dsoarprefix=%SOAR_HOME%" "-Dversion=%SOAR_VERSION%" -f "%%a\build.xml" %TARGET%
+			)
+		)
+	)
+)
