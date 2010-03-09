b3e94c104d7fa2b64422e7af742c829214e3606d
Jon Voigt <voigtjr@gmail.com>
2010-03-09 20:08:47 +0000

    version bump

--- soar-vars.bat
+++ soar-vars.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set SOAR_HOME=%CD%\out
-set SOAR_VERSION=9.2.0
+set SOAR_VERSION=9.3.0
 if "%1" == "" (
 	set TARGET=build
 ) else (
@@ -15,10 +15,9 @@ echo JAVA_HOME: %JAVA_HOME%
 echo PYTHON_HOME: %PYTHON_HOME%
 echo SWIG_HOME: %SWIG_HOME%
 echo.
-echo Edit the build.bat file to check these variables:
+echo Edit the soar-vars.bat file to check these variables:
 echo SOAR_HOME: %SOAR_HOME%
 echo SOAR_VERSION: %SOAR_VERSION%
-echo.
 echo TARGET: %TARGET%
 echo.
 
