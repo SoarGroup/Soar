6e195b1a751a713acfbb34b1721858e43dbfdf6b
Jon Voigt <voigtjr@gmail.com>
2010-04-08 17:51:53 +0000

    added max/min distances for get/drop, debugged mixed real/virtual again with blocks. agent needs work.

--- Sproom/src/main/java/edu/umich/soar/sproom/splinter/Splinter.java
+++ Sproom/src/main/java/edu/umich/soar/sproom/splinter/Splinter.java
@@ -1,7 +1,6 @@
 package edu.umich.soar.sproom.splinter;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
