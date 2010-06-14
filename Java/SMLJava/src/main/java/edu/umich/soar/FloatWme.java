82819e5ced15c67830c185ad082753a1e97c9675
Jon Voigt <voigtjr@gmail.com>
2010-06-14 17:54:27 +0000

    Various updates to smljava library that I didn't have time to push in to the last release.
     * More comments
     * Spawn debugger with only an agent reference
     * Added common Wme interface for valued element wrappers
     * Added value getter for valued element wrappers

--- Java/SMLJava/src/main/java/edu/umich/soar/FloatWme.java
+++ Java/SMLJava/src/main/java/edu/umich/soar/FloatWme.java
@@ -3,12 +3,32 @@ package edu.umich.soar;
 import sml.FloatElement;
 import sml.Identifier;
 
-public class FloatWme
+/**
+ * Wrapper for float valued working memory elements.
+ * 
+ * @author voigtjr
+ *
+ */
+public class FloatWme implements Wme
 {
+    /**
+     * Create working memory element instance with no value. The value will be set
+     * (and underlying SML Wme created) on first update.
+     * @param parent Parent identifier
+     * @param attr Attribute
+     * @return Wme ready for update
+     */
     public static FloatWme newInstance(Identifier parent, String attr) {
 	return new FloatWme(parent, attr);
     }
     
+    /**
+     * Create working memory element instance with known initial value.
+     * @param parent Parent identifier
+     * @param attr Attribute
+     * @param val Initial value
+     * @return Wme ready for update
+     */
     public static FloatWme newInstance(Identifier parent, String attr, double val) {
 	FloatWme temp = new FloatWme(parent, attr);
 	temp.update(val);
@@ -24,6 +44,10 @@ public class FloatWme
 	this.attr = attr;
     }
     
+    /**
+     * Update to possibly new value. See Agent.BlinkIfNoChange()
+     * @param val The new value
+     */
     public void update(double val) {
 	if (wme != null) {
 	    wme.Update(val);
@@ -32,6 +56,18 @@ public class FloatWme
 	}
     }
     
+    /**
+     * Get the current value.
+     * @throws IllegalStateException if destroyed or value was not set
+     * @return Current value
+     */
+    public double getValue() {
+	if (wme == null) 
+	    throw new IllegalStateException();
+	return wme.GetValue();
+    }
+    
+    @Override
     public void destroy() {
 	if (wme != null) {
 	    wme.DestroyWME();
