6d4c4715761ed4a05b5d1eeff8297354ec2a99d1
Jon Voigt <voigtjr@gmail.com>
2010-06-29 17:08:04 +0000

    Added comments.

--- Java/DebuggerAPI/src/main/java/edu/umich/soar/debugger/jmx/SoarCommandLineMXBean.java
+++ Java/DebuggerAPI/src/main/java/edu/umich/soar/debugger/jmx/SoarCommandLineMXBean.java
@@ -1,6 +1,29 @@
 package edu.umich.soar.debugger.jmx;
 
-public interface SoarCommandLineMXBean {
+/**
+ * Java JMX interface to simple Soar command line.
+ * 
+ * @author voigtjr
+ * 
+ */
+public interface SoarCommandLineMXBean
+{
+    /**
+     * Get the name of the command line interface instance, usually agent name.
+     * 
+     * @return Name of instance (agent name).
+     */
     public String getName();
+
+    /**
+     * Execute a command line.
+     * 
+     * Multiple commands can be included in the line. Syntax legal for .soar
+     * files is legal here.
+     * 
+     * @param line
+     *            The command line to execute.
+     * @return String result, often empty. Null if error.
+     */
     public String executeCommandLine(String line);
 }
