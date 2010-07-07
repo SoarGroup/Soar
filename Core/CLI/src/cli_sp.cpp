24a6923b727fbfd400042da3d24ecdd4545accea
Jon Voigt <voigtjr@gmail.com>
2010-07-07 03:15:49 +0000

    Fixed bug where if the production failed to be parsed or entered in to the system, that it would segfault trying to write the filename to the production struct.

--- Core/CLI/src/cli_sp.cpp
+++ Core/CLI/src/cli_sp.cpp
@@ -67,15 +67,6 @@ bool CommandLineInterface::DoSP(const std::string& productionString) {
 	unsigned char rete_addition_result = 0;
 	p = parse_production( m_pAgentSoar, &rete_addition_result );
 
-	if (!m_SourceFileStack.empty()) {
-		std::string path;
-		GetCurrentWorkingDirectory(path);
-		path.append(get_directory_separator());
-		path.append(m_SourceFileStack.top());
-
-		p->filename = make_memory_block_for_string(m_pAgentSoar, path.c_str());
-	}
-
 	set_lexer_allow_ids( m_pAgentSoar, true );
 	soarAlternateInput( m_pAgentSoar, 0, 0, true ); 
 
@@ -87,6 +78,15 @@ bool CommandLineInterface::DoSP(const std::string& productionString) {
 		// production ignored
 		++m_NumProductionsIgnored;
 	} else {
+		if (!m_SourceFileStack.empty()) {
+			std::string path;
+			GetCurrentWorkingDirectory(path);
+			path.append(get_directory_separator());
+			path.append(m_SourceFileStack.top());
+
+			p->filename = make_memory_block_for_string(m_pAgentSoar, path.c_str());
+		}
+
 		// production was sourced
 		++m_NumProductionsSourced;
 		if (m_RawOutput) {
