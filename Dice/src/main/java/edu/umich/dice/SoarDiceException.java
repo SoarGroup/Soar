f74aaeb245935d5cfdcccaaecc812849140930f1
Jon Voigt <voigtjr@gmail.com>
2010-08-05 14:38:09 +0000

    Documentation updates, formatting updates. Improved error message handling. Split off new exception. Added main method for running stand-alone.

---
+++ Dice/src/main/java/edu/umich/dice/SoarDiceException.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.umich.dice;
+
+class SoarDiceException extends Exception
+{
+    private static final long serialVersionUID = 7944707250565125602L;
+
+    public SoarDiceException(String message)
+    {
+        super(message);
+    }
+}
