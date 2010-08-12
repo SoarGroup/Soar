61d2780ce7ec3f342251057a7d2a6eb1930f8bb1
Nate Derbinsky <nate.derbinsky@gmail.com>
2010-08-12 14:49:02 +0000

    added dice module and modified classpath to use variables (makes eclipse happy)

---
+++ SoarQnA/src/edu/umich/soar/qna/dice/ComputeProbabilityQueryState.java
@@ -0,0 +1,76 @@
+package edu.umich.soar.qna.dice;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import edu.umich.dice.LiarsDice.Predicate;
+import edu.umich.soar.qna.ComputationalQueryState;
+
+public class ComputeProbabilityQueryState extends ComputationalQueryState {
+	
+	private static final String COMMAND_DICE = "number-of-dice";
+	private static final String COMMAND_SIDES = "number-of-faces";
+	private static final String COMMAND_COUNT = "count";
+	private static final String COMMAND_PREDICATE = "predicate";
+	
+	private Integer myDice = null;
+	private Integer mySides = null;
+	private Integer myCount = null;
+	private Predicate myPred = null;
+	private Double myResult = null;
+
+	@Override
+	public boolean initialize(String querySource,
+			Map<Object, List<Object>> queryParameters) {
+		boolean returnVal = false;
+		
+		if (queryParameters.size() == 4) {
+			if (queryParameters.containsKey(COMMAND_DICE) && queryParameters.get(COMMAND_DICE).size()==1 &&
+				queryParameters.containsKey(COMMAND_SIDES) && queryParameters.get(COMMAND_SIDES).size()==1 &&
+				queryParameters.containsKey(COMMAND_COUNT) && queryParameters.get(COMMAND_COUNT).size()==1 &&
+				queryParameters.containsKey(COMMAND_PREDICATE) && queryParameters.get(COMMAND_PREDICATE).size()==1) {
+				
+				if ((queryParameters.get(COMMAND_DICE).iterator().next() instanceof Integer) &&
+					(queryParameters.get(COMMAND_SIDES).iterator().next() instanceof Integer) &&
+					(queryParameters.get(COMMAND_COUNT).iterator().next() instanceof Integer) &&
+					(queryParameters.get(COMMAND_PREDICATE).iterator().next() instanceof String)) {
+					
+					myDice = (Integer) queryParameters.get(COMMAND_DICE).iterator().next();
+					mySides = (Integer) queryParameters.get(COMMAND_SIDES).iterator().next();
+					myCount = (Integer) queryParameters.get(COMMAND_COUNT).iterator().next();
+					myPred = Predicate.valueOf((String) queryParameters.get(COMMAND_PREDICATE).iterator().next());					
+					
+					try {
+						myResult = myPred.get(myDice, mySides, myCount);
+						
+						returnVal = true;
+						hasComputed = false;
+					} catch (Exception e) {
+						
+					}
+				}			
+			}
+		}
+		
+		return returnVal;
+	}
+
+	@Override
+	public Map<String, List<Object>> next() {
+		if (!hasComputed) {
+			hasComputed = true;
+			HashMap<String, List<Object>> returnVal = new HashMap<String, List<Object>>();
+			List<Object> newList = new LinkedList<Object>();
+			
+			newList.add(myResult);
+			returnVal.put("probability", newList);
+			
+			return returnVal;
+		}
+		
+		return null;
+	}
+
+}
