cff7eb292f30a123acd5495a0d0d8a5e972b6e4c
Joseph Xu <josephzxu@gmail.com>
2011-03-23 09:27:58 +0000

    Build now dies if any commands documented on the wiki are not referenced anywhere in the manual. Function summary at end of manual is now automatically generated.

--- ManualSource/wikicmd/gen_tex.sh
+++ ManualSource/wikicmd/gen_tex.sh
@@ -1,26 +1,51 @@
 #!/bin/sh
 PYTHON=python2
 
-svn export http://soar.googlecode.com/svn/wiki/CommandLineInterface.wiki
+svn export -q http://soar.googlecode.com/svn/wiki/CommandLineInterface.wiki CommandLineInterface.wiki.new
 
-# split into single file for each command
-awk '
-	BEGIN { f="/dev/null" }
+if [ -f CommandLineInterface.wiki ] && cmp -s CommandLineInterface.wiki.new CommandLineInterface.wiki
+then
+	# no changes
+	rm CommandLineInterface.wiki.new
+else
+	mv CommandLineInterface.wiki.new CommandLineInterface.wiki
 	
-	/^= *[-_a-zA-Z]* *=$/ {
-		f="wiki/" $2 ".wiki"
-	}
+	# split into single file for each command
+	awk '
+		BEGIN { f="/dev/null" }
+		
+		/^= *[-_a-zA-Z]* *=$/ {
+			f="wiki/" $2 ".wiki"
+		}
+		
+		/----/ { next }   # delete horizontal separators
+		
+		{ print > f }' CommandLineInterface.wiki || exit 1
 	
-	/----/ { next }   # delete horizontal separators
-	
-	{ print > f }' CommandLineInterface.wiki || exit 1
+	# translate to tex
+	echo generating command sources
+	for f in `ls wiki`
+	do
+		tf=`echo "$f" | sed 's/wiki$/tex/'`
+		echo -n "$tf "
+		$PYTHON moin2latex.py "wiki/$f" > "tex/$tf" || exit 1
+	done
+	echo
+fi
 
-# translate to tex
-echo generating command sources
-for f in `ls wiki`
+# make sure every command listed on the wiki is included in the
+# interface section of the manual
+
+for cmd in `ls wiki/ | awk -F. '{print $1}'`
 do
-	tf=`echo "$f" | sed 's/wiki$/tex/'`
-	echo -n "$tf "
-	$PYTHON moin2latex.py "wiki/$f" > "tex/$tf" || exit 1
+	if ! grep -q "input{wikicmd/tex/$cmd}" ../interface.tex
+	then
+		unused=1
+		echo UNUSED: $cmd
+	fi
 done
-echo finished
+
+if [ -n "$unused" ]
+then
+	exit 1
+fi
