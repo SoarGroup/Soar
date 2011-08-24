f10eb7d736fa79008a39c46d786892418857b2a9
Nate Derbinsky <nate.derbinsky@gmail.com>
2011-08-24 18:38:55 +0000

    experimental addition: prevents the creation of more than one o-supported preference for a WME that is in the closure of top-state. since goal-preferences are only cleaned when the WME is removed or the state is removed, persistent structures can build up large numbers of preferences (either via rules or LTM retrievals), which can negatively impact runtime performance. This change *shouldn't* affect much at the user level, though the result of the preferences command will only reflect the "first" o-supported preference for a WME. Performance benefits, if applicable, should include reduced time and space (instantiations and preferences may be deallocated earlier). There may be a small time addition due to searching slot preferences, which will be worse for multi-valued attributes. However, other processes have to perform this search in the same circumstances, so it should not be significant.

--- Core/SoarKernel/src/recmem.cpp
+++ Core/SoarKernel/src/recmem.cpp
@@ -1214,17 +1214,27 @@ void assert_new_preferences (agent* thisAgent, pref_buffer_list& bufdeallo)
 			else if (inst->in_ms || pref->o_supported) 
 			{
 				/* --- normal case --- */
-				add_preference_to_tm (thisAgent, pref);
-
-
-				/* REW: begin 09.15.96 */
-				/* No knowledge retrieval necessary in Operand2 */
-				/* REW: end   09.15.96 */
-
-
-				if ( wma_enabled( thisAgent ) )
+				if ( add_preference_to_tm (thisAgent, pref) )
 				{
-					wma_activate_wmes_in_pref( thisAgent, pref );
+					/* REW: begin 09.15.96 */
+					/* No knowledge retrieval necessary in Operand2 */
+					/* REW: end   09.15.96 */
+
+
+					if ( wma_enabled( thisAgent ) )
+					{
+						wma_activate_wmes_in_pref( thisAgent, pref );
+					}
+				}
+				else
+				{
+					// NLD: the preference was o-supported, at
+					// the top state, and was asserting an acceptable 
+					// preference for a WME that was already
+					// o-supported. hence unnecessary.
+
+					preference_add_ref( pref );
+					preference_remove_ref( thisAgent, pref );
 				}
 			} 
 			else 
