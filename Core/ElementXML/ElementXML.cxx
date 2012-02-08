11ba9d6b948e2338314b215c23c78a1ad4cda755
Joseph Xu <josephzxu@gmail.com>
2012-02-08 23:53:13 +0000

    First attempt at Windows build. Static library works, but not DLL

--- Core/ElementXML/ElementXML.cxx
+++ Core/ElementXML/ElementXML.cxx
@@ -1,4 +1,4 @@
-#include "src/ElementXMLdll.cpp"
+//#include "src/ElementXMLdll.cpp"
 #include "src/ElementXMLInterface.cpp"
 #include "src/ElementXMLImpl.cpp"
 #include "src/ParseXML.cpp"
--- Core/KernelSML/src/KernelSMLdll.h
+++
@@ -1,7 +0,0 @@
-#ifndef KERNELSMLDLL_H
-#define KERNELSMLDLL_H
-
-// get definition of EXPORT
-#include "Export.h"
-
-#endif // KERNELSMLDLL
