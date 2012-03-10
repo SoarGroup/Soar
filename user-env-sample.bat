e46cc1abebdfbcfef026a8a8c0bbff93a66dfaab
Jon Voigt <voigtjr@gmail.com>
2012-03-10 22:11:14 +0000

    Added environment sample file and having b.bat call it. Note that this is not implemented in the script right now.

---
+++ user-env-sample.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem set SWIG_HOME="C:\swigwin-2.0.4"
+
+rem This should point to the root of the Java SDK
+rem set JAVA_HOME="C:\Program Files\Java\jdk1.6.0_26"
+
+rem This shouldn't be necessary due to registry inspection
+rem set PYTHON_HOME="C:\Python26"
+
