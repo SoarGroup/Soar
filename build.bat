a02177eb52d9446913f8018ed80746b326a997b6
Joseph Xu <josephzxu@gmail.com>
2012-03-13 02:20:01 +0000

    build.bat now prompts user for directories, generates user-env.bat on first run

--- build.bat
+++ build.bat
@@ -1,36 +1,76 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
-REM source environment variables
-if exist user-env.bat (
-  echo Sourcing local environment variables from user-env.bat
-  call user-env.bat
+if not exist user-env.bat (
+	call :findexe python.exe
+	if not "!retval!"=="fail" set PYTHON_HOME=!retval!
+	
+	call :findexe javac.exe
+	if not "!retval!"=="fail" set JAVA_HOME=!retval:\bin=!
+	
+	call :findexe swig.exe
+	if not "!retval!"=="fail" set SWIG_HOME=!retval!
+	
+	echo set PYTHON_HOME=!PYTHON_HOME!>>user-env.bat
+	echo set JAVA_HOME=!JAVA_HOME!>>user-env.bat
+	echo set SWIG_HOME=!SWIG_HOME!>>user-env.bat
+) else (
+	echo Reading local environment variables from user-env.bat
+	call user-env.bat
 )
 
-REM try to find python.exe in PATH
-set tempxx="%PATH:;=";"%"
-for %%i in (%tempxx%) do (
-	if exist %%i\python.exe (
-		set python=%%i\python.exe
-		goto :found
-	)
+if not exist %PYTHON_HOME%\python.exe (
+	echo cannot locate python executable
+	exit /B
 )
 
-REM python.exe not in PATH, search registry for installation directory
-REM In the case of multiple installations, the highest version number will be used
-for /F %%i in ('reg query HKLM\SOFTWARE\Python\PythonCore') do set pyver=%%i
-if X%pyver%==X goto :fail
-
-for /F "tokens=3" %%i in ('reg query %pyver%\InstallPath /ve') do set pydir=%%i
-if exist %pydir%\python.exe (
-	set python=%pydir%\python.exe
-	goto :found
-)
-
-:fail
-echo cannot locate python executable
+set PATH=%PYTHON_HOME%;%JAVA_HOME%\bin;%SWIG_HOME%;%PATH%
+%PYTHON_HOME%\python.exe scons\scons.py %*
 exit /B
 
-:found
-echo using %python%
-%python% scons\scons.py %*
+rem a "function" that tries to find an executable
+:findexe
+	set retval=fail
+	set tempxx="%PATH:;=";"%"
+	for %%i in (%tempxx%) do (
+		if exist "%%i\%~1" (
+			set retval=%%~i
+			goto :EOF
+		)
+	)
+	
+	if "%~1"=="python.exe" (
+		call :findpython
+		if not "!retval!"=="fail" goto :EOF
+	)
+	
+	call :askuser %~1
+goto :EOF
+
+rem Search for python. If python.exe not in PATH, search registry for installation directory.
+rem In the case of multiple installations, the highest version number will be used
+:findpython
+	set retval=fail
+	for /F %%i in ('reg query HKLM\SOFTWARE\Python\PythonCore') do set pyver=%%i
+	if not X%pyver%==X (
+		for /F "tokens=3" %%i in ('reg query %pyver%\InstallPath /ve') do (
+			if exist %%i\python.exe set retval=%%i
+		)
+	)
+goto :EOF
+
+:askuser
+	:whilebegin
+	set tempdir=
+	set /P tempdir=Enter directory that contains %~1, or nothing to ignore it:
+	
+	if "%tempdir%"=="" (
+		set retval=fail
+		goto :EOF
+	)
+	if not exist "%tempdir%\%~1" (
+		echo it's not there
+		goto :whilebegin
+	)
+	set retval=%tempdir%
+goto :EOF
