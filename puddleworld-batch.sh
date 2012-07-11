4535783eb62f94a306c8f87267822d743a4e6622
Mitchell Bloch <bazald@gmail.com>
2012-07-11 20:38:40 +0000

    rl-impasse branch:   merge trunk;   puddleworld-batch.sh fixed;

--- puddleworld-batch.sh
+++ puddleworld-batch.sh
@@ -4,24 +4,31 @@ if [ "$CORES" == "" ]; then
   CORES=1
 fi
 
-AGGREGATE=10
+AGGREGATE=8
 EPISODES=10000
 
 declare -a VALS=( \
                  10 10
+                 10 20
+                 20 10
                  20 20
                  )
 declare -a VALSP=( \
                   -1 0 0
                   -1 0 0
+                  -1 0 0
+                  -1 0 0
                  )
 
-for i in $(seq 1 $AGGREGATE); do
-  random[$i]=$RANDOM
-done
+random=($(cat experiment/random))
 
-echo ${random[@]} > experiment/random
-#random=($(cat experiment/random))
+if [ ${#random[@]} -eq 0 ]; then
+  for i in $(seq 0 $(($AGGREGATE - 1))); do
+    random[$i]=$RANDOM
+  done
+
+  echo ${random[@]} > experiment/random
+fi
 
 for v in $(seq 0 $((${#VALS[@]} / 2 - 1))); do
   BASE="experiment"
@@ -34,15 +41,15 @@ for v in $(seq 0 $((${#VALS[@]} / 2 - 1))); do
 done
 
 experiment () {
-  echo time out/PuddleWorld --episodes $EPISODES --seed $1 --rules $2/in.soar --rl-rules-out $2/out-$1.soar \> $2/puddleworld-$1.out
-       time out/PuddleWorld --episodes $EPISODES --seed $1 --rules $2/in.soar --rl-rules-out $2/out-$1.soar > $2/puddleworld-$1.out
-  echo ""
-}
-
-experiment_sp () {
   echo time out/PuddleWorld --episodes $EPISODES --seed $1 --rules $2/in.soar --rl-rules-out $2/out-$1.soar --sp-special ${VALSP[$((3 * $3 + 0))]} ${VALSP[$((3 * $3 + 1))]} ${VALSP[$((3 * $3 + 2))]} \> $2/puddleworld-$1.out
        time out/PuddleWorld --episodes $EPISODES --seed $1 --rules $2/in.soar --rl-rules-out $2/out-$1.soar --sp-special ${VALSP[$((3 * $3 + 0))]} ${VALSP[$((3 * $3 + 1))]} ${VALSP[$((3 * $3 + 2))]}  > $2/puddleworld-$1.out
   echo ""
+}
+
+experiment_sub () {
+  for v in $(seq $1 $2 $((${#random[@]} - 1))); do
+    experiment ${random[v]} $3 $4
+  done
 }
 
 experiments () {
@@ -59,17 +66,16 @@ experiments () {
               (<s> ^name puddleworld
                   ^div <d>)
               (<d> ^name default
-                  ^x (/ 1.001 ${VALS[$(($2 * 2 * v + 0))]})
-                  ^y (/ 1.001 ${VALS[$(($2 * 2 * v + 1))]}))
+                  ^x (/ 1.001 ${VALS[$((2 * v + 0))]})
+                  ^y (/ 1.001 ${VALS[$((2 * v + 1))]}))
           }" >> $DIR/in.soar
 
 #     for r in ${random[@]}; do
-#       experiment $r $DIR &
+#       experiment $r $DIR $v
 #     done
-#     wait
 
-    for r in ${random[@]}; do
-      experiment_sp $r $DIR $v #&
+    for x in $(seq 0 $(($CORES - 1))); do
+      experiment_sub $x $2 $DIR $v &
     done
     wait 
 
@@ -79,6 +85,6 @@ experiments () {
 
 for x in $(seq 0 $(($CORES - 1))); do
 #   echo experiments $((4 * x)) $((4 * $CORES))
-  experiments $x $CORES &
+  experiments $x $CORES #&
 done
 wait
