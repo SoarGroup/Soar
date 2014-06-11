080d247db82976fc4fd238aa1ecfd425b88f0dd9
Mazin Assanie <mazina@umich.edu>
2014-06-11 15:56:01 +0000

    - Added missing aliases defined in cSoar to tcl side

--- Tcl/tcl/aliases.tcl
+++ Tcl/tcl/aliases.tcl
@@ -1,42 +1,53 @@
-alias ?        help
-alias a        alias
-alias aw       add-wme
-alias cnf      chunk-name-format
-alias ctf      command-to-file
-alias d        run -d
-alias e        run -e
-alias ea       excise --all
-alias eb       explain-backtraces
-alias ec       excise --chunks
-alias et       excise --task
-alias ex       excise
-alias fc       firing-counts
-alias fw       format-watch
-alias h        help
-alias halt     stop-soar
-alias inds     indifferent-selection
-alias ip       input-period
-alias is       init-soar
-alias l        learn
-alias m        matches
-alias osd      output-strings-destination
-alias p        print
-alias pc       print --chunks
-alias pf       production-find
-alias pr       preferences
-alias ps       print --stack
-alias pw       pwatch
-alias r        run
-alias rn       rete-net
-alias rw       remove-wme
-alias s        run 1
-alias se       send
-alias ss       stop-soar
-alias st       stats
-alias un       unalias
-alias w        watch
-alias wmes     print -depth 0 -internal
-alias quit     exit
+alias ?                 help
+alias a                 alias
+alias aw                add-wme
+alias chdir             cd
+alias cnf               chunk-name-format
+alias ctf               command-to-file
+alias d                 run -d
+alias dir               ls
+alias e                 run -e
+alias ea                excise --all
+alias eb                explain-backtraces
+alias ec                excise --chunks
+alias et                excise --task
+alias ex                excise
+alias fc                firing-counts
+alias fw                format-watch
+alias h                 help
+alias halt              stop-soar
+alias inds              indifferent-selection
+alias init              init-soar
+alias interrupt         stop-soar
+alias ip                input-period
+alias is                init-soar
+alias l                 learn
+alias m                 matches
+alias man               help
+alias osd               output-strings-destination
+alias p                 print
+alias pc                print --chunks
+alias pf                production-find
+alias pr                preferences
+alias ps                print --stack
+alias pw                pwatch
+alias quit              exit
+alias r                 run
+alias rn                rete-net
+alias rw                remove-wme
+alias s                 run 1
+alias se                send
+alias set-default-depth default-wme-depth
+alias sn                soarnews
+alias ss                stop-soar
+alias st                stats
+alias step              run -d
+alias stop              stop-soar
+alias topd              pwd
+alias un                unalias
+alias varprint          print -v -d 100
+alias w                 watch
+alias wmes              print -depth 0 -internal
 
 #----------------------------
 
