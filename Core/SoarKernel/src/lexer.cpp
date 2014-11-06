f8793fcce65e27e9a26ba87655d7c0ab48ba0d5f
Nathan Glenn <garfieldnate@gmail.com>
2014-11-06 15:39:28 +0900

    remove extra length field
    Reroll of 1dd2dbb003.
    
    Remove the unneeded `length()` field and rename `size` to `length()`,
    which is more intuitive for strings.

--- Core/SoarKernel/src/lexer.cpp
+++ Core/SoarKernel/src/lexer.cpp
@@ -98,7 +98,7 @@ bool Lexer::determine_type_of_constituent_string () {
     bool rereadable;
 
     determine_possible_symbol_types_for_string (current_lexeme.string(),
-        current_lexeme.size(),
+        current_lexeme.length(),
         &possible_id,
         &possible_var,
         &possible_sc,
@@ -158,7 +158,7 @@ bool Lexer::determine_type_of_constituent_string () {
         current_lexeme.type = STR_CONSTANT_LEXEME;
         if (thisAgent->sysparams[PRINT_WARNINGS_SYSPARAM]) {
             if ( (current_lexeme.lex_string[0] == '<') ||
-                 (current_lexeme.lex_string[current_lexeme.size()-1] == '>') )
+                 (current_lexeme.lex_string[current_lexeme.length()-1] == '>') )
             {
                 print (thisAgent, "Warning: Suspicious string constant \"%s\"\n", current_lexeme.lex_string);
                 print_location_of_most_recent_lexeme();
@@ -225,7 +225,7 @@ void Lexer::lex_equal () {
   /* Note: this routine relies on = being a constituent character */
 
   read_constituent_string();
-  if (current_lexeme.size()==1) { current_lexeme.type = EQUAL_LEXEME; return; }
+  if (current_lexeme.length()==1) { current_lexeme.type = EQUAL_LEXEME; return; }
   determine_type_of_constituent_string();
 }
 
@@ -234,7 +234,7 @@ void Lexer::lex_ampersand () {
   /* Note: this routine relies on & being a constituent character */
 
   read_constituent_string();
-  if (current_lexeme.size()==1) { current_lexeme.type = AMPERSAND_LEXEME; return; }
+  if (current_lexeme.length()==1) { current_lexeme.type = AMPERSAND_LEXEME; return; }
   determine_type_of_constituent_string();
 }
 
@@ -255,8 +255,8 @@ void Lexer::lex_greater () {
   /* Note: this routine relies on =,> being constituent characters */
 
   read_constituent_string();
-  if (current_lexeme.size()==1) { current_lexeme.type = GREATER_LEXEME; return; }
-  if (current_lexeme.size()==2) {
+  if (current_lexeme.length()==1) { current_lexeme.type = GREATER_LEXEME; return; }
+  if (current_lexeme.length()==2) {
     if (current_lexeme.lex_string[1]=='>') { current_lexeme.type = GREATER_GREATER_LEXEME; return;}
     if (current_lexeme.lex_string[1]=='=') { current_lexeme.type = GREATER_EQUAL_LEXEME; return; }
   }
@@ -268,13 +268,13 @@ void Lexer::lex_less () {
   /* Note: this routine relies on =,<,> being constituent characters */
 
   read_constituent_string();
-  if (current_lexeme.size()==1) { current_lexeme.type = LESS_LEXEME; return; }
-  if (current_lexeme.size()==2) {
+  if (current_lexeme.length()==1) { current_lexeme.type = LESS_LEXEME; return; }
+  if (current_lexeme.length()==2) {
     if (current_lexeme.lex_string[1]=='>') { current_lexeme.type = NOT_EQUAL_LEXEME; return; }
     if (current_lexeme.lex_string[1]=='=') { current_lexeme.type = LESS_EQUAL_LEXEME; return; }
     if (current_lexeme.lex_string[1]=='<') { current_lexeme.type = LESS_LESS_LEXEME; return; }
   }
-  if (current_lexeme.size()==3) {
+  if (current_lexeme.length()==3) {
     if ((current_lexeme.lex_string[1]=='=')&&(current_lexeme.lex_string[2]=='>'))
       { current_lexeme.type = LESS_EQUAL_GREATER_LEXEME; return; }
   }
@@ -287,7 +287,7 @@ void Lexer::lex_period () {
   /* --- if we stopped at '.', it might be a floating-point number, so be
      careful to check for this case --- */
   if (isdigit(current_char)) read_rest_of_floating_point_number();
-  if (current_lexeme.size()==1) { current_lexeme.type = PERIOD_LEXEME; return; }
+  if (current_lexeme.length()==1) { current_lexeme.type = PERIOD_LEXEME; return; }
   determine_type_of_constituent_string();
 }
 
@@ -302,11 +302,11 @@ void Lexer::lex_plus () {
      careful to check for this case --- */
   if (current_char=='.') {
     could_be_floating_point = TRUE;
-    for (i=1; i<current_lexeme.size(); i++)
+    for (i=1; i<current_lexeme.length(); i++)
       if (! isdigit(current_lexeme.lex_string[i])) could_be_floating_point = FALSE;
     if (could_be_floating_point) read_rest_of_floating_point_number();
   }
-  if (current_lexeme.size()==1) { current_lexeme.type = PLUS_LEXEME; return; }
+  if (current_lexeme.length()==1) { current_lexeme.type = PLUS_LEXEME; return; }
   determine_type_of_constituent_string();
 }
 
@@ -321,12 +321,12 @@ void Lexer::lex_minus () {
      careful to check for this case --- */
   if (current_char=='.') {
     could_be_floating_point = TRUE;
-    for (i=1; i<current_lexeme.size(); i++)
+    for (i=1; i<current_lexeme.length(); i++)
       if (! isdigit(current_lexeme.lex_string[i])) could_be_floating_point = FALSE;
     if (could_be_floating_point) read_rest_of_floating_point_number();
   }
-  if (current_lexeme.size()==1) { current_lexeme.type = MINUS_LEXEME; return; }
-  if (current_lexeme.size()==3) {
+  if (current_lexeme.length()==1) { current_lexeme.type = MINUS_LEXEME; return; }
+  if (current_lexeme.length()==3) {
     if ((current_lexeme.lex_string[1]=='-')&&(current_lexeme.lex_string[2]=='>'))
       { current_lexeme.type = RIGHT_ARROW_LEXEME; return; }
   }
@@ -342,7 +342,7 @@ void Lexer::lex_digit () {
      careful to check for this case --- */
   if (current_char=='.') {
     could_be_floating_point = TRUE;
-    for (i=1; i<current_lexeme.size(); i++)
+    for (i=1; i<current_lexeme.length(); i++)
       if (! isdigit(current_lexeme.lex_string[i])) could_be_floating_point = FALSE;
     if (could_be_floating_point) read_rest_of_floating_point_number();
   }
