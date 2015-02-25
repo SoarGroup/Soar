f07307a64878166826d2459665933d16868a03e7
Mazin Assanie <mazina@umich.edu>
2015-02-25 00:36:54 -0500

    Removed unification tests being added during backtracing.  Instead potentials are added even if already marked.  Merger gets rid of extra condition and allows the unification needed by kirk-chunk-test11. Still passes same unit tests.

--- Core/SoarKernel/src/debug_defines.h
+++ Core/SoarKernel/src/debug_defines.h
@@ -12,10 +12,10 @@
 /* -- The schema version used by the output manager's debug database -- */
 #define DEBUG_SCHEMA_VERSION "0.1"
 
-#define DEBUG_FREE_SETTINGS
+//#define DEBUG_FREE_SETTINGS
 //#define DEBUG_FULL_SETTINGS
 //#define DEBUG_REFCOUNT_SETTINGS
-//#define DEBUG_UNITTEST_SETTINGS
+#define DEBUG_UNITTEST_SETTINGS
 
 #ifdef DEBUG_FREE_SETTINGS
 /* -- Which trace messages should be printed -- */
@@ -52,15 +52,15 @@
 #define TRACE_Init_DT_LHS_VARIABLIZATION          false
 #define TRACE_Init_DT_RHS_VARIABLIZATION          false
 #define TRACE_Init_DT_RL_VARIABLIZATION           false
-#define TRACE_Init_DT_VARIABLIZATION_MANAGER      false
+#define TRACE_Init_DT_VARIABLIZATION_MANAGER      true
 #define TRACE_Init_DT_OVAR_MAPPINGS               false
 #define TRACE_Init_DT_NCC_VARIABLIZATION          false
 #define TRACE_Init_DT_IDENTITY_PROP               false
 #define TRACE_Init_DT_SOAR_INSTANCE               false
 #define TRACE_Init_DT_CLI_LIBRARIES               false
 #define TRACE_Init_DT_CONSTRAINTS                 false
-#define TRACE_Init_DT_MERGE                       false
-#define TRACE_Init_DT_FIX_CONDITIONS              false
+#define TRACE_Init_DT_MERGE                       true
+#define TRACE_Init_DT_FIX_CONDITIONS              true
 
 #define TRACE_Init_DT_EPMEM_CMD                   false
 
