f039ebb9c251fad65daefc310933d71829dec75e
Alex Turner <bluechill@me.com>
2015-06-24 15:24:17 -0400

    Actual RHS functions like JSoar taking advantage of internal agent access

--- Tests/Prototype-UnitTesting/Tests/conversion.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-find . -type f -name "*.soar" -exec sed -i '' 's/(succeeded)/(exec succeeded)/g' {} \;
-find . -type f -name "*.soar" -exec sed -i '' 's/(failed)/(exec failed)/g' {} \;
-find . -type f -name "*.soar" -exec sed -i '' 's/(halt)/(exec halt)/g' {} \;
