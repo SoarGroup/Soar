5a287e4840da4e977c09319da135937c4f61114f
scijones <scijones@umich.edu>
2016-09-20 17:06:20 -0400

    Revert "When variables are used instead of direct assignment of lti ids, nondeterministic behavior is allowed. As such, the smem add statements in the units tests for activation now specify the lti ids directly to enforce assignment to the predicted lti ids."
    This reverts commit 0e047985adc99fa1b5267e0320a3da1efc97d1fe.

--- Tests/Prototype-UnitTesting/kernel/SMemFunctionalTests.cpp
+++ Tests/Prototype-UnitTesting/kernel/SMemFunctionalTests.cpp
@@ -173,11 +173,11 @@ void SMemFunctionalTests::testSimpleNonCueBasedRetrieval_ActivationRecency()
 
 	assertTrue_msg("testSimpleNonCueBasedRetrieval_ActivationRecency functional test did not halt", halted);
 
-	std::string expected = "(@1 ^location @2 ^name foo [+1.000])\n(@2 ^x 1 ^y 2 ^z 3 [+2.000])\n(@3 ^location @4 ^name bar [+0.000])\n(@4 ^x 2 ^y 3 ^z 1 [+0.000])\n";
+	std::string expected = "(@1 ^location @2 ^name foo [+1.000])\n(@2 ^x 1 ^y 2 ^z 3 [+2.000])\n(@3 ^x 2 ^y 3 ^z 1 [+0.000])\n(@4 ^location @3 ^name bar [+0.000])\n";
 
 	result = agent->ExecuteCommandLine("print @");
 
-	assertTrue_msg("testSimpleNonCueBasedRetrieval_ActivationRecency: Invalid Activation Values\nString did not match:\n" "(@1 ^location @2 ^name foo [+1.000])\n(@2 ^x 1 ^y 2 ^z 3 [+2.000])\n(@3 ^location @4 ^name bar [+0.000])\n(@4 ^x 2 ^y 3 ^z 1 [+0.000])\n", result == expected);
+	assertTrue_msg("testSimpleNonCueBasedRetrieval_ActivationRecency: Invalid Activation Values\nString did not match:\n" "(@1 ^location @2 ^name foo [+1.000])\n(@2 ^x 1 ^y 2 ^z 3 [+2.000])\n(@3 ^x 2 ^y 3 ^z 1 [+0.000])\n(@4 ^location @3 ^name bar [+0.000])\n", result == expected);
 }
 
 void SMemFunctionalTests::testSimpleNonCueBasedRetrieval_ActivationRecency_WithoutActivateOnQuery()
@@ -188,11 +188,11 @@ void SMemFunctionalTests::testSimpleNonCueBasedRetrieval_ActivationRecency_Witho
 
 	assertTrue_msg("testSimpleNonCueBasedRetrieval_ActivationRecency_WithoutActivateOnQuery functional test did not halt", halted);
 
-	std::string expected = "(@1 ^location @2 ^name foo [+0.000])\n(@2 ^x 1 ^y 2 ^z 3 [+1.000])\n(@3 ^location @4 ^name bar [+0.000])\n(@4 ^x 2 ^y 3 ^z 1 [+0.000])\n";
+	std::string expected = "(@1 ^location @2 ^name foo [+0.000])\n(@2 ^x 1 ^y 2 ^z 3 [+1.000])\n(@3 ^x 2 ^y 3 ^z 1 [+0.000])\n(@4 ^location @3 ^name bar [+0.000])\n";
 
 	result = agent->ExecuteCommandLine("print @");
 
-	assertTrue_msg("testSimpleNonCueBasedRetrieval_ActivationRecency_WithoutActivateOnQuery: Invalid Activation Values\nString did not match:\n(@1 ^location @2 ^name foo [+0.000])\n(@2 ^x 1 ^y 2 ^z 3 [+1.000])\n(@3 ^location @4 ^name bar [+0.000])\n(@4 ^x 2 ^y 3 ^z 1 [+0.000])\n", result == expected);
+	assertTrue_msg("testSimpleNonCueBasedRetrieval_ActivationRecency_WithoutActivateOnQuery: Invalid Activation Values\nString did not match:\n(@1 ^location @2 ^name foo [+0.000])\n(@2 ^x 1 ^y 2 ^z 3 [+1.000])\n(@3 ^x 2 ^y 3 ^z 1 [+0.000])\n(@4 ^location @3 ^name bar [+0.000])\n\n", result == expected);
 }
 
 void SMemFunctionalTests::testSimpleNonCueBasedRetrieval_ActivationFrequency()
@@ -301,11 +301,11 @@ void SMemFunctionalTests::testSimpleNonCueBasedRetrieval_ActivationBaseLevel_Sta
 	lowEndExpectations.push_back(0.0);
 	highEndExpectations.push_back(0.0);
 
-	lowEndExpectations.push_back(0.534);
-	highEndExpectations.push_back(0.535);
-
 	lowEndExpectations.push_back(0.0);
 	highEndExpectations.push_back(0.0);
+
+	lowEndExpectations.push_back(0.534);
+	highEndExpectations.push_back(0.535);
 
  //agent->ExecuteCommandLine("print @");
 
@@ -344,11 +344,11 @@ void SMemFunctionalTests::testSimpleNonCueBasedRetrieval_ActivationBaseLevel_Nai
 	lowEndExpectations.push_back(0.0);
 	highEndExpectations.push_back(0.0);
 
-	lowEndExpectations.push_back(-0.347);
-	highEndExpectations.push_back(-0.346);
-
 	lowEndExpectations.push_back(0.0);
 	highEndExpectations.push_back(0.0);
+
+	lowEndExpectations.push_back(-0.347);
+	highEndExpectations.push_back(-0.346);
 
 	// This is the expected output from print @ modified from CSoar to look like JSoar outputs it (reverse string attributes)
 //	std::string expected = R"raw(
@@ -385,11 +385,11 @@ void SMemFunctionalTests::testSimpleNonCueBasedRetrieval_ActivationBaseLevel_Inc
 	lowEndExpectations.push_back(0.0);
 	highEndExpectations.push_back(0.0);
 
-	lowEndExpectations.push_back(-0.550);
-	highEndExpectations.push_back(-0.549);
-
 	lowEndExpectations.push_back(0.0);
 	highEndExpectations.push_back(0.0);
+
+	lowEndExpectations.push_back(-0.550);
+	highEndExpectations.push_back(-0.549);
 
 	// This is the expected output from print @ modified from CSoar to look like JSoar outputs it (reverse string attributes)
 //	std::string expected = R"raw(
