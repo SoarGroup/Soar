603d981264efce6f8a78710edbec5974e49efbea
Mazin Assanie <mazina@umich.edu>
2017-01-27 13:12:55 -0500

    Repair failures will no longer trigger an assert
    - This was just for debugging. We can just reject the rule in a release 
      build.

--- Core/SoarKernel/src/explanation_based_chunking/ebc_repair.cpp
+++ Core/SoarKernel/src/explanation_based_chunking/ebc_repair.cpp
@@ -107,7 +107,7 @@ wme_list* Repair_Manager::find_path_to_goal_for_symbol(Symbol* pNonOperationalSy
     lCurrentPath->clean_up();
     thisAgent->memoryManager->free_with_pool(MP_repair_path, lCurrentPath);
 
-    assert(final_path);
+//    assert(final_path);
     return final_path;
 }
 
