fd675384039eb13ef6d597a1bad75a3ec5afb0ac
Nathan Glenn <garfieldnate@gmail.com>
2022-12-16 20:01:07 +0100

    Update TestPythonSML.py to Python 3
    `print` is now a function, and `time.clock()` was removed in 3.8.
    
    Also test for the existence of the source files before creating the
    kernel, since Python exceptions while running Soar result in segfaults.

--- Core/ClientSMLSWIG/Python/TestPythonSML.py
+++ Core/ClientSMLSWIG/Python/TestPythonSML.py
@@ -2,57 +2,66 @@
 #
 # This is a test script which tests several aspects of the Python SML interface
 #  including kernel and agent creation, running, registering and unregistering
-#  several kinds of callbacks, inreinitializing, agent destruction, and kernel
+#  several kinds of callbacks, reinitializing, agent destruction, and kernel
 #  destruction (and maybe some other things, too).
 #
-import sys, os
-sys.path.append('../lib')
+from pathlib import Path
+import sys
+import time
+
 import Python_sml_ClientInterface
 from Python_sml_ClientInterface import *
 
-BASEDIR = os.path.dirname(os.path.dirname(Python_sml_ClientInterface.__file__))
+BASEDIR = Path(Python_sml_ClientInterface.__file__).parent.parent
+
+
+towers_of_hanoi_file = BASEDIR / 'share/soar/Demos/towers-of-hanoi/towers-of-hanoi.soar'
+toh_test_file = BASEDIR / 'share/soar/Tests/TOHtest.soar'
+for source_file in (towers_of_hanoi_file, toh_test_file):
+    print(f"Source file doesn't exist: {source_file}", file=sys.stderr)
+    sys.exit(1)
 
 def PrintCallback(id, userData, agent, message):
-	print message
+	print(message)
 
 def ProductionExcisedCallback(id, userData, agent, prodName, instantiation):
-	print "removing", prodName
+	print("removing", prodName)
 
 def ProductionFiredCallback(id, userData, agent, prodName, instantiation):
-	print "fired", prodName
+	print("fired", prodName)
 
 def PhaseExecutedCallback(id, userData, agent, phase):
-	print "phase", phase, "executed"
+	print("phase", phase, "executed")
 
 def AgentCreatedCallback(id, userData, agent):
-	print agent.GetAgentName(), "created"
+	print(agent.GetAgentName(), "created")
 
 def AgentReinitializedCallback(id, userData, agent):
-	print agent.GetAgentName(), "reinitialized"
+	print(agent.GetAgentName(), "reinitialized")
 
 def AgentDestroyedCallback(id, userData, agent):
-	print "destroying agent", agent.GetAgentName()
+	print("destroying agent", agent.GetAgentName())
 
 def SystemShutdownCallback(id, userData, kernel):
-	print "Shutting down kernel", kernel
+	print("Shutting down kernel", kernel)
 
 def RhsFunctionTest(id, userData, agent, functionName, argument):
-	print "Agent", agent.GetAgentName(), "called RHS function", functionName, "with argument(s) '", argument, "' and userData '", userData, "'"
+	print("Agent", agent.GetAgentName(), "called RHS function", functionName, "with argument(s) '", argument, "' and userData '", userData, "'")
 	return "success"
 
 def StructuredTraceCallback(id, userData, agent, pXML):
-	print "structured data:", pXML.GenerateXMLString(True)
-	
+	print("structured data:", pXML.GenerateXMLString(True))
+
 def UpdateEventCallback(id, userData, kernel, runFlags):
-	print "update event fired with flags", runFlags
+	print("update event fired with flags", runFlags)
 
 def UserMessageCallback(id, userData, agent, clientName, message):
-	print "Agent", agent.GetAgentName(), "received usermessage event for clientName '", clientName, "' with message '", message, "'"
+	print("Agent", agent.GetAgentName(), "received usermessage event for clientName '", clientName, "' with message '", message, "'")
 	return ""
 
 kernel = Kernel.CreateKernelInNewThread()
 if not kernel:
-	print >> sys.stderr, 'kernel creation failed'
+	print('kernel creation failed', file=sys.stderr)
 	sys.exit(1)
 
 agentCallbackId0 = kernel.RegisterForAgentEvent(smlEVENT_AFTER_AGENT_CREATED, AgentCreatedCallback, None)
@@ -65,7 +74,7 @@ messageCallbackId = kernel.RegisterForClientMessageEvent("TestMessage", UserMess
 
 agent = kernel.CreateAgent('Soar1')
 if not agent:
-	print >> sys.stderr, 'agent creation failed'
+	print('agent creation failed', file=sys.stderr)
 	sys.exit(1)
 
 printcallbackid = agent.RegisterForPrintEvent(smlEVENT_PRINT, PrintCallback, None)
@@ -75,9 +84,10 @@ runCallbackId = agent.RegisterForRunEvent(smlEVENT_AFTER_PHASE_EXECUTED, PhaseEx
 structuredCallbackId = agent.RegisterForXMLEvent(smlEVENT_XML_TRACE_OUTPUT, StructuredTraceCallback, None)
 
 #load the TOH productions
-result = agent.LoadProductions(os.path.join(BASEDIR, 'share', 'soar', 'Demos', 'towers-of-hanoi', 'towers-of-hanoi.soar'))
+result = agent.LoadProductions(str(towers_of_hanoi_file))
+
 #loads a function to test the user-defined RHS function stuff
-result = agent.LoadProductions(os.path.join(BASEDIR, 'share', 'soar', 'Tests', 'TOHtest.soar'))
+result = agent.LoadProductions(str(toh_test_file))
 
 kernel.SendClientMessage(agent, "TestMessage", "This is a \"quoted\"\" message")
 kernel.UnregisterForClientMessageEvent(messageCallbackId)
@@ -91,7 +101,7 @@ kernel.RunAllAgents(3)
 
 kernel.UnregisterForUpdateEvent(updateCallbackId)
 
-print ""
+print("")
 
 #set the watch level to 0
 result = kernel.ExecuteCommandLine("watch 0", "Soar1")
@@ -100,15 +110,14 @@ result = kernel.ExecuteCommandLine("watch 0", "Soar1")
 result = kernel.ExecuteCommandLine("excise towers-of-hanoi*monitor*operator-execution*move-disk", "Soar1")
 
 #run TOH the rest of the way and time it
-import time
-start = time.clock()
+start = time.time()
 result = agent.RunSelfForever()
-end = time.clock()
-print "\nTime in seconds:", end - start
+end = time.time()
+print("\nTime in seconds:", end - start)
 
 #the output of "print s1" should contain "^rhstest success"
 if kernel.ExecuteCommandLine("print s1", "Soar1").find("^rhstest success") == -1:
-	print >> sys.stderr, "\nRHS test FAILED"
+	print("\nRHS test FAILED", file=sys.stderr)
 	sys.exit(1)
 
 result = kernel.ExecuteCommandLine("init-soar", "Soar1")
@@ -116,7 +125,7 @@ result = kernel.ExecuteCommandLine("init-soar", "Soar1")
 kernel.DestroyAgent(agent)
 
 #remove all the remaining kernel callback functions (not required, just to test)
-print "removing callbacks"
+print("removing callbacks")
 kernel.UnregisterForAgentEvent(agentCallbackId0)
 kernel.UnregisterForAgentEvent(agentCallbackId1)
 kernel.UnregisterForAgentEvent(agentCallbackId2)
