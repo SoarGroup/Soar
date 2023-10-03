17e3797cae01e00c415ee16e42e423d6ea49ba70
Nathan Glenn <garfieldnate@gmail.com>
2023-10-02 19:35:22 -0500

    Small updates to Tcl SML test script
    * Copy the Tcl SML test script to Soar's build directory, next to the Python SML
    test script.
    * Update the test script to use the .soar files updated for the Python SML test
    script.
    * Make the test script runnable from other directories
    * Add a few more assertions. Nothing comprehensive.
    * Improve some formatting.

--- Core/ClientSMLSWIG/Tcl/TestTclSML.tcl
+++ Core/ClientSMLSWIG/Tcl/TestTclSML.tcl
@@ -19,14 +19,22 @@
 #  require some other directory to be there.  Modify the line below appropriately.
 #
 #
-# load the sml stuff
+# load the SML lib from the current directory
 lappend auto_path .
 lappend auto_path [pwd]
 package require tcl_sml_clientinterface
 
+variable myLocation [file normalize [info script]]
 
-set towersOfHanoiFile [file join [pwd] .. Agents towers-of-hanoi-simple towers-of-hanoi.soar]
-set tohTestFile [file join [pwd] test_agents TOHtest.soar]
+if { $argc != 1 } {
+    set agentDir [file join [file dirname $myLocation] SoarUnitTests]
+    puts "agent directory not provided on command line; assuming $agentDir"
+} else {
+    set agentDir [lindex $argv 0]
+}
+
+set towersOfHanoiFile [file join $agentDir Chunking tests towers-of-hanoi-recursive towers-of-hanoi-recursive towers-of-hanoi-recursive_source.soar]
+set tohTestFile [file join $agentDir TOHtest.soar]
 
 if {[expr {![file exists $towersOfHanoiFile]}]} {
     puts stderr "Error: $towersOfHanoiFile does not exist"
@@ -81,21 +89,29 @@ proc UpdateEventCallback {id userData kernel runFlags} {
 	puts "update event fired with flags $runFlags"
 }
 
+set user_message_received 0
 proc UserMessageCallback {id userData agent clientName message} {
-	puts "Agent $agent received usermessage event for clientName '$clientName' with message '$message'"
+    variable user_message_received
+	set user_message_received 1
+    puts "Agent $agent received UserMessage event for clientName '$clientName', userData '$userData' and message '$message'"
+    # assert clientName is TestMessage and message is 'This is a "quoted" message'
+    if { $clientName != "TestMessage" || $message != "This is a \"quoted\" message" } {
+        puts "ERROR: UserMessage event received with incorrect parameters"
+        exit 1
+    }
 }
 
-#this event loop periodically checks for new events from the kernel
+# this event loop periodically checks for new events from the kernel
 # ensuring that they get executed on the client side
 proc CheckForEvents {k} {
 	$k CheckForIncomingCommands
 	after 50 CheckForEvents $k
 }
 
-#create an embedded kernel running in a the current thread (running in a new thread isn't supported in Tcl because of Tcl threading issues))
+# create an embedded kernel running in the current thread (running in a new thread isn't supported in Tcl because of Tcl threading issues))
 set kernel [Kernel_CreateKernelInCurrentThread]
 
-#start event loop (apparently not necessary in this example, presumably because there's no GUI, but illustrative)
+# start event loop (apparently not necessary in this example, presumably because there's no GUI, but illustrative)
 CheckForEvents $kernel
 
 set agentCallbackId0 [$kernel RegisterForAgentEvent $smlEVENT_AFTER_AGENT_CREATED AgentCreatedCallback ""]
@@ -105,7 +121,7 @@ set systemCallbackId [$kernel RegisterForSystemEvent $smlEVENT_BEFORE_SHUTDOWN S
 set rhsCallbackId [$kernel AddRhsFunction RhsFunctionTest ""]
 set updateCallbackId [$kernel RegisterForUpdateEvent $smlEVENT_AFTER_ALL_OUTPUT_PHASES UpdateEventCallback ""]
 set messageCallbackId [$kernel RegisterForClientMessageEvent "TestMessage" UserMessageCallback ""]
-#create an agent named Soar1
+# create an agent named Soar1
 set agent [$kernel CreateAgent Soar1]
 
 set printCallbackId [$agent RegisterForPrintEvent $smlEVENT_PRINT PrintCallback ""]
@@ -114,12 +130,30 @@ set productionCallbackId [$agent RegisterForProductionEvent $smlEVENT_AFTER_PROD
 set runCallbackId [$agent RegisterForRunEvent $smlEVENT_AFTER_PHASE_EXECUTED PhaseExecutedCallback ""]
 set structuredCallbackId [$agent RegisterForXMLEvent $smlEVENT_XML_TRACE_OUTPUT StructuredTraceCallback ""]
 
-#load the TOH productions
+# load the TOH productions
 set result [$agent LoadProductions $towersOfHanoiFile]
-#loads a function to test the user-defined RHS function stuff
+
+if { $result == 0 } {
+	puts "\nFAILED to load $towersOfHanoiFile"
+    exit 1
+}
+
+# loads a function to test the user-defined RHS function stuff
 set result [$agent LoadProductions $tohTestFile]
 
-$kernel SendClientMessage $agent "TestMessage" "This is a \"quoted\"\" message"
+if { $result == 0 } {
+	puts "\nFAILED to load $tohTestFile"
+    exit 1
+}
+
+$kernel SendClientMessage $agent "TestMessage" "This is a \"quoted\" message"
+if { $user_message_received == 0 } {
+    puts "ERROR: UserMessage event not received"
+    exit 1
+} else {
+    puts "SUCCESS: UserMessage event received"
+}
+
 $kernel UnregisterForClientMessageEvent $messageCallbackId
 
 $agent RunSelf 2 $sml_ELABORATION
@@ -128,22 +162,21 @@ $agent UnregisterForProductionEvent $productionCallbackId
 $agent UnregisterForRunEvent $runCallbackId
 
 $kernel RunAllAgents 3
-#$agent RunSelf 3 $sml_DECIDE
 
 $kernel UnregisterForUpdateEvent $updateCallbackId
 
 puts ""
 
-#set the watch level to 0
+# set the watch level to 0
 set result [$kernel ExecuteCommandLine "watch 0" Soar1]
-#excise the monitor production
+# excise the monitor production
 set result [$kernel ExecuteCommandLine "excise towers-of-hanoi*monitor*operator-execution*move-disk" Soar1]
 
-#run TOH the rest of the way and time it using Tcl's built-in timer
+# run TOH the rest of the way and time it using Tcl's built-in timer
 set speed [time {set result [$agent RunSelfForever]}]
 puts "\n$speed"
 
-#the output of "print s1" should contain "^rhstest success"
+# the output of "print s1" should contain "^rhstest success"
 if { [string first "^rhstest success" [$kernel ExecuteCommandLine "print s1" Soar1]] == -1 } {
 	puts "\nRHS test FAILED"
 } else {
@@ -154,7 +187,7 @@ set result [$kernel ExecuteCommandLine "init-soar" Soar1]
 
 $kernel DestroyAgent $agent
 
-#remove all the remaining kernel callback functions (not required, just to test)
+# remove all the remaining kernel callback functions (not required, just to test)
 puts "removing callbacks"
 $kernel UnregisterForAgentEvent $agentCallbackId0
 $kernel UnregisterForAgentEvent $agentCallbackId1
@@ -162,12 +195,12 @@ $kernel UnregisterForAgentEvent $agentCallbackId2
 $kernel UnregisterForSystemEvent $systemCallbackId
 $kernel RemoveRhsFunction $rhsCallbackId
 
-#shutdown the kernel; this makes sure agents are deleted and events fire correctly
+# shutdown the kernel; this makes sure agents are deleted and events fire correctly
 $kernel Shutdown
 
-#delete kernel object
+# delete kernel object
 set result [$kernel -delete]
-#don't leave bad pointers around
+# don't leave bad pointers around
 unset agent
 unset kernel
 
@@ -175,40 +208,40 @@ unset kernel
 # This is important for some environments (i.e. Tcl Eaters created each agent in a separate slave interpreter)
 # This also demonstrates adding something to the input-link (which we don't do above)
 
-#create slave
+# create slave
 interp create red
 
-#load package in slave
+# load package in slave
 red eval lappend auto_path .
 red eval lappend auto_path [pwd]
 red eval package require tcl_sml_clientinterface
 
-#create kernel and agent
+# create kernel and agent
 set kernel [red eval Kernel_CreateKernelInNewThread]
 set agent [red eval $kernel CreateAgent Soar1]
 
-#add wme to input-link
-set il [red eval $agent GetInputLink]
-set wme [red eval $agent CreateIdWME $il test]
+# add wme to input-link
+set input_link [red eval $agent GetInputLink]
+set wme [red eval $agent CreateIdWME $input_link test]
 
-#commit the changes to working memory
+# commit the changes to working memory
 red eval $agent Commit
 
-#print the timetag
-#note that the timetag will be negative since it's a client-side timetag
-#client-side timetags don't match kernel-side timetags for performance reasons
+# print the timetag
+# note that the timetag will be negative since it's a client-side timetag
+# client-side timetags don't match kernel-side timetags for performance reasons
 set timetag [red eval $wme GetTimeTag]
 puts "timetag = $timetag"
 
-#shutdown the kernel; this makes sure agents are deleted and events fire correctly
+# shutdown the kernel; this makes sure agents are deleted and events fire correctly
 red eval $kernel Shutdown
-#delete the kernel
+# delete the kernel
 red eval $kernel -delete
 
-#delete the interp
+# delete the interp
 interp delete red
 
-#don't leave bad pointers around
+# don't leave bad pointers around
 unset agent
 unset kernel
 
