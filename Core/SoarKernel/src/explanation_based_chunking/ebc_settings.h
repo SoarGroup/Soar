71e11677d365a163ce8efba94da00fdfdbfd49a7
scijones <scijones@umich.edu>
2024-03-22 11:53:07 -0400

    new setting that makes soar turn any string attribute into a singleton pattern, set to "on" by default

--- Core/SoarKernel/src/explanation_based_chunking/ebc_settings.h
+++ Core/SoarKernel/src/explanation_based_chunking/ebc_settings.h
@@ -37,6 +37,7 @@ class ebc_param_container: public soar_module::param_container
         soar_module::boolean_param* interrupt_on_chunk;
         soar_module::boolean_param* interrupt_on_warning;
         soar_module::boolean_param* interrupt_on_watched;
+        soar_module::boolean_param* automatically_create_singletons;
 
         /* Mechanisms */
         soar_module::boolean_param* mechanism_add_OSK;
