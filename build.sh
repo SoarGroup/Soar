a0d95ddb349ddad7bf55472f4be42b8b864f11a4
Moritz Schmidt <mo_schmidt@posteo.de>
2025-04-22 08:18:32 +0000

    build: Fix typo in build script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ pip install conan
 conan profile detect --force
 conan install . --build=missing
 conan install . --build=missing -s build_type=Debug
-cmake --workflow --preset debug-test-workflow
+cmake --workflow --preset Debug-test-workflow
 cmake --install build/Debug
